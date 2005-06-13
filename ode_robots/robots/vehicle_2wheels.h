096839953d3659d539d75eed5a8d87f13a0775a8
fhesse <fhesse>
2005-06-13 16:48:36 +0000

    inherited from abstract robot

--- ode_robots/robots/vehicle_2wheels.h
+++ ode_robots/robots/vehicle_2wheels.h
@@ -1,230 +1,238 @@
+#ifndef __VEHICLE_H
+#define __VEHICLE_H
 
 
-class Vehicle{
+class Vehicle : public AbstractRobot{
  public:  
   // some constants
 
-#define LENGTH 0.7/10.0	// chassis length
-#define WIDTH 0.5/4.0	// chassis width
-#define HEIGHT 0.2	// chassis height
-#define RADIUS 0.18/2.8	// wheel radius
-  //#define STARTZ 0.5	// starting height of chassis
-#define CMASS 1.0		// chassis mass
-#define WMASS 0.2	// wheel mass
+  double length;  // chassis length
+  double width;  // chassis width
+  double height;   // chassis height
+  double radius;  // wheel radius
+  double cmass;    // chassis mass
+  double wmass;    // wheel mass
+  int sensorno;      //number of sensors
+  int motorno;       // number of motors
+  int segmentsno;    // number of motorsvehicle segments
   
 
-  double start_x, start_y, start_z;
-  double color_r, color_g, color_b;
-  double max_force;
-  
+  double pos_x, pos_y, pos_z;  // position of vehicle
+  double max_force;            // maximal force for motors
 
-
-  // dynamics and collision objects (chassis, 3 wheels, environment)
-
-  dBodyID body[4];
-  dJointID joint[3];	// joint[0] is the front wheel
+  Object object[3];  // 1 cylinder, 2 wheels
+  dJointID joint[2]; // joints between cylinder and each wheel
 
   dSpaceID car_space;
-  dGeomID box[1];
-  dGeomID sphere[3];
-
-
-
-  // things that the user controls
-  
-  dReal speed,steer;	// user commands
- 
-  dSpaceID *space;
-  dWorldID *world;
-
 
  public:
   
-  Vehicle(dWorldID *w, dSpaceID *s){
-    world=w;   	
-    space=s;
+  Vehicle(dWorldID *w, dSpaceID *s):
+    AbstractRobot::AbstractRobot(w,s){
 
-    speed=0;
-    steer=0;
+    pos_x=0.0;
+    pos_y=0.0;
+    pos_z=0.5;
   
-    start_x=0.0;
-    start_y=0.0;
-    start_z=0.5;
-  
-
-  
-    color_r=0;
-    color_g=1;
-    color_b=1;
+    color.r=0;
+    color.g=1;
+    color.b=1;
     
     max_force=0.05;
+
+    length=0.07; 
+    width=0.125; 
+    height=0.2;  
+    radius=0.064;
+    cmass=1.0;  
+    wmass=0.2;  
+    sensorno=2; 
+    motorno=2;  
+    segmentsno=3;
+
+  };
+
+  virtual ~Vehicle(){
+  };
+
+  /** sets actual motorcommands
+      @param motors motors scaled to [-1,1] 
+      @param motornumber length of the motor array
+  */
+  virtual void setMotors(motor* motors, int motornumber){
+    double tmp;
+    int len = (motornumber < motorno)? motornumber : motorno;
+    for (int i=0; i<len; i++){ 
+      motors[i]*=20.0;  // scaling
+      tmp=dJointGetHinge2Param(joint[i],dParamVel2);
+      dJointSetHinge2Param(joint[i],dParamVel2,tmp + 0.5*(motors[i]-tmp) );
+      dJointSetHinge2Param (joint[i],dParamFMax2,max_force);
+    }
+  };
+
+  /** returns actual sensorvalues
+      @param sensors sensors scaled to [-1,1] (more or less)
+      @param sensornumber length of the sensor array
+      @return number of actually written sensors
+  */
+  virtual int getSensors(sensor* sensors, int sensornumber){
+    int len = (sensornumber < sensorno)? sensornumber : sensorno;
+    for (int i=0; i<len; i++){
+      sensors[i]=dJointGetHinge2Angle2Rate(joint[i]);
+      sensors[i]*=0.05;  //scaling
+    }
+    return len;
+  };
+
+  /** returns number of sensors
+  */
+  virtual int getSensorNumber(){
+    return sensorno;
+  };
+
+  /** returns number of motors
+  */
+  virtual int getMotorNumber(){
+    return motorno;
+  };
+
+  /** sets the position of robot to pos
+      @param pos vector of desired position (x,y,z)
+   */
+
+
+  /** sets the vehicle to position  pos
+      @param pos vector of desired position (x,y,z)
+   */
+  virtual void setPosition(const dVector3& pos){
+  //virtual void setPosition(double x, double y, double z_){
+    double z=pos[2]+radius; // to put wheels on ground, not in ground
+    dBodySetPosition (object[1].body,pos[0] ,pos[1] +width*0.5,z);
+    dBodySetPosition (object[2].body,pos[0] ,pos[1] -width*0.5,z);
+    dBodySetPosition (object[0].body,pos[0],pos[1],z);    
     
   };
 
-  ~Vehicle(){
+  /** returns position of vehicle 
+      @param pos vector of position (x,y,z)
+   */
+  virtual void getPosition(dVector3& pos){
+    const dReal* act_pos=dBodyGetPosition(object[0].body);
+    for (int i=0; i<3; i++){
+      pos[i]=act_pos[i];
+    }
+   
+    pos[2]-=radius; // substract wheel radius, because vehicle stands on the ground
   };
 
-  void draw(){
-    dsSetColor (color_r,color_g,color_b);
+
+  /** sets vehicle back to initial position   
+   */
+  virtual void setToInitialPosition(){
+    dBodySetPosition (object[0].body,pos_x, pos_y,           pos_z+radius);
+    dBodySetPosition (object[1].body,pos_x ,pos_y+ width*0.5,pos_z+radius );
+    dBodySetPosition (object[2].body,pos_x ,pos_y -width*0.5,pos_z+radius );
+  };
+  
+  /** returns a list with the positionvectors of all segments of the robot
+      @param poslist list with positionvectors (of all robot segments) (free after use!)
+      @return length of the list
+  */
+  virtual int getSegmentsPosition(dVector3*& poslist){
+    poslist = (dVector3*)malloc(segmentsno * sizeof(dVector3));
+    for (int i=0; i<segmentsno; i++){
+      const dReal* act_pos = dBodyGetPosition(object[i].body);
+      memcpy(poslist[i], act_pos,  4*sizeof(dReal));
+    }
+    return segmentsno;
+  };
+  
+
+  //
+  // welche von den folgenden in abstractrobot aufnehmen?
+  //
+
+
+  /** returns number of segments of the vehicle
+  */
+  virtual int getNumberSegments(){
+    return segmentsno;
+  };
+
+
+  /**
+   * draws the vehicle
+   */
+  virtual void draw(){
+    dsSetColor (color.r,color.g,color.b); // set color for cylinder
     dsSetTexture (DS_WOOD);
-    //dReal sides[3] = {LENGTH,WIDTH,HEIGHT};
-    //dsDrawBox (dBodyGetPosition(body[0]),dBodyGetRotation(body[0]),sides);
-    dsDrawCappedCylinder(dBodyGetPosition(body[0]),dBodyGetRotation(body[0]),LENGTH, WIDTH/2 );
-    dsSetColor (1,1,1);
-    //for (int i=1; i<=3; i++) {
-    for (int i=2; i<=3; i++) {
-      dsDrawCylinder (dBodyGetPosition(body[i]), dBodyGetRotation(body[i]),0.02f,RADIUS);
+    dsDrawCappedCylinder(dBodyGetPosition(object[0].body),dBodyGetRotation(object[0].body),length, width/2 );
+    dsSetColor (1,1,1); // set color for wheels
+    // draw wheels
+    for (int i=1; i<3; i++) { 
+      dsDrawCylinder (dBodyGetPosition(object[i].body), dBodyGetRotation(object[i].body),0.02f,radius);
     }
   };
 
   void create(){
     dMass m;
-
-
-    // chassis body
-    body[0] = dBodyCreate (*world);
-    dBodySetPosition (body[0],start_x,start_y,start_z);
-    //dMassSetBox (&m,1,LENGTH,WIDTH,HEIGHT);
-      dQuaternion q;
-      dQFromAxisAndAngle (q,0,1,0,M_PI*0.5);
-      dBodySetQuaternion (body[0],q);
-
-    dMassSetCappedCylinder(&m,1,1,WIDTH/2,LENGTH);
-    dMassAdjust (&m,CMASS);
-    dBodySetMass (body[0],&m);
-    //box[0] = dCreateBox (0,LENGTH,WIDTH,HEIGHT);
-    box[0] = dCreateCCylinder (0,WIDTH/2,LENGTH);
-    dGeomSetBody (box[0],body[0]);
+    // cylinder
+    object[0].body = dBodyCreate (*world);
+    dBodySetPosition (object[0].body,pos_x,pos_y,pos_z+radius);
+    dQuaternion q;
+    dQFromAxisAndAngle (q,0,1,0,M_PI*0.5);
+    dBodySetQuaternion (object[0].body,q);
+    
+    dMassSetCappedCylinder(&m,1,1,width/2,length);
+    dMassAdjust (&m,cmass);
+    dBodySetMass (object[0].body,&m);
+    object[0].geom = dCreateCCylinder (0,width/2,length);
+    dGeomSetBody (object[0].geom, object[0].body);
 
     // wheel bodies
-    //for (int i=1; i<=3; i++) {
-    for (int i=2; i<=3; i++) {
-      body[i] = dBodyCreate (*world);
+    for (int i=1; i<3; i++) {
+      object[i].body = dBodyCreate (*world);
       dQuaternion q;
       dQFromAxisAndAngle (q,1,0,0,M_PI*0.5);
-      dBodySetQuaternion (body[i],q);
-      dMassSetSphere (&m,1,RADIUS);
-      dMassAdjust (&m,WMASS);
-      dBodySetMass (body[i],&m);
-      sphere[i-1] = dCreateSphere (0,RADIUS);
-      dGeomSetBody (sphere[i-1],body[i]);
+      dBodySetQuaternion (object[i].body,q);
+      dMassSetSphere (&m,1,radius);
+      dMassAdjust (&m,wmass);
+      dBodySetMass (object[i].body,&m);
+      object[i].geom = dCreateSphere (0,radius);
+      dGeomSetBody (object[i].geom,object[i].body);
     }
-    //dBodySetPosition (body[1],0.5*LENGTH,0,STARTZ-HEIGHT*0.5);
-    dBodySetPosition (body[2],start_x /*-0.5*LENGTH*/,start_y+ WIDTH*0.5,start_z /*-HEIGHT*0.5*/);
-    dBodySetPosition (body[3],start_x /*-0.5*LENGTH*/,start_y -WIDTH*0.5,start_z /*-HEIGHT*0.5*/);
+    dBodySetPosition (object[1].body,pos_x ,pos_y+ width*0.5,pos_z+radius );
+    dBodySetPosition (object[2].body,pos_x ,pos_y -width*0.5,pos_z+radius );
 
-    // front wheel hinge
-    /*
-      joint[0] = dJointCreateHinge2 (world,0);
-      dJointAttach (joint[0],body[0],body[1]);
-      const dReal *a = dBodyGetPosition (body[1]);
-      dJointSetHinge2Anchor (joint[0],a[0],a[1],a[2]);
-      dJointSetHinge2Axis1 (joint[0],0,0,1);
-      dJointSetHinge2Axis2 (joint[0],0,1,0);
-    */
 
-    // front and back wheel hinges
-    //for (int i=0; i<3; i++) {
-    for (int i=1; i<3; i++) {
+    for (int i=0; i<2; i++) {
       joint[i] = dJointCreateHinge2 (*world,0);
-      dJointAttach (joint[i],body[0],body[i+1]);
-      const dReal *a = dBodyGetPosition (body[i+1]);
-      dJointSetHinge2Anchor (joint[i],a[0],a[1],a[2]);
+      dJointAttach (joint[i],object[0].body,object[i+1].body);
+      const dReal *a = dBodyGetPosition (object[i+1].body);
+      dJointSetHinge2Anchor(joint[i],a[0],a[1],a[2]);
       dJointSetHinge2Axis1 (joint[i],0,0,1);
       dJointSetHinge2Axis2 (joint[i],0,1,0);
     }
-
-    // set joint suspension
-    //for (int i=0; i<3; i++) {
-    /*    for (int i=1; i<3; i++) {
-      dJointSetHinge2Param (joint[i],dParamSuspensionERP,0.4);
-      dJointSetHinge2Param (joint[i],dParamSuspensionCFM,0.8);
-    }
-    */
-    // lock back wheels along the steering axis
-    for (int i=1; i<3; i++) {
+    for (int i=0; i<2; i++) {
       // set stops to make sure wheels always stay in alignment
       dJointSetHinge2Param (joint[i],dParamLoStop,0);
       dJointSetHinge2Param (joint[i],dParamHiStop,0);
-      // the following alternative method is no good as the wheels may get out
-      // of alignment:
-      //   dJointSetHinge2Param (joint[i],dParamVel,0);
-      //   dJointSetHinge2Param (joint[i],dParamFMax,dInfinity);
     }
-
     // create car space and add it to the top level space
     car_space = dSimpleSpaceCreate (*space);
     dSpaceSetCleanup (car_space,0);
-    dSpaceAdd (car_space,box[0]);
-    //dSpaceAdd (car_space,sphere[0]);
-    dSpaceAdd (car_space,sphere[1]);
-    dSpaceAdd (car_space,sphere[2]);
-
-
-  };
-
-
-  void setMotors(double *x){
-/*  
-    // motor
-    dJointSetHinge2Param (joint[0],dParamVel2,-speed);
-    dJointSetHinge2Param (joint[0],dParamFMax2,0.1);
-  
-    // steering
-    dReal v = steer - dJointGetHinge2Angle1 (joint[0]);
-    if (v > 0.1) v = 0.1;
-    if (v < -0.1) v = -0.1;
-    v *= 10.0;
-    dJointSetHinge2Param (joint[0],dParamVel,v);
-    dJointSetHinge2Param (joint[0],dParamFMax,0.2);
-    dJointSetHinge2Param (joint[0],dParamLoStop,-0.75);
-    dJointSetHinge2Param (joint[0],dParamHiStop,0.75);
-    dJointSetHinge2Param (joint[0],dParamFudgeFactor,0.1);
-*/
-
-    // motor
-    //dJointSetHinge2Param (joint[1],dParamVel2,x[0]);
-    double tmp;
-    tmp=dJointGetHinge2Param(joint[1],dParamVel2);
-    dJointSetHinge2Param(joint[1],dParamVel2,tmp + 0.5*(x[0]-tmp) );
-    dJointSetHinge2Param (joint[1],dParamFMax2,max_force);
+    for (int i=0; i<3; i++){
+      dSpaceAdd (car_space,object[i].geom);
+    }
+  }; 
 
 
 
-    //dJointSetHinge2Param (joint[2],dParamVel2,x[1]);
-    tmp=dJointGetHinge2Param(joint[2],dParamVel2);
-    dJointSetHinge2Param(joint[2],dParamVel2,tmp + 0.5*(x[1]-tmp) );
-    dJointSetHinge2Param (joint[2],dParamFMax2,max_force);
-  
-  };
 
-  void getSensors(double *x){
-    x[0]=dJointGetHinge2Param (joint[1],dParamVel2);  //  falsch, gibt einfach nur wieder den Hingeparameter zurück
-    x[1]=dJointGetHinge2Param (joint[2],dParamVel2);
-
-
-  };
-
-  void setInitialPosition(double x, double y, double z){
-    start_x=x;
-    start_y=y;
-    start_z=z+0.3; // sonst Räder im Boden
-  };
-
-  void setPosition(double x, double y, double z_){
-    double z=z_+RADIUS; // sonst Räder im Boden
-    
-    dBodySetPosition (body[2],x /*-0.5*LENGTH*/,y+ WIDTH*0.5,z /*-HEIGHT*0.5*/);
-    dBodySetPosition (body[3],x /*-0.5*LENGTH*/,y -WIDTH*0.5,z /*-HEIGHT*0.5*/);
-    dBodySetPosition (body[0],x,y,z);    
-    
-  };
-  
-  
   void setColor(double r, double g, double b){
-    color_r=r;
-    color_g=g;
-    color_b=b;
+    color.r=r;
+    color.g=g;
+    color.b=b;
   };
   void setMaxForce(double m){
     max_force=m;
@@ -234,3 +242,6 @@ class Vehicle{
   };
 
 };
+
+
+#endif
