f8ab10a5f176d898c4d3e115f0467f10855c6152
fhesse <fhesse>
2005-06-14 14:59:10 +0000

    place() instead of create and set Position, getSegmentsPosition with vector

--- ode_robots/abstractrobot.h
+++ ode_robots/abstractrobot.h
@@ -2,6 +2,9 @@
 #define __ABSTRACTROBOT_H
 
 #include <ode/common.h>
+
+#include <vector>
+using namespace std;
  
 typedef double sensor;
 typedef double motor;
@@ -49,8 +52,11 @@ public:
   /// draws the robot
   virtual void draw() = 0;
 
-  /// creates the robot at the given position and with the given color.
-  virtual void create(double& x, double& y, double& z, Color& color) = 0;
+/** sets the vehicle to position pos, sets color to c, and creates robot if necessary
+    @params pos desired position of the robot in struct Position
+    @param c desired color for the robot in struct Color
+*/
+  virtual void place(Position pos, Color *c) = 0;
 
   /** checks for internal collisions and treats them. In case of a treatment return true else false.
   */
@@ -80,22 +86,18 @@ public:
   */
   virtual int getMotorNumber()=0;
 
-  /** sets the position of robot to pos
-      @param pos vector of desired position (x,y,z)
-   */
-  virtual void setPosition(const Position pos)=0;
- 
   /** returns position of robot 
       @param pos vector of desired position (x,y,z)
    */
   virtual Position getPosition()=0;
 
-
-  /** returns a list with the positionvectors of all segments of the robot
-      @param poslist list with positionvectors (of all robot segments) (free after use!)
+  /** returns a vector with the positions of all segments of the robot
+      @param vector of positions (of all robot segments) 
       @return length of the list
   */
-  virtual int getSegmentsPosition(Position*& poslist) = 0;  
+  virtual int getSegmentsPosition(vector<Position> &poslist) = 0;
+
+
  protected:
 
   dSpaceID *space;
