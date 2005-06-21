4ce77bad0a5e21391bf8d4ffc0dede699056c098
martius <martius>
2005-06-21 15:30:33 +0000

    sensornumber taken from controller!

--- ode_robots/agents/plotagent.cpp
+++ ode_robots/agents/plotagent.cpp
@@ -20,7 +20,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.3  2005-06-17 10:48:58  martius
+ *   Revision 1.4  2005-06-21 15:30:33  martius
+ *   sensornumber taken from controller!
+ *
+ *   Revision 1.3  2005/06/17 10:48:58  martius
  *   Catched Signal SIGPIPE (in case Logger dies)
  *   Logging mode as enum (GuiLogger, NoPlot)
  *
@@ -30,6 +33,8 @@
  ***************************************************************************/
 #include "plotagent.h"
 #include <signal.h>
+#include "printInternals.h"
+#include "printInternals.cpp" // TODO: avoid this kind of hack
 
 PlotAgent::PlotAgent(PlotMode plotmode/*=GuiLogger*/)
   : Agent(), plotmode(plotmode) 
@@ -60,32 +65,16 @@ bool PlotAgent::init(AbstractController* controller, AbstractRobot* robot){
   if(!Agent::init(controller, robot)) return false;  
   if(plotmode == GuiLogger){
     if(!OpenGui()) return false;
-    ///// send channel names to GUI
-    // first the sensor and motor channels
-    int s = robot->getSensorNumber();
+    int s = controller->getSensorNumber();
     int m = robot->getMotorNumber();
-    fprintf(pipe,"#C");
-    for(int i = 0; i < s; i++){
-      fprintf(pipe, " x[%i]", i);
-    }
-    for(int i = 0; i < m; i++){
-      fprintf(pipe, " y[%i]", i);
-    }
-    // then the internal parameters
-    paramkey* keys;
-    numberInternalParameters = controller->getInternalParamNames(keys);
-    for(int i = 0; i < numberInternalParameters; i++){
-      fprintf(pipe, " %s", keys[i]);
-    }
-    fprintf(pipe,"\n"); // terminate line
-    fflush(pipe);
+    numberInternalParameters = printInternalParameterNames(pipe, s,m,controller);
   }
   return true;
 }
 
 void PlotAgent::plot(const sensor* x, int sensornumber, const motor* y, int motornumber){
   if(!controller || !x || !y || plotmode==NoPlot || !pipe) return;
-  if(sensornumber!=robot->getSensorNumber()) {
+  if(sensornumber!=controller->getSensorNumber()) {
     fprintf(stderr, "%s:%i: Given sensor number does not match the one from robot!\n", 
 	    __FILE__, __LINE__);
   }
@@ -93,26 +82,8 @@ void PlotAgent::plot(const sensor* x, int sensornumber, const motor* y, int moto
     fprintf(stderr, "%s:%i: Given motor number does not match the one from robot!\n", 
 	    __FILE__, __LINE__);
   }
-  for(int i = 0; i < sensornumber; i++){
-    fprintf(pipe, " %f", x[i]);
-  }
-  for(int i = 0; i < motornumber; i++){
-    fprintf(pipe, " %f", y[i]);
-  }
-  // internal parameters ( we allocate one place more to be able to realise when the number raises)
-  paramval* vals = (paramval*)malloc(sizeof(paramval) * numberInternalParameters+1);
-  int len = controller->getInternalParams(vals, numberInternalParameters+1);
-  if(len!= numberInternalParameters) {
-    fprintf(stderr, "%s:%i: Weird! Internal parameter number seems to have changed!\n", 
-	    __FILE__, __LINE__);
-  }
-  for(int i=0; i<len; i++){
-    fprintf(pipe, " %f", vals[i]);
-  }
 
-  fprintf(pipe,"\n"); // terminate line
-  
-  fflush(pipe);
-  free(vals);
+  printInternalParameters(pipe, x, sensornumber, y, motornumber, 
+			  numberInternalParameters, controller);
+
 };
-
