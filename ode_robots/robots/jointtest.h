62f0163cd2b59f8f3314f022cec246de71284cf8
fhesse <fhesse>
2005-06-22 15:37:45 +0000

    sensor and motor values are wheel velocities

---
+++ ode_robots/robots/jointtest.h
@@ -0,0 +1,135 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1  2005-06-22 15:37:45  fhesse
+ *   sensor and motor values are wheel velocities
+ *
+ *                                                                 *
+ ***************************************************************************/
+#ifndef __JOINTTEST_H
+#define __JOINTTEST_H
+
+#include "abstractrobot.h"
+
+class JointTest : public AbstractRobot{
+
+public:
+  
+  JointTest(dWorldID *w, dSpaceID *s, dJointGroupID *c);
+
+  virtual ~JointTest(){};
+
+  /**
+   * draws the robot
+   */
+  virtual void draw();
+
+  /** sets the robot to position pos, sets color to c, and creates robot if necessary
+      @params pos desired position of the robot in struct Position
+      @param c desired color for the robot in struct Color
+  */
+  virtual void place(Position pos , Color *c = 0);
+
+  /** checks for internal collisions and treats them. 
+   *  In case of a treatment return true (collision will be ignored by other objects and the default routine)
+   *  else false (collision is passed to other objects and (if not treated) to the default routine).
+   */
+  virtual bool collisionCallback(void *data, dGeomID o1, dGeomID o2);
+
+  /** returns actual sensorvalues
+      @param sensors sensors scaled to [-1,1] 
+      @param sensornumber length of the sensor array
+      @return number of actually written sensors
+  */
+  virtual int getSensors(sensor* sensors, int sensornumber);
+
+  /** sets actual motorcommands
+      @param motors motors scaled to [-1,1] 
+      @param motornumber length of the motor array
+  */
+  virtual void setMotors(const motor* motors, int motornumber);
+
+  /** returns number of sensors
+   */
+  virtual int getSensorNumber(){
+    return sensorno;
+  };
+
+  /** returns number of motors
+   */
+  virtual int getMotorNumber(){
+    return motorno;
+  };
+
+  /** returns position of robot 
+      @return position robot position in struct Position  
+  */
+  virtual Position getPosition();
+
+  /** returns a vector with the positions of all segments of the robot
+      @param poslist vector of positions (of all robot segments) 
+      @return length of the list
+  */
+  virtual int getSegmentsPosition(vector<Position> &poslist);
+
+protected:
+
+  /** creates robot at desired position 
+      @param pos struct Position with desired position
+  */
+  virtual void create(Position pos); 
+
+  /** destroys robot and space
+   */
+  virtual void destroy();
+
+  /**
+   *vereinfachte Objektpositions-Einzelermittlungsfunktion
+   *@author Marcel Kretschmann
+   *@version
+   **/
+  virtual double dBodyGetPositionAll ( dBodyID basis , int para );
+
+  double glieder_masse;
+  double glieder_durchmesser;
+  double glieder_laenge;
+  const static  int armanzahl=2;
+  Object segments[armanzahl];
+  dJointID joints[armanzahl+1];
+
+
+  int sensorno;      //number of sensors
+  int motorno;       // number of motors
+  int segmentsno;    // number of motorsrobot segments
+  
+  Position initial_pos;    // initial position of robot
+  double max_force;        // maximal force for motors
+
+  bool created;      // true if robot was created
+
+  int t;
+  bool positiv;
+
+};
+
+
+#endif
