edd91746eab2de5e964763d6be72ab173dc1d175
fhesse <fhesse>
2005-07-05 07:57:15 +0000

    some tests with force, nimm2 and sphere added, invermotorspace used as controller, gravity=0

--- ode_robots/simulations/template_schlangeforce/main.cpp
+++ ode_robots/simulations/template_schlangeforce/main.cpp
@@ -5,10 +5,13 @@
 #include "simulation.h"
 #include "one2oneagent.h"
 #include "playground.h"
+#include "sphere.h"
 
 #include "invertnchannelcontroller.h"
+#include "invertmotorspace.h"
 
 #include "schlangeforce.h"
+#include "nimm2.h"
 
 ConfigList configs;
 
@@ -28,39 +31,87 @@ void start()
   dsPrint ( "------------------------------------------------------------------------\n" );
   dsPrint ( "Press Ctrl-C for an basic commandline interface.\n\n" );
 
+  dWorldSetGravity ( world , 0 , 0 , 0 );
+  
+
   //Anfangskameraposition und Punkt auf den die Kamera blickt
-  float KameraXYZ[3]= {2.1640f,-1.3079f,1.7600f};
-  float KameraViewXYZ[3] = {125.5000f,-17.0000f,0.0000f};;
+  float KameraXYZ[3]= {0.276f,7.12f,1.78f};
+  float KameraViewXYZ[3] = {-88.0f,-5.5f,0.0000f};;
   dsSetViewpoint ( KameraXYZ , KameraViewXYZ );
   dsSetSphereQuality (2); //Qualitaet in der Sphaeren gezeichnet werden
 
   // initialization
   simulationConfig.noise=0.1;
+  simulationConfig.setParam("simstepsize",0.005);
+  configs.push_back(&simulationConfig);
+
 
   Playground* playground = new Playground(&world, &space);
-  playground->setGeometry(70.0, 0.2, 1.5);
+  playground->setGeometry(7.0, 0.2, 1.5);
   playground->setPosition(0,0,0); // playground positionieren und generieren
   obstacles.push_back(playground);
 
+  Sphere* sphere[1];
+  for (int i=0; i<1; i++){
+    sphere[i]= new Sphere(&world, &space);
+    sphere[i]->setPosition(0,0,3+i); //positionieren und generieren
+    obstacles.push_back(sphere[i]);
+  }
+
   //****************
-  SchlangeForce* schlange1 = new SchlangeForce ( 1 , &world , &space , &contactgroup , 0 , 0 , 0.25 , 4 , 0.5 , 0.2 , 0 , 0.1 , 2 , 10 , anglerate);
-  Position p = {0,0,0};
+  SchlangeForce* schlange1 = new SchlangeForce ( 1 , &world , &space , &contactgroup , 0 , 0 , 0.25 , 8 , 0.8/*0.5*/ , 0.2 , 0 , 0.4 , 2 , 10 , anglerate);
+  Position p = {0,0,10};
   Color col = {0,0.5,0.8};
   schlange1->place(p,&col);
-  AbstractController *controller = new InvertNChannelController(10,true);  
+  AbstractController *controller = new InvertMotorSpace(100/*,true*/);  
   
   One2OneAgent* agent = new One2OneAgent(/*NoPlot*/GuiLogger);
   agent->init(controller, schlange1);
   agents.push_back(agent);
-
-  
-  //****************  
-  configs.push_back(&simulationConfig);
-  //****************  
-  
   configs.push_back(controller);
   configs.push_back(schlange1);
-  //************
+
+  simulationConfig.setParam("controlinterval",5);
+
+  controller->setParam("eps",0.5);
+  controller->setParam("factorA",0.3);
+  controller->setParam("factorB",0.0);
+  controller->setParam("zetaupdate",0.0);
+  controller->setParam("s4avg",40.0);
+
+  schlange1->setParam("gamma", 1.0);
+  schlange1->setParam("friction_ground",0.3);
+  schlange1->setParam("factor_force", 10);
+
+
+
+  /*
+  SchlangeForce* schlange3 = new SchlangeForce ( 2 , &world , &space , &contactgroup , 0 , 0 , 0.25 , 4 , 0.5 , 0.2 , 0 , 0.4 , 2 , 10 , anglerate);
+  Position p3 = {1,1,0};
+  Color col3 = {1,1,0};
+  schlange1->place(p3,&col3);
+  AbstractController *controller3 = new InvertNChannelController(10,true);  
+  
+  One2OneAgent* agent3 = new One2OneAgent(NoPlot/*GuiLogger* /);
+  agent3->init(controller3, schlange3);
+  agents.push_back(agent3);
+  configs.push_back(controller3);
+  configs.push_back(schlange3);
+  */
+
+  /*
+  Nimm2* vehicle = new Nimm2 ( &world , &space , &contactgroup);
+  Position p2 = {0,0,3};
+  vehicle->place(p2);
+  AbstractController *controller2 = new InvertNChannelController(10,true);  
+  
+  One2OneAgent* agent2 = new One2OneAgent(NoPlot/*GuiLogger* /);
+  agent2->init(controller2, vehicle);
+  agents.push_back(agent2);
+  //  configs.push_back(controller2);
+  */
+  
+  
   
   showParams(configs);
 }
