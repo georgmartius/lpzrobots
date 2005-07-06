93eb5a02f6a93332694eee8cf0e6bf18c04f7e29
martius <martius>
2005-07-06 16:02:07 +0000

    pseudo sensor test

---
+++ ode_robots/simulations/pseudosensor_wheelrobot/main.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <drawstuff/drawstuff.h>
+#include <ode/ode.h>
+
+#include "noisegenerator.h"
+#include "simulation.h"
+#include "one2oneagent.h"
+#include "derivativeagent.h"
+#include "nimm2.h"
+#include "playground.h"
+
+#include "invertmotorspace.h"
+
+ConfigList configs;
+
+// Funktion die die Steuerung des Roboters uebernimmt
+bool StepRobot()
+{
+  for(AgentList::iterator i=agents.begin(); i != agents.end(); i++){
+    (*i)->step(simulationConfig.noise);
+  }
+  return true;
+}
+
+//Startfunktion die am Anfang der Simulationsschleife, einmal ausgefuehrt wird
+void start() 
+{
+  dsPrint ( "\nWelcome to the virtual ODE - robot simulator of the Robot Group Leipzig\n" );
+  dsPrint ( "------------------------------------------------------------------------\n" );
+  dsPrint ( "Press Ctrl-C for an basic commandline interface.\n\n" );
+
+  //Anfangskameraposition und Punkt auf den die Kamera blickt
+  float KameraXYZ[3]= {2.1640f,-1.3079f,1.7600f};
+  float KameraViewXYZ[3] = {125.5000f,-17.0000f,0.0000f};;
+  dsSetViewpoint ( KameraXYZ , KameraViewXYZ );
+  dsSetSphereQuality (2); //Qualitaet in der Sphaeren gezeichnet werden
+
+  // initialization
+  simulationConfig.noise=0.15;
+  
+  Playground* playground = new Playground(&world, &space);
+  playground->setGeometry(7.0, 0.2, 1.5);
+  playground->setPosition(0,0,0); // playground positionieren und generieren
+  obstacles.push_back(playground);
+
+  Nimm2* vehicle = new Nimm2(&world, &space, &contactgroup);
+  Position p = {0,0,0};
+  vehicle->place(p);
+  AbstractController *controller = new InvertMotorSpace(10);  
+  controller->setParam("factorB",0);
+  controller->setParam("eps",0.5);
+  
+  //  One2OneAgent* agent = new One2OneAgent(new ColorUniformNoise(0.1), GuiLogger);
+  DerivativeAgent* agent = new DerivativeAgent(true, false, false, 0.05, new ColorUniformNoise(0.1), GuiLogger,5);
+  agent->init(controller, vehicle);
+  agents.push_back(agent);
+  
+  configs.push_back(&simulationConfig);
+  configs.push_back(controller);
+  showParams(configs);
+}
+
+void end(){
+  for(ObstacleList::iterator i=obstacles.begin(); i != obstacles.end(); i++){
+    delete (*i);
+  }
+  obstacles.clear();
+  for(AgentList::iterator i=agents.begin(); i != agents.end(); i++){
+    delete (*i)->getRobot();
+    delete (*i)->getController();
+    delete (*i);
+  }
+  agents.clear();
+}
+
+
+// this function is called if the user pressed Ctrl-C
+void config(){
+  changeParams(configs);
+}
+
+
+int main (int argc, char **argv)
+{  
+  // initialise the simulation and provide the start, end, and config-function
+  simulation_init(&start, &end, &config);
+  // start the simulation (returns, if the user closes the simulation)
+  simulation_start(argc, argv);
+  simulation_close();  // tidy up.
+  return 0;
+}
+ 
