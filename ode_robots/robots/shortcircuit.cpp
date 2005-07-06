9f0d44eb51ce0df3614c44a29b55f27a54b930ab
martius <martius>
2005-07-06 16:03:37 +0000

    dummy robot that connects motors with sensors

---
+++ ode_robots/robots/shortcircuit.cpp
@@ -0,0 +1,73 @@
+#include <drawstuff/drawstuff.h>
+#include <ode/ode.h>
+#include <assert.h>
+
+#include "simulation.h"
+
+#include "shortcircuit.h"
+
+
+ShortCircuit::ShortCircuit(dWorldID *w, dSpaceID *s, dJointGroupID *c, int sensornumber, int motornumber)
+  : AbstractRobot::AbstractRobot(w, s, c){
+
+  sensorno = sensornumber; 
+  motorno  = motornumber;  
+  motors = (motor*)malloc(motorno * sizeof(motor));
+  for(int i=0; i < motorno; i++){
+    motors[i]=0.0;
+  }
+  
+};
+
+/** sets actual motorcommands
+    @param motors motors scaled to [-1,1] 
+    @param motornumber length of the motor array
+*/
+void ShortCircuit::setMotors(const motor* _motors, int motornumber){
+  assert(motornumber == motorno);
+  memcpy(motors, _motors, sizeof(motor) * motornumber);
+};
+
+/** returns actual sensorvalues
+    @param sensors sensors scaled to [-1,1] (more or less)
+    @param sensornumber length of the sensor array
+    @return number of actually written sensors
+*/
+int ShortCircuit::getSensors(sensor* sensors, int sensornumber){
+  assert(sensornumber == sensorno);  
+  for (int i=0; i<sensorno; i++){
+    sensors[i]=motors[i%motorno];
+  }
+  return sensorno;
+};
+
+/** returns position of robot 
+    @return position robot position in struct Position  
+*/
+Position ShortCircuit::getPosition(){
+  Position pos;
+  pos.x=0;
+  pos.y=0;
+  pos.z=0;
+  return pos;
+};
+
+/** returns a vector with the positions of all segments of the robot
+    @param poslist vector of positions (of all robot segments) 
+    @return length of the list
+*/
+int ShortCircuit::getSegmentsPosition(vector<Position> &poslist){
+  return 0;
+};  
+
+/**
+ * draws the vehicle
+ */
+void ShortCircuit::draw(){};
+
+
+
+
+
+
+
---
+++ ode_robots/robots/shortcircuit.h
@@ -0,0 +1,97 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1  2005-07-06 16:03:37  martius
+ *   dummy robot that connects motors with sensors
+ *
+ ***************************************************************************/
+#ifndef __SHORTCIRCUIT_H
+#define __SHORTCIRCUIT_H
+
+#include <ode/common.h>
+
+#include <vector>
+using namespace std;
+ 
+
+/**
+ * 
+ */
+class ShortCircuit : public AbstractRobot{
+public:
+  /**
+   * Constructor
+   * @param w world in which robot should be created
+   * @param s space in which robot should be created
+   * @param c contactgroup for collision treatment
+   */
+  ShortCircuit(dWorldID *w, dSpaceID *s, dJointGroupID *c,int sensornumber, int motornumber);
+
+  /// draws the robot
+  virtual void draw();
+
+/** sets the vehicle to position pos, sets color to c, and creates robot if necessary
+    @params pos desired position of the robot in struct Position
+    @param c desired color for the robot in struct Color
+*/
+  virtual void place(Position pos , Color *c = 0) {}
+
+  /** returns actual sensorvalues
+      @param sensors sensors scaled to [-1,1] 
+      @param sensornumber length of the sensor array
+      @return number of actually written sensors
+  */
+  virtual int getSensors(sensor* sensors, int sensornumber);
+
+  /** sets actual motorcommands
+      @param motors motors scaled to [-1,1] 
+      @param motornumber length of the motor array
+  */
+  virtual void setMotors(const motor* motors, int motornumber);
+
+  /** returns number of sensors
+  */
+  virtual int getSensorNumber() {return sensorno; }
+
+  /** returns number of motors
+  */
+  virtual int getMotorNumber() {return motorno; }
+
+  /** returns position of robot 
+      @param pos vector of desired position (x,y,z)
+   */
+  virtual Position getPosition();
+
+  /** returns a vector with the positions of all segments of the robot
+      @param vector of positions (of all robot segments) 
+      @return length of the list
+  */
+  virtual int getSegmentsPosition(vector<Position> &poslist);
+
+ protected:
+  int sensorno;      //number of sensors
+  int motorno;       // number of motors
+  motor* motors;
+} ;
+
+#endif
+ 
