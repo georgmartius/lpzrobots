78866a767801ead7dbd57f0bedbf141f9934eeac
fhesse <fhesse>
2005-07-08 10:00:33 +0000

    initial version

---
+++ ode_robots/obstacles/sphere.h
@@ -0,0 +1,131 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1  2005-07-08 10:00:33  fhesse
+ *   initial version
+ *                                                    *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef __SPHERE_H
+#define __SPHERE_H
+
+
+#include <stdio.h>
+#include <math.h>
+#include <drawstuff/drawstuff.h>
+
+#include "abstractobstacle.h"
+
+class Sphere : public AbstractObstacle{
+
+  double radius;
+  double masse;
+  double base_x, base_y, base_z;
+
+  dGeomID geom;
+  dBodyID body;
+
+  bool obstacle_exists;
+
+ public:
+  
+  Sphere(dWorldID *w, dSpaceID *s):
+    AbstractObstacle::AbstractObstacle(w, s){
+
+    base_x=0.0;
+    base_y=0.0;
+    base_z=0.0;
+	
+    radius=0.3;
+
+    masse=1;
+ 
+    obstacle_exists=false;
+    
+    setColor(0,0,1);
+
+  };
+
+  /**
+   * draws the obstacle 
+   */
+  virtual void draw(){
+    //dsSetTexture (DS_WOOD);    
+    dsSetColor (color.r, color.g, color.b);
+    dsDrawSphere(dBodyGetPosition(body),dBodyGetRotation(body),radius );
+  };
+  
+  
+  virtual void setPosition(double x, double y, double z){
+    base_x=x;
+    base_y=y;
+    base_z=z;
+    if (obstacle_exists){
+      destroy();
+    }
+    create();
+  };
+
+  virtual void getPosition(double& x, double& y, double& z){
+    x=base_x;
+    y=base_y;
+    z=base_z;
+  }
+  
+  virtual void setGeometry(double length_, double width_, double height_){
+    radius=length_;
+  };
+
+  virtual void setColor(double r, double g, double b){
+    color.r=r;
+    color.g=g;
+    color.b=b;
+  };
+
+ protected:
+  virtual void create(){
+    dMass m;
+
+
+    body= dBodyCreate(*world);
+    dMassSetSphere(&m,1,radius);
+    dMassAdjust (&m,masse);
+    dBodySetMass (body,&m);
+    dBodySetPosition ( body, base_x, base_y, base_z + radius);
+    printf("z-position=%f\n",base_z);
+    geom = dCreateSphere ( *space, radius );
+    dGeomSetBody (geom, body);
+
+    obstacle_exists=true;
+  };
+
+
+  virtual void destroy(){
+    dGeomDestroy( geom );
+    dBodyDestroy( body );
+    
+    obstacle_exists=false;
+  };
+
+};
+
+#endif
