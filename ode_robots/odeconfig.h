ca1e627624106c83e5d8c37e621cf44ad2ac6d44
martius <martius>
2005-07-29 10:22:04 +0000

    realTimerFactor added drawInterval is automatically set to 25fps

--- ode_robots/odeconfig.h
+++ ode_robots/odeconfig.h
@@ -7,10 +7,11 @@ class OdeConfig : public Configurable {
 public:
   OdeConfig(){
     simStepSize=0.01;
-    drawInterval=1;
     controlInterval=5;
+    realTimeFactor=1.0;
     noise=0.1;
     gravity=-9.81;
+    drawInterval=calcDrawInterval();
     // prepare name;
     strcpy(name,"Simulation Environment: ");
     Configurable::insertCVSInfo(name + strlen(name), "$RCSfile$", "$Revision$");
@@ -21,20 +22,23 @@ public:
   }
 
   virtual int getParamList(paramkey*& keylist,paramval*& vallist) const {
-    int number_params=5;
+    int number_params=6;
+    int i=0;
     keylist=(paramkey*)malloc(sizeof(paramkey)*number_params);
     vallist=(paramval*)malloc(sizeof(paramval)*number_params);
-    keylist[0]="noise";
-    keylist[1]="simstepsize";
-    keylist[2]="drawinterval";
-    keylist[3]="controlinterval";
-    keylist[4]="gravity";
-                    
-    vallist[0]=noise;
-    vallist[1]=simStepSize;
-    vallist[2]=drawInterval;
-    vallist[3]=controlInterval;
-    vallist[4]=gravity;
+    keylist[i++]="noise";
+    keylist[i++]="simstepsize";
+    keylist[i++]="realtimefactor";
+    keylist[i++]="drawinterval";
+    keylist[i++]="controlinterval";
+    keylist[i++]="gravity";
+    i=0;
+    vallist[i++]=noise;
+    vallist[i++]=simStepSize;
+    vallist[i++]=realTimeFactor;
+    vallist[i++]=drawInterval;
+    vallist[i++]=controlInterval;
+    vallist[i++]=gravity;
     return number_params;
   }
 
@@ -42,6 +46,7 @@ public:
     if(!key) return 0.0;
     if(strcmp(key, "noise")==0) return noise; 
     else if(strcmp(key, "simstepsize")==0) return simStepSize; 
+    else if(strcmp(key, "realtimefactor")==0) return realTimeFactor; 
     else if(strcmp(key, "drawinterval")==0) return drawInterval; 
     else if(strcmp(key, "controlinterval")==0) return controlInterval; 
     else if(strcmp(key, "gravity")==0) return gravity; 
@@ -51,7 +56,13 @@ public:
   bool setParam(const paramkey key, const paramval val){
     if(!key) return false;
     if(strcmp(key, "noise")==0) noise = val; 
-    else if(strcmp(key, "simstepsize")==0) simStepSize=val; 
+    else if(strcmp(key, "simstepsize")==0) {
+      simStepSize=max(0.0000001,val); 
+      drawInterval=calcDrawInterval();
+    }else if(strcmp(key, "realtimefactor")==0){
+      realTimeFactor=max(0.0,val); 
+      drawInterval=calcDrawInterval();
+    }
     else if(strcmp(key, "drawinterval")==0) drawInterval=(int)val; 
     else if(strcmp(key, "controlinterval")==0) controlInterval=(int)val; 
     else if(strcmp(key, "gravity")==0) {
@@ -62,8 +73,18 @@ public:
     return true;
   }
 
+private:
+  /// calculates the draw interval with simStepSize and realTimeFactor so that we have 25 frames/sec
+  int calcDrawInterval(){
+    if(realTimeFactor>0 && simStepSize>0){
+      return int(1/(25.0*simStepSize/realTimeFactor));
+    }else return 50;
+  }
+
+
 public:
   double simStepSize;
+  double realTimeFactor;
   int drawInterval;
   int controlInterval;
   double noise;
