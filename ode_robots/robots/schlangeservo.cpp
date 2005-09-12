8e82b62b8ca00357cd7bd87ff34aa4f9e8e1de86
martius <martius>
2005-09-12 00:07:53 +0000

    snake with servos

---
+++ ode_robots/robots/schlangeservo.cpp
@@ -0,0 +1,259 @@
+#include <drawstuff/drawstuff.h>
+#include "simulation.h"
+#include "schlangeservo.h"
+
+SchlangeServo::SchlangeServo ( const ODEHandle& odeHandle, 
+			       const SchlangeServoConf& conf, const char* n) 
+  : AbstractRobot(odeHandle, n) 
+{
+  this->conf = conf;
+  created=0;
+
+  // prepare name;
+  Configurable::insertCVSInfo(name, "$RCSfile$", 
+			      "$Revision$");
+}
+	
+SchlangeServo::~SchlangeServo()
+{
+  if(created) destroy();
+}
+	
+
+void SchlangeServo::place (Position pos, Color *c)
+{
+  
+  pos.z += conf.segmDia/2;
+  if(c)
+    color = *c;
+
+  if (!created){ 
+    create(pos);
+  }
+  else{  
+    Position basepos(dBodyGetPosition( objects[0].body ));
+    Position d = pos - basepos;
+    
+    for ( unsigned int n = 0; n < objects.size(); n++ ){
+      Position npos (dBodyGetPosition( objects[n].body ));
+      npos = npos + d;
+      dBodySetPosition ( objects[n].body , npos.x, npos.y, npos.z );
+    }
+  }  
+}
+
+/**
+ *Draws all elements of the snake.
+ **/
+void SchlangeServo::draw()
+{
+  assert(created);
+  dsSetTexture (texture);
+  dsSetColor ( color.r , color.g , color.b );
+
+  for ( int n = 0; n < conf.segmNumber; n++ ) {
+    dsDrawCappedCylinder ( dGeomGetPosition ( objects[n].geom ) , 
+			   dGeomGetRotation ( objects[n].geom ) , 
+			   conf.segmLength , conf.segmDia );
+  }
+}
+
+
+/**
+ *This is the collision handling function for snake robots.
+ *This overwrides the function collisionCallback of the class robot.
+ *@param data
+ *@param o1 first geometrical object, which has taken part in the collision
+ *@param o2 second geometrical object, which has taken part in the collision
+ *@return true if the collision was threated  by the robot, false if not
+ **/
+bool SchlangeServo::collisionCallback(void *data, dGeomID o1, dGeomID o2)
+{
+  //checks if one of the collision objects is part of the robot
+  if( o1 == (dGeomID)snake_space || o2 == (dGeomID)snake_space ){
+    int i,n;  
+    const int N = 20;
+    dContact contact[N];
+    n = dCollide (o1,o2,N,&contact[0].geom,sizeof(dContact));
+    for (i=0; i<n; i++){
+      contact[i].surface.mode = dContactMu2 | dContactSlip1 | dContactSlip2 |
+	dContactSoftERP | dContactSoftCFM | dContactApprox1;
+      contact[i].surface.slip1 = 0.1;
+      contact[i].surface.slip2 = 0.1;
+      contact[i].surface.mu = conf.frictionGround*10;
+      contact[i].surface.mu2 = conf.frictionGround;
+      contact[i].surface.soft_erp = 0.9;
+      contact[i].surface.soft_cfm = 0.001;
+
+      dJointID c = dJointCreateContact( world, contactgroup, &contact[i]);
+      dJointAttach ( c , dGeomGetBody(contact[i].geom.g1) , dGeomGetBody(contact[i].geom.g2)); 
+    }
+    return true;
+  }
+  return false;
+}
+
+
+/**
+ *Reads the actual motor commands from an array, an sets all motors (forces) of the snake to this values.
+ *It is an linear allocation.
+ *@param motors pointer to the array, motor values are scaled to [-1,1] 
+ *@param motornumber length of the motor array
+ **/
+void SchlangeServo::setMotors ( const motor* motors, int motornumber )
+{
+  assert(created);
+  int len = min(motornumber, (int)servos.size());
+  // controller output as torques 
+  for (int i = 0; i < len; i++){
+    servos[i]->set(motors[i]);
+  }
+}	
+
+/**
+ *Writes the sensor values to an array in the memory.
+ *@param sensor* pointer to the arrays
+
+ *@param sensornumber length of the sensor array
+ *@return number of actually written sensors
+ **/
+int SchlangeServo::getSensors ( sensor* sensors, int sensornumber )
+{
+  assert(created);
+  int len = min(sensornumber, getSensorNumber());
+  
+  for (int n = 0; n < len; n++) {
+    sensors[n] = servos[n]->get();
+  }
+	
+  return len;
+}
+
+
+/** The list of all parameters with there value as allocated lists.
+    @param keylist,vallist will be allocated with malloc (free it after use!)
+    @return length of the lists
+*/
+int SchlangeServo::getParamList(paramkey*& keylist,paramval*& vallist) const{
+  int number_params=1; // don't forget to adapt number params!
+  keylist=(paramkey*)malloc(sizeof(paramkey)*number_params);
+  vallist=(paramval*)malloc(sizeof(paramval)*number_params);
+  keylist[0]="frictionGround";  
+
+  vallist[0]=conf.frictionGround;
+  return number_params;
+}
+
+
+paramval SchlangeServo::getParam(paramkey key) const{
+  if(!key) return 0.0;
+  if(strcmp(key, "frictionGround")==0) return conf.frictionGround; 
+  else  return Configurable::getParam(key) ;
+}
+
+bool SchlangeServo::setParam(paramkey key, paramval val){
+  if(!key) {
+    fprintf(stderr, "%s: empty Key!\n", __FILE__);
+    return false;
+  }
+  if(strcmp(key, "frictionGround")==0) conf.frictionGround = val; 
+  else return Configurable::setParam(key, val);
+  return true;
+}
+
+
+Position SchlangeServo::getPosition(){
+  assert(created);
+  int n = conf.segmNumber/2;
+  return Position(dBodyGetPosition( objects[n].body));
+}
+
+int SchlangeServo::getSegmentsPosition(vector<Position> &poslist){
+  assert(created);
+  for(int n = 0; n < conf.segmNumber; n++){
+    poslist.push_back(Position(dBodyGetPosition( objects[n].body)));
+  }
+  return conf.segmNumber;
+
+}
+
+
+void SchlangeServo::create(const Position& pos){
+  if (created) {
+    destroy();
+  }
+  // create car space and add it to the top level space
+  snake_space = dSimpleSpaceCreate (space);
+  dSpaceSetCleanup (snake_space,0);
+
+  dMass masse;
+  dMatrix3 R;	
+  //*************body definition**************
+	
+  dMassSetCappedCylinderTotal ( &masse , conf.segmMass , 2 , conf.segmLength , conf.segmDia );
+	
+  dRFromAxisAndAngle ( R , 0 , 1 , 0 , M_PI/2 );//rotation of the matrix R by 90 degree
+
+  int half = conf.segmNumber/2;
+  for(int n = 0; n < conf.segmNumber; n++){
+    Object obj;
+    obj.body = dBodyCreate(world);
+    Position p = pos + Position((n-half)*conf.segmLength, 0 , conf.segmDia/2);
+            
+    dBodySetPosition(obj.body, p.x, p.y, p.z);
+
+    dBodySetMass(obj.body, &masse);
+	
+    obj.geom = dCreateCCylinder(snake_space, conf.segmDia, conf.segmLength);
+    dGeomSetBody(obj.geom, obj.body);
+    
+    dGeomSetRotation(obj.geom, R); //includes rotation of the body
+    objects.push_back(obj);
+  }
+
+  //*****************joint definition***********
+  for ( int n = 0; n < conf.segmNumber-1; n++ ) {		
+    dJointID joint = dJointCreateHinge(world, 0);
+    dJointAttach ( joint , objects[n].body , objects[n+1].body );
+
+    Position p1(dBodyGetPosition(objects[n].body));
+    Position p2(dBodyGetPosition(objects[n+1].body));
+    Position anchor = (p1 + p2)*0.5;
+    dJointSetHingeAnchor(joint, anchor.x, anchor.y, anchor.z);
+    
+    dJointSetHingeAxis(joint, 0, 0, 1);
+    
+    // setting stops at universal joints		
+    dJointSetHingeParam(joint, dParamLoStop, -conf.jointLimit);
+    dJointSetHingeParam(joint, dParamHiStop,  conf.jointLimit);
+    
+    // making stops bouncy
+    //    dJointSetUniversalParam ( jointliste.back () , dParamBounce, 0.9 );
+    //    dJointSetUniversalParam ( jointliste.back () , dParamBounce2, 0.9 );
+    joints.push_back(joint); 
+    HingeServo* servo =  new HingeServo(joint, -conf.jointLimit, conf.jointLimit, conf.servoPower);
+    servos.push_back(servo);
+  }	  
+  
+  created = true;
+}
+
+
+/** destroys vehicle and space
+ */
+void SchlangeServo::destroy(){
+  if (created){
+    for (int i=0; i<conf.segmNumber; i++){
+      dBodyDestroy(objects[i].body);
+      dGeomDestroy(objects[i].geom);     
+    }
+    // Todo: delete bumpers
+    dSpaceDestroy(snake_space);
+  }
+  created=false;
+}
+
+/** sets the texture */
+void SchlangeServo::setTexture(int texture){
+  this->texture=texture;
+}
---
+++ ode_robots/robots/schlangeservo.h
@@ -0,0 +1,155 @@
+/************************************************************************/
+/*schlangeforce.h							*/
+/*Schlangenkonstrukt fuer das ODE-Robotersystem des Authors		*/
+/*@author Marcel Kretschmann						*/
+/*@version alpha 0.1							*/
+/*									*/
+/************************************************************************/
+#ifndef __SCHLANGEFORCE_H
+#define __SCHLANGEFORCE_H
+
+#include<vector>
+using namespace std;
+
+#include "abstractrobot.h"
+#include "configurable.h"
+#include "hingeservo.h"
+
+typedef struct {
+public:
+  int segmNumber;     ///  number of snake elements
+  double segmLength;  /// length of one snake element
+  double segmDia;     ///  diameter of a snake element
+  double segmMass;    ///  mass of one snake element
+  double servoPower;  ///  power of the servos
+  double frictionGround; /// friction with ground
+  double jointLimit;     /// maximal angle for the joints (M_PI/2 = 90 degree)
+} SchlangeServoConf;
+
+
+/**
+ * This is a class, which models a snake like robot. 
+ * It consists of a number of equal elements, each linked 
+ * by a joint powered by 2 servos
+ **/
+class SchlangeServo: public AbstractRobot, public Configurable
+{
+private:
+
+  char name[50];
+  bool created;
+
+  vector <Object> objects;
+  vector <dJointID> joints;
+  vector <HingeServo*> servos;
+  dSpaceID snake_space;
+  SchlangeServoConf conf;
+  int texture;
+
+public:
+  SchlangeServo ( const ODEHandle& odeHandle, 
+		  const SchlangeServoConf& conf, const char* name);
+
+  static SchlangeServoConf getDefaultConf(){
+    SchlangeServoConf conf;
+    conf.segmNumber = 10;     ///  number of snake elements
+    conf.segmLength = 0.8;  /// length of one snake element
+    conf.segmDia    = 0.2;     ///  diameter of a snake element
+    conf.segmMass   = 0.4;    ///  mass of one snake element
+    conf.servoPower = 10;  ///  power of the servos
+    conf.frictionGround = 1.0; /// friction with ground
+    conf.jointLimit =  M_PI/4;
+    return conf;
+  }
+
+  /**
+   *Destruktor
+   *@author Marcel Kretschmann
+   *@version beta
+   **/
+  virtual ~SchlangeServo();
+	
+  
+
+  /**sets the robot to position pos, sets color to c, and creates robot if necessary
+   *Only sets the color, there is no special place operation for this unspecified robot.
+   *@param pos new position of the robot
+   *@param c desired color for the robot in struct Color
+   **/
+  virtual void place(Position pos, Color *c);
+
+  /**
+   *draws all geometrical objects
+   **/
+  virtual void draw();
+
+  /**
+   *This is the collision handling function for snake robots.
+   *This overwrides the function collisionCallback of the class robot.
+   *@param data
+   *@param o1 first geometrical object, which has taken part in the collision
+   *@param o2 second geometrical object, which has taken part in the collision
+   *@return true if the collision was threated  by the robot, false if not
+   **/
+  virtual bool collisionCallback(void *data, dGeomID o1, dGeomID o2);	
+
+	
+  /**
+   *Reads the actual motor commands from an array, 
+   *an sets all motors of the snake to this values.
+   *It is an linear allocation.
+   *@param motors pointer to the array, motor values are scaled to [-1,1] 
+   *@param motornumber length of the motor array
+   **/
+  virtual void setMotors ( const motor* motors, int motornumber );
+
+  /**
+   *Writes the sensor values to an array in the memory.
+   *@param sensor* pointer to the array
+   *@param sensornumber length of the sensor array
+   *@return number of actually written sensors
+   **/
+  virtual int getSensors ( sensor* sensors, int sensornumber );
+	
+  /** returns number of sensors
+   */
+  virtual int getSensorNumber() { assert(created); return servos.size(); }
+
+  /** returns number of motors
+   */
+  virtual int getMotorNumber(){ assert(created); return servos.size(); }
+
+  /** returns position of robot 
+      @return position robot position in struct Position  
+  */
+  virtual Position getPosition();
+
+  /** returns a vector with the positions of all segments of the robot
+      @param poslist vector of positions (of all robot segments) 
+      @return length of the list
+  */
+  virtual int getSegmentsPosition(vector<Position> &poslist);
+
+  /// returns the name of the object (with version number)
+  virtual constparamkey getName() const { return name; } 
+
+  /** The list of all parameters with there value as allocated lists.
+      @param keylist,vallist will be allocated with malloc (free it after use!)
+      @return length of the lists
+  */
+  virtual int getParamList(paramkey*& keylist,paramval*& vallist) const;
+
+  virtual paramval getParam(paramkey key) const;
+
+  virtual bool setParam(paramkey key, paramval val);
+
+
+  /** sets the texture */
+  virtual void setTexture(int texture);
+
+private:
+  void create(const Position& pos);
+  void destroy();
+};
+
+#endif
