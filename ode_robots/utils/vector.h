adebae3838b2e1900f12be7cd0dcf019349693e0
robot7 <robot7>
2005-09-12 17:48:45 +0000

    minor changes, added +,- function

--- ode_robots/utils/vector.h
+++ ode_robots/utils/vector.h
@@ -4,11 +4,11 @@
 
 #include <vector>
 #include <math.h>
-#include "exception.h"
+#include "exceptions.h"
 
 
 namespace university_of_leipzig {
-namespace robot {
+namespace robots {
 
 
 template<typename T>
@@ -63,7 +63,7 @@ template <typename T>
 T& Vector<T>::operator() (unsigned i)
 {
   if(i >= data.size())
-    exception::IndexOutOfBounds().raise();
+    IndexOutOfBoundsException().raise();
 
 
   return data[i];
@@ -74,7 +74,7 @@ template <typename T>
 Vector<T> Vector<T>::operator - (const Vector<T> &r_rhs) const
 {
   if(get_dimension() != r_rhs.get_dimension())
-    throw exception::IndexOutOfBounds();
+    IndexOutOfBoundsException().raise();
   
   unsigned n = get_dimension();
 
@@ -85,6 +85,24 @@ Vector<T> Vector<T>::operator - (const Vector<T> &r_rhs) const
 
   return result;
 }
+
+/*
+template <typename T>
+Vector<T> Vector<T>::operator + (const Vector<T> &r_rhs) const
+{
+  if(get_dimension() != r_rhs.get_dimension())
+    IndexOutOfBoundsException().raise();
+  
+  unsigned n = get_dimension();
+
+  Vector result(n);
+
+  for(unsigned i = 0; i < n; ++i)
+    result(i) = (*this)(i) + r_rhs(i);
+
+  return result;
+}
+*/
 
 
 template <typename T>
@@ -111,7 +129,7 @@ template <typename T>
 const T& Vector<T>::operator() (unsigned i) const
 {
   if(i >= data.size())
-    exception::IndexOutOfBounds().raise();
+    IndexOutOfBoundsException().raise();
 
 
   return data[i];
@@ -147,39 +165,62 @@ template<class T> class Vector3 {
   }
 
 
-  Vector3(const T* p) {
+  Vector3(const T* p)
+  {
     memcpy(c, p, sizeof(c));
   }
 
 
   template <class U> Vector3(const Vector3<U> &r_other) :
-    x(static_cast<U>(r_other.x)),
-    y(static_cast<U>(r_other.y)),
-    z(static_cast<U>(r_other.z))
+    x(r_other.x),
+    y(r_other.y),
+    z(r_other.z)
   {
   }
 
 
   template <class U> Vector3<T> operator - (const Vector3<U> &r_rhs) const
   {
-    return Vector3<T>(x - static_cast<T>(r_rhs.x),
-		      y - static_cast<T>(r_rhs.y),
-		      z - static_cast<T>(r_rhs.z));
+    return Vector3<T>(x - r_rhs.x,
+		      y - r_rhs.y,
+		      z - r_rhs.z);
+  }
+
+
+  template <class U> Vector3<T> operator + (const Vector3<U> &r_rhs) const
+  {
+    return Vector3<T>(x + r_rhs.x,
+		      y + r_rhs.y,
+		      z + r_rhs.z);
   }
 
 
   template <class U> Vector3<T> operator / (const U &r_rhs) const
   {
-    return Vector3<T>(x / r_rhs, y / r_rhs, z / r_rhs);
+    return Vector3<T>(x / r_rhs,
+                      y / r_rhs,
+                      z / r_rhs);
   }
 
 
-  Vector3<T>& make_unit_length() 
+
+  Vector3<T> get_unit_vector() const
   {
-    T l = length();
-    if(static_cast<T>(0) == l)
+    double l = length();
+    if(0.0 == l)
       return *this;
+
+    return Vector3<T>(x / l, y / l, z / l);
+  }
+
     
+
+  Vector3<T>& make_unit_length()
+  {
+    double l = length();
+    if(0.0 == l)
+      return *this;
+
     x /= l;
     y /= l;
     z /= l;
@@ -188,16 +229,17 @@ template<class T> class Vector3 {
   }
 
 
-  template <class U> Vector3& operator = (const Vector3<U> &r_rhs)
+  template <class U> Vector3<T>& operator = (const Vector3<U> &r_rhs)
   {
-    x = static_cast<T>(r_rhs.x);
-    y = static_cast<T>(r_rhs.y);
-    z = static_cast<T>(r_rhs.z);
+    x = r_rhs.x;
+    y = r_rhs.y;
+    z = r_rhs.z;
 
     return *this;
   }
 
 
+  /*
   const Vector3& operator = (const Vector3 &r_rhs) {
     x = r_rhs.x;
     y = r_rhs.y;
@@ -205,6 +247,7 @@ template<class T> class Vector3 {
 
     return *this;
   }
+  */
 
   double square_length() const
   {
