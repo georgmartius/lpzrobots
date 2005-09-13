32ac82db57ffd18c2566917f48e38dd4e34e4eb6
martius <martius>
2005-09-13 15:31:55 +0000

    shell script for converting and encoding single frames from capturing mode

---
+++ ode_robots/utils/encodevideo.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+if test -z "$1"; then 
+    echo "USAGE: $0: BaseName";
+    exit 1;
+fi
+
+NAME=$1;
+
+for F in $NAME*.ppm; do echo "convert $F"; convert "$F" "${F%ppm}sgi"; rm "$F"; done
+
+echo -e "***********************\nUse the following for mpeg2 encoding:";
+echo "mencoder mf://$NAME*.sgi -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mpeg2video -oac copy -o $NAME.mpg";
+echo -e "***********************\nUse the following for mpeg4 DIVX encoding:";
+echo "mencoder mf://$NAME*.sgi -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mpeg4 -oac copy -ffourcc DX50 -o $NAME.mpg";
+
+
+
