af63412543ee1f1c3179cc8689f3dfbc8e5f0c1f
martius <martius>
2005-09-14 08:20:30 +0000

    mjpeg cmdline added

--- ode_robots/utils/encodevideo.sh
+++ ode_robots/utils/encodevideo.sh
@@ -9,9 +9,11 @@ NAME=$1;
 
 for F in $NAME*.ppm; do echo "convert $F"; convert "$F" "${F%ppm}sgi"; rm "$F"; done
 
-echo -e "***********************\nUse the following for mpeg2 encoding:";
+echo -e "*********************** mjpeg encoding:";
+echo "mencoder mf://$NAME*.sgi -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mjpeg -oac copy -o $NAME.mjpeg";
+echo -e "*********************** mpeg2 encoding:";
 echo "mencoder mf://$NAME*.sgi -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mpeg2video -oac copy -o $NAME.mpg";
-echo -e "***********************\nUse the following for mpeg4 DIVX encoding:";
+echo -e "*********************** mpeg4 DIVX encoding:";
 echo "mencoder mf://$NAME*.sgi -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mpeg4 -oac copy -ffourcc DX50 -o $NAME.mpg";
 
 
