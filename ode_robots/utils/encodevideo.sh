fa6cbd124fe5ffd82f123a9d4f6c9173fa286d78
martius <martius>
2005-09-19 07:24:55 +0000

    run encoder automatically for mjpeg mpg2 and divX

--- ode_robots/utils/encodevideo.sh
+++ ode_robots/utils/encodevideo.sh
@@ -9,12 +9,12 @@ NAME=$1;
 
 for F in $NAME*.ppm; do echo "convert $F"; convert "$F" "${F%ppm}sgi"; rm "$F"; done
 
-echo -e "*********************** mjpeg encoding:";
-echo "mencoder mf://$NAME*.sgi -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mjpeg -oac copy -o $NAME.mjpeg";
-echo -e "*********************** mpeg2 encoding:";
-echo "mencoder mf://$NAME*.sgi -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mpeg2video -oac copy -o $NAME.mpg";
-echo -e "*********************** mpeg4 DIVX encoding:";
-echo "mencoder mf://$NAME*.sgi -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mpeg4 -oac copy -ffourcc DX50 -o $NAME.mpg";
+echo -e "*********************** mjpeg encoding **************************";
+mencoder mf://$NAME*.sgi -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mjpeg -oac copy -o $NAME.mjpeg
+echo -e "*********************** mpeg2 encoding **************************";
+mencoder mf://$NAME*.sgi -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mpeg2video -oac copy -o $NAME.mpg
+echo -e "*********************** mpeg4 DIVX encoding **************************";
+mencoder mf://$NAME*.sgi -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mpeg4 -oac copy -ffourcc DX50 -o $NAME.avi
 
 
 
