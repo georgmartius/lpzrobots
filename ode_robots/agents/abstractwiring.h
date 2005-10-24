66d737d0ed004b9e0540b9f2021ec45a7148cb44
fhesse <fhesse>
2005-10-24 09:52:36 +0000

    comments in doxygen

--- ode_robots/agents/abstractwiring.h
+++ ode_robots/agents/abstractwiring.h
@@ -20,7 +20,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.6  2005-10-06 17:11:36  martius
+ *   Revision 1.7  2005-10-24 09:52:36  fhesse
+ *   comments in doxygen
+ *
+ *   Revision 1.6  2005/10/06 17:11:36  martius
  *   switched to stl lists
  *
  *   Revision 1.5  2005/08/03 20:34:58  martius
@@ -47,13 +50,15 @@
 #include "noisegenerator.h"
 #include "inspectable.h"
 
-/// Abstract wiring-object between controller and robot. 
-//   Implements wiring of robot sensors to inputs of the controller and
-//   controller outputs to robot motors. 
+/** Abstract wiring-object between controller and robot. 
+ *  Implements wiring of robot sensors to inputs of the controller and
+ *  controller outputs to robot motors. 
+ */
 class AbstractWiring : public Inspectable {
 public:
-  /// constructor
-  // @param noise NoiseGenerator that is used for adding noise to sensor values
+  /** constructor
+   *  @param noise NoiseGenerator that is used for adding noise to sensor values
+   */
   AbstractWiring(NoiseGenerator* noise){
     rsensornumber = 0;
     rmotornumber  = 0;
@@ -62,73 +67,86 @@ public:
     noiseGenerator = noise;
   }
 
+  /** destructor
+   */
   virtual ~AbstractWiring(){
     if(noiseGenerator) delete noiseGenerator;
   }
 
-  /// Initializes the number of sensors and motors from robot, calculate
-  //  number of sensors and motors on controller side.
-  //  Must be overloaded to calculate and provide the appropriate numbers
-  //  controllersensornumber (csensornumber) and controllermotornumber (cmotornumber).
-  //  Also rmotornumber and rsensornumber should be set.
+  /** Initializes the  number of sensors and motors from robot 
+   *  (to be precise the internal parameters rsensornumber and rmotornumber!), 
+   *  calculates the number of sensors and motors on controller side.
+   *  Must be overloaded to calculate and provide the appropriate numbers
+   *  controllersensornumber (csensornumber), controllermotornumber (cmotornumber),
+   *  robotsensornumber (rsensornumber) and robotmotornumber (rmotornumber), 
+   *  @return returns false if error, else true
+   */
   virtual bool init(int robotsensornumber, int robotmotornumber) = 0;
 
-  /// Realizes wiring from robot sensors to controller sensors. 
-  //   Must be overloaded in order to implement the appropriate mapping. 
-  //   @param rsensors pointer to array of sensorvalues from robot 
-  //   @param rsensornumber number of sensors from robot
-  //   @param csensors pointer to array of sensorvalues for controller  
-  //   @param csensornumber number of sensors to controller
-  //   @param noise size of the noise added to the sensors
-  //   @return returns false if error, else true
+  /** Realizes wiring from robot sensors to controller sensors. 
+   *   Must be overloaded in order to implement the appropriate mapping. 
+   *   @param rsensors pointer to array of sensorvalues from robot 
+   *   @param rsensornumber number of sensors from robot
+   *   @param csensors pointer to array of sensorvalues for controller  
+   *   @param csensornumber number of sensors to controller
+   *   @param noise size of the noise added to the sensors
+   *   @return returns false if error, else true
+   */
   virtual bool wireSensors(const sensor* rsensors, int rsensornumber, 
 			   sensor* csensors, int csensornumber,
 			   double noise) = 0;
 
-  /// Realizes wiring from controller motor outputs to robot motors. 
-  //   Must be overloaded in order to implement the appropriate mapping. 
-  //   @param rmotors pointer to array of motorvalues for robot 
-  //   @param rmotornumber number of robot motors 
-  //   @param cmotors pointer to array of motorvalues from controller  
-  //   @param cmotornumber number of motorvalues from controller
-  //   @return returns false if error, else true
+  /** Realizes wiring from controller motor outputs to robot motors. 
+   *   Must be overloaded in order to implement the appropriate mapping. 
+   *   @param rmotors pointer to array of motorvalues for robot 
+   *   @param rmotornumber number of robot motors 
+   *   @param cmotors pointer to array of motorvalues from controller  
+   *   @param cmotornumber number of motorvalues from controller
+   *   @return returns false if error, else true
+   */
   virtual bool wireMotors(motor* rmotors, int rmotornumber,
 			  const motor* cmotors, int cmotornumber)  = 0;
 
   
 
-  /// Returns the number of sensors on robot side.
+  /** Returns the number of sensors on robot side.
+   */
   virtual int getRobotSensornumber(){return rsensornumber;}
 
-  /// Returns the number of motors on robot side.
+  /** Returns the number of motors on robot side.
+   */
   virtual int getRobotMotornumber() {return rmotornumber;}
 
-  /// Returns the number of sensors on controller side.
+  /** Returns the number of sensors on controller side.
+   */
   virtual int getControllerSensornumber(){return csensornumber;}
 
-  /// Returns the number of motors on controller side.
+  /** Returns the number of motors on controller side.
+   */
   virtual int getControllerMotornumber() {return cmotornumber;}
 
-
-  /** The list of the names of all internal parameters given by getInternalParams().
-      @param: keylist (do NOT free it! It is a pointer to an internal structure)
-      @return: length of the lists
+  /** Returns the list of the names of all internal parameters.
    */
   virtual list<iparamkey> getInternalParamNames() { return list<iparamkey>(); };
-  /** The list of the names of all internal parameters given by getInternalParams().
-      @param vallist stores the values of all internal parameters 
-      (in the order given by getInternalParamNames())
-      @param length length of vallist array
-      @return: number of parameters actually written
+
+  /** Returns a list of the values of all internal parameters
+      (in the order given by getInternalParamNames()).
    */
   virtual list<iparamval>  getInternalParams() { return list<iparamval>(); };
 
 
 protected:
-  int rsensornumber;  // number of robot sensors
-  int rmotornumber;   // number of robot motors
-  int csensornumber;  // number of controller sensors
-  int cmotornumber;   // number of controller motors
+  /// number of sensors at robot side
+  int rsensornumber;
+
+  /// number of motors at robot side
+  int rmotornumber;
+
+  /// number of sensors at controller side
+  int csensornumber;
+
+  /// number of motors at controller side
+  int cmotornumber;
 
   NoiseGenerator* noiseGenerator;
 
