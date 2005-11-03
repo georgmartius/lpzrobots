077a01b68aefa003adeee2971f696de934ade199
martius <martius>
2005-11-03 16:10:27 +0000

    added texture coding and singlepot

--- ode_robots/textures/calcheightmaps/calcheightmaps.cpp
+++ ode_robots/textures/calcheightmaps/calcheightmaps.cpp
@@ -3,37 +3,52 @@ using namespace std;
 
 #include "imageppm.h"
 
-
 double sqr(double p) { return p*p;}
 double min(double a,double b) { return a<b ? a : b;}
 double max(double a,double b) { return a>b ? a : b;}
 
-void codeSum(double value, unsigned char* data);
+#define IMAGE_SIZE 1024
+#define TEXTURE_STEPS 8
+#define TEXTURE_MIN 128
+#define TEXTURE_MAX 255
+
+/// texture coding that uses TEXTURE_MIN, TEXTURE_MAX
+void codeGrayTex(double value, unsigned char* data);
+/// heightmap coding where r g b get the same value 
 void codeEqual(double value, unsigned char* data);
+// heightmap coding where r+g+b = heightvalue
+void codeSum(double value, unsigned char* data);
+/// heightmap coding where r<<16+g<<8+b = heightvalue
 void codeLowMidHigh(double value, unsigned char* data);
 
 double calcMacroSpheres(double x, double y){  
   double s=4.4;
   double p1 = sqr((x-0.33)*s)+sqr((y-0.33)*s);
-  double p2 = sqr((x-0.58)*s)+sqr((y-0.36)*s);
+  double p2 = sqr((x-0.54)*s)+sqr((y-0.36)*s);
   double p3 = sqr((x-0.67)*s)+sqr((y-0.67)*s);
 
   return min(1,min(min(p1,p2),p3));
 }
 
+double calcSingleParabel(double x, double y){  
+  double s=2.1;
+  double p1 = sqr((x-0.5)*s)+sqr((y-0.5)*s);
+  return min(1,p1);
+}
+
 /// function that returns an value of the height map at the position (x,y)
 // range: 0<= x < 1; 0<= y < 1; 
 // can return any number. The map is scaled to 0 - 255 later.
-double (*fun)(double, double) = &calcMacroSpheres;
+double (*fun)(double, double) = &calcSingleParabel;
 
 /// function that codes the double value into the r g b values of the given data array
-void (*code)(double value, unsigned char* data) = &codeSum;
+void (*code)(double value, unsigned char* data) = &codeGrayTex;
 
 int main(){
   double value;
   double minimum=1e100;
   double maximum=-1e100;
-  int size=256;
+  int size=IMAGE_SIZE;
   double *array = new double[size*size];
   for(int x=0; x<size; x++){
     for(int y=0; y<size; y++){
@@ -61,6 +76,15 @@ int main(){
   delete[] array;
 
   return 0;
+}
+
+void codeGrayTex(double value, unsigned char* data){
+  // discretised and scaled
+  int v = (int(value*TEXTURE_STEPS))*(TEXTURE_MAX-TEXTURE_MIN)/TEXTURE_STEPS + TEXTURE_MIN;
+
+  data[0] = v;
+  data[1] = v;
+  data[2] = v;  
 }
 
 void codeEqual(double value, unsigned char* data){
