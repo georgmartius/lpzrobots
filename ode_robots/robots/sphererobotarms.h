cc2909ca70212d3e214693a09f04ee936146cef2
martius <martius>
2005-11-04 14:45:18 +0000

    GPL added

--- ode_robots/robots/sphererobotarms.h
+++ ode_robots/robots/sphererobotarms.h
@@ -1,10 +1,35 @@
-/************************************************************************/
-/*shpererobot.h								*/
-/*Schlangenkonstrukt fuer das ODE-Robotersystem des Authors		*/
-/*@author Marcel Kretschmann						*/
-/*@version alpha 0.1							*/
-/*									*/
-/************************************************************************/
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                 *
+ ***************************************************************************
+ *                                                                         *
+ * Spherical Robot inspired by Julius Popp.                                *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.8  2005-11-04 14:45:18  martius
+ *   GPL added
+ *
+ *                                                                 *
+ ***************************************************************************/
+
 #ifndef __SPHEREROBOTARMS_H
 #define __SPHEREROBOTARMS_H
 
