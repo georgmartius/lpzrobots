c1b4a9e143fa12f2e5e4fcc90d4e6906dc5655eb
martius <martius>
2005-11-07 16:55:22 +0000

    *** empty log message ***

--- ode_robots/utils/trackrobots.h
+++ ode_robots/utils/trackrobots.h
@@ -3,33 +3,49 @@
 
 #include <stdio.h>
 
-typedef struct TrackOptions {
-  TrackOptions(){
+class AbstractRobot;
+class Agent;
+
+class TrackRobot {
+public:
+
+  friend class Agent;
+
+  TrackRobot(){
     trackPos = false;
     trackSpeed = false;
     trackOrientation = false;
+    interval = 1;
     file=0;
+    cnt=0;
   }
   /** Set the tracking mode of the simulation environment. 
       If one of the parameters is true the tracking is enabled.
       The tracking is written into a file with the current date and time as name.
    */ 
-  TrackOptions(bool trackPos, bool trackSpeed, bool trackOrientation){
+  TrackRobot(bool trackPos, bool trackSpeed, bool trackOrientation, int interval = 1){
     this->trackPos     = trackPos;
     this->trackSpeed   = trackSpeed;
     this->trackOrientation = trackOrientation;
+    this->interval = interval;
+    file=0;
+    cnt=0;
   }
 
+  bool open(const AbstractRobot* robot);
+  void track(AbstractRobot* robot);
+  void close();
+
+
+private:
   bool trackPos;
   bool trackSpeed;
   bool trackOrientation;
+  int interval;
   FILE* file;
-}TrackOptions;
+  long cnt;
+};
 
-class AbstractRobot;
 
-bool openTrackFile(TrackOptions& trackoptions);
-void trackRobot(AbstractRobot* robot, const TrackOptions& trackoptions);
-void closeTrackFile(TrackOptions& trackoptions);
 
 #endif
