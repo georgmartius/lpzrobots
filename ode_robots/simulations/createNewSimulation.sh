05b43250e977fe5b08591e250fecfe43e65f0bfd
martius <martius>
2005-11-09 14:49:05 +0000

    changed comment to Makefile.conf

--- ode_robots/simulations/createNewSimulation.sh
+++ ode_robots/simulations/createNewSimulation.sh
@@ -12,5 +12,5 @@ make -s -C "$2" clean;
 echo "Created a new subdirectory $2";
 echo -e "Please call\n cvs add $2\n cvs add $2/*"
 echo "in order to add the directory to the cvs repository.";
-echo "Please add $2 to the SUBDIRECTORIES variable in the Makefile placed in this directory.";
+echo "Please add $2 to the SIMULATIONS variable in the Makefile.conf placed in this directory.";
 echo "    Have Fun!";
