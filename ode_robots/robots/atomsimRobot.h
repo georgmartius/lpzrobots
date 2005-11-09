9b3e923570973100e69ebb6107d21f0fd34730d8
martius <martius>
2005-11-09 13:24:42 +0000

    added GPL

--- ode_robots/robots/atomsimRobot.h
+++ ode_robots/robots/atomsimRobot.h
@@ -5,6 +5,38 @@
 /*@version alpha 0.1							*/
 /*									*/
 /************************************************************************/
+/************************************************************************/
+/*atomsimAtom.h								*/
+/*atom modell which tries to repesent some aspekts of molecular atoms	*/
+/*@author Marcel Kretschmann						*/
+/*@version alpha 0.1							*/
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.8  2005-11-09 13:24:42  martius
+ *   added GPL
+ *
+ *                                                                 *
+ ***************************************************************************/
 #ifndef __ATOMSIMROBOT_H
 #define __ATOMSIMROBOT_H
 
