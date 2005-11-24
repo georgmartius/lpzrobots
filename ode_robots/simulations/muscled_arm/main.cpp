91c147c55fe7cf96ae7324dfcc9fe0c3a0779b12
fhesse <fhesse>
2005-11-24 16:16:40 +0000

    moved from main branch

--- ode_robots/simulations/muscled_arm/main.cpp
+++ ode_robots/simulations/muscled_arm/main.cpp
@@ -20,8 +20,11 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.1.4.1  2005-11-15 12:29:48  martius
- *   new selforg structure and OdeAgent, OdeRobot ...
+ *   Revision 1.1.4.2  2005-11-24 16:16:40  fhesse
+ *   moved from main branch
+ *
+ *   Revision 1.2  2005/11/17 16:28:40  fhesse
+ *   initial version
  *
  *   Revision 1.1  2005/11/11 15:35:54  fhesse
  *   preinitial version
@@ -57,15 +60,17 @@
 
 // used arena
 #include "playground.h"
+#include "sphere.h"
 
 // used controller
 #include <selforg/invertnchannelcontroller.h>
-
-// plotoptions is set to NoPlot at the beginning, 
-// means no online gnuplot windows and no logging to file
-// can be changed with commandline options, see main() at the bottom of this file
+#include <selforg/invertmotornstep.h>
+#include <selforg/sinecontroller.h>
+#include <selforg/invertnchannelcontroller.h>
+// plotoptions is a list of possible online output, 
+// if the list is empty no online gnuplot windows and no logging to file occurs.
+// The list is modified with commandline options, see main() at the bottom of this file
 list<PlotOption> plotoptions;
-
 
 // starting function (executed once at the beginning of the simulation loop)
 void start(const OdeHandle& odeHandle, GlobalData& global) 
@@ -75,8 +80,8 @@ void start(const OdeHandle& odeHandle, GlobalData& global)
   dsPrint ( "Press Ctrl-C for an basic commandline interface.\n\n" );
 
   // initial camera position and viewpoint
-  float KameraXYZ[3]= {2.1640f,-1.3079f,1.7600f};
-  float KameraViewXYZ[3] = {125.5000f,-17.0000f,0.0000f};;
+  float KameraXYZ[3]= {0.340f,-0.0779f,1.51f};
+  float KameraViewXYZ[3] = {179.90f,-97.69f,0.0000f};;
   dsSetViewpoint ( KameraXYZ , KameraViewXYZ );
 
   // quality in which spheres are drawn
@@ -100,17 +105,32 @@ void start(const OdeHandle& odeHandle, GlobalData& global)
   playground->setPosition(0,0,0); // playground positionieren und generieren
   global.obstacles.push_back(playground);
 
+
+  Sphere* sphere =new Sphere(odeHandle);
+  sphere->setGeometry(0.2,0,0);
+  sphere->setPosition(0,1,0); 
+  global.obstacles.push_back(sphere);
   
   MuscledArmConf  conf = MuscledArm::getDefaultConf();
-  //conf.includeMuscles=false;
-  //conf.drawMuscles=false;
+  conf.drawSphere=true;
+  conf.strained=false;
+  conf.jointAngleSensors=false;
+  conf.jointAngleRateSensors=false;
+  conf.MuscleLengthSensors=true;
   MuscledArm* arm = new MuscledArm(odeHandle, conf);
-  //arm->setTextures(DS_WOOD, chessTexture); 
+  arm->setParam("damping",20);
+  arm->setParam("factorMotors",5);
   arm->place(Position(0,0,0));
+  global.configs.push_back(arm);
 
   // create pointer to controller
   // push controller in global list of configurables
-  AbstractController *controller = new InvertNChannelController(10);  
+  //  InvertMotorNStep(int buffersize, double cInit = 0.1, bool useS = false, bool someInternalParams = true);
+
+  InvertMotorNStepConf cc = InvertMotorNStep::getDefaultConf();
+  cc.buffersize=30;
+  AbstractController *controller = new InvertMotorNStep(cc);
+  //AbstractController *controller = new   SineController();
   global.configs.push_back(controller);
   
   // create pointer to one2onewiring
@@ -121,7 +141,12 @@ void start(const OdeHandle& odeHandle, GlobalData& global)
   // push agent in globel list of agents
   OdeAgent* agent = new OdeAgent(plotoptions);
   agent->init(controller, arm, wiring);
+  //  agent->setTrackOptions(TrackRobot(true, false, false,50));
   global.agents.push_back(agent);
+
+  // switch off gravity
+  global.odeConfig.setParam("gravity",0);
+  global.odeConfig.setParam("realtimefactor",0);
 
   // show (print to console) params of all objects in configurable list 
   showParams(global.configs);
@@ -177,4 +202,88 @@ int main (int argc, char **argv)
   simulation_close();  
   return 0;
 }
- 
+
+/*
+ [Simulation Environment:  odeconfig.h,v - 1.12 ][1804289383]
+ noise=              0.100000
+ simstepsize=        0.010000
+ realtimefactor=     0.000000
+ drawinterval=       50.000000
+ controlinterval=    5.000000
+ gravity=            0.000000
+ [muscledarm.cpp,v - 1.2 ][2031419189]
+ factorMotors=       0.100000
+ factorSensors=      4.000000
+ damping=            1.000000
+ [invertmotornstep.cpp,v - 1.19 ][240828263]
+ epsA=               0.001299
+ epsC=               0.000000
+ adaptrate=          0.010000
+ nomupdate=          0.010000
+ desens=             0.000000
+ s4delay=            1.000000
+ s4avg=              5.000000
+ steps=              4.000000
+ zetaupdate=         0.000000
+ logaE=              0.000000
+ rootE=              0.000000
+ relativeE=          0.000000
+ factorB=            0.200000
+ noiseB=             0.001000
+*/
+
+/*
+[Simulation Environment:  odeconfig.h,v - 1.12 ][1804289383]
+ noise=              0.000000
+ simstepsize=        0.010000
+ realtimefactor=     0.000000
+ drawinterval=       50.000000
+ controlinterval=    5.000000
+ gravity=            0.000000
+[ muscledarm.cpp,v - 1.2 ][715246296]
+ factorMotors=       0.000000
+ factorSensors=      10.000000
+ damping=            10.000000
+[ sinecontroller.hpp,v - 1.4 ][394186711]
+ sinerate=           400.000000
+ phaseshift=         1.000000
+*/
+
+
+
+/*
+
+Use random number seed: 1132245941
+
+[Simulation Environment:  odeconfig.h,v - 1.12 ][1804289383]
+ noise=              0.100000
+ simstepsize=        0.010000
+ realtimefactor=     0.000000
+ drawinterval=       50.000000
+ controlinterval=    5.000000
+ gravity=            0.000000
+[ muscledarm.cpp,v - 1.3 ][1577223465]
+ factorMotors=       2.000000
+ factorSensors=      4.000000
+ damping=            20.000000
+[ invertmotornstep.cpp,v - 1.19 ][686205722]
+ epsA=               0.005501
+ epsC=               0.000000
+ adaptrate=          0.010000
+ nomupdate=          0.010000
+ desens=             0.000000
+ s4delay=            1.000000
+ s4avg=              1.000000
+ steps=              3.000000
+ zetaupdate=         0.000000
+ logaE=              0.000000
+ rootE=              0.000000
+ relativeE=          0.000000
+ factorB=            0.200000
+ noiseB=             0.001000
+
+
+
+
+*/
+
