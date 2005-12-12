7a2608352d3d38c29781a64608589b2acea91c65
martius <martius>
2005-12-12 23:42:14 +0000

    Pos is a class againt to have customisable Constructors

--- ode_robots/utils/pos.h
+++ ode_robots/utils/pos.h
@@ -20,7 +20,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.1.2.1  2005-12-11 23:35:08  martius
+ *   Revision 1.1.2.2  2005-12-12 23:42:14  martius
+ *   Pos is a class againt to have customisable Constructors
+ *
+ *   Revision 1.1.2.1  2005/12/11 23:35:08  martius
  *   *** empty log message ***
  *
  *
@@ -28,11 +31,17 @@
 #ifndef __POS_H
 #define __POS_H
 
-#include<osg/Vec3>
+#include <osg/Vec3>
+#include <ode/ode.h>
 
 namespace lpzrobots{
 
-  typedef osg::Vec3 Pos;
+  class Pos : public osg::Vec3 {
+  public:
+    Pos () : osg::Vec3 () {};
+    Pos (float x, float y, float z) : osg::Vec3(x, y, z) {}
+    Pos (const dReal v[3]) : osg::Vec3(v[0], v[1], v[2]) {}
+  };
 
 }
 
