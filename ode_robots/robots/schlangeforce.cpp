36d5f172bd6ff71d67afe613b318c90a60af34c7
martius <martius>
2005-12-12 22:36:28 +0000

    indentation

--- ode_robots/robots/schlangeforce.cpp
+++ ode_robots/robots/schlangeforce.cpp
@@ -20,7 +20,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.12  2005-11-09 13:24:42  martius
+ *   Revision 1.12.4.1  2005-12-12 22:36:28  martius
+ *   indentation
+ *
+ *   Revision 1.12  2005/11/09 13:24:42  martius
  *   added GPL
  *
  ***************************************************************************/
@@ -45,10 +48,10 @@ SchlangeForce::SchlangeForce ( int startRoboterID , const OdeHandle& odeHandle,
 			       const SchlangenConf& conf ) 
   :Schlange ( startRoboterID , odeHandle, conf ) 
 {
-        // prepare name;
-        Configurable::insertCVSInfo(name, "$RCSfile$", 
-		           	            "$Revision$");
-	gamma=0.4;
+  // prepare name;
+  Configurable::insertCVSInfo(name, "$RCSfile$", 
+			      "$Revision$");
+  gamma=0.4;
 }
 	
 /**
@@ -73,52 +76,52 @@ SchlangeForce::~SchlangeForce()
  *@author Marcel Kretschmann
  *@version beta
  **/
- void SchlangeForce::doInternalStuff(const GlobalData& global){}
+void SchlangeForce::doInternalStuff(const GlobalData& global){}
 bool SchlangeForce::collisionCallback(void *data, dGeomID o1, dGeomID o2)
 {
-	//checks if one of the collision objects is part of the robot
-	bool tmp_kollisionsbeteiligung = false;
-	for ( int n = 0; n < getObjektAnzahl (); n++ )
+  //checks if one of the collision objects is part of the robot
+  bool tmp_kollisionsbeteiligung = false;
+  for ( int n = 0; n < getObjektAnzahl (); n++ )
+    {
+      if ( getObjektAt ( n ).geom == o1 || getObjektAt ( n ).geom == o2 )
 	{
-		if ( getObjektAt ( n ).geom == o1 || getObjektAt ( n ).geom == o2 )
-		{
-			tmp_kollisionsbeteiligung = true;
-			break;
-		}
+	  tmp_kollisionsbeteiligung = true;
+	  break;
 	}
+    }
  
-	if ( tmp_kollisionsbeteiligung == true )		
+  if ( tmp_kollisionsbeteiligung == true )		
+    {
+      int i,n;
+      const int N = 10;
+      dContact contact[N];
+      bool kollission = false;
+
+      //tests, if a special collision should not be threated
+      if ( SchlangeForce::kollisionsermittlung ( o1 , o2 ) == true )
+	kollission = true;
+
+      if ( kollission == false )
 	{
-		int i,n;
-		const int N = 10;
-		dContact contact[N];
-		bool kollission = false;
+	  n = dCollide (o1,o2,N,&contact[0].geom,sizeof(dContact));
+	  if (n > 0)
+	    for (i=0; i<n; i++)
+	      {
+		contact[i].surface.mode = dContactSlip1 | dContactSlip2 |
+		  dContactSoftERP | dContactSoftCFM | dContactApprox1;
+		contact[i].surface.mu = conf.frictionGround;
+		contact[i].surface.slip1 = 0.005;
+		contact[i].surface.slip2 = 0.005;
+		contact[i].surface.soft_erp = 1;
+		contact[i].surface.soft_cfm = 0.00001;
 
-		//tests, if a special collision should not be threated
-		if ( SchlangeForce::kollisionsermittlung ( o1 , o2 ) == true )
-			kollission = true;
-
-		if ( kollission == false )
-		{
-			n = dCollide (o1,o2,N,&contact[0].geom,sizeof(dContact));
-			if (n > 0)
-				for (i=0; i<n; i++)
-				{
-					contact[i].surface.mode = dContactSlip1 | dContactSlip2 |
-					dContactSoftERP | dContactSoftCFM | dContactApprox1;
-					contact[i].surface.mu = conf.frictionGround;
-					contact[i].surface.slip1 = 0.005;
-					contact[i].surface.slip2 = 0.005;
-					contact[i].surface.soft_erp = 1;
-					contact[i].surface.soft_cfm = 0.00001;
-
-					dJointID c = dJointCreateContact ( world , contactgroup , &contact[i] );
-					dJointAttach ( c , dGeomGetBody(contact[i].geom.g1) , dGeomGetBody(contact[i].geom.g2)) ;
-				}
-		}
-		return true; //if collision was threated by this robot
+		dJointID c = dJointCreateContact ( world , contactgroup , &contact[i] );
+		dJointAttach ( c , dGeomGetBody(contact[i].geom.g1) , dGeomGetBody(contact[i].geom.g2)) ;
+	      }
 	}
-	else return false; //if collision was not threated by this robot
+      return true; //if collision was threated by this robot
+    }
+  else return false; //if collision was not threated by this robot
 }
 
 
@@ -137,22 +140,22 @@ void SchlangeForce::setMotors ( const motor* motors, int motornumber )
   for (int i=0; i<motornumber/2; i++){
     dJointAddUniversalTorques( jointliste[i],conf.factorForce* motors[2*i],
 			       conf.factorForce*motors[2*i +1]);  // motorcommand
-  dJointAddUniversalTorques( jointliste[i], -(gamma)*dJointGetUniversalAngle1Rate(jointliste[i]),
+    dJointAddUniversalTorques( jointliste[i], -(gamma)*dJointGetUniversalAngle1Rate(jointliste[i]),
 			       -(gamma) *dJointGetUniversalAngle2Rate(jointliste[i]) ); // friction
   }
 
 
   /*  // controller outputs as wheel velocity
-  for ( int n = 0; n < motornumber; n++ )
-    if ( n % 2 == 0 )
+      for ( int n = 0; n < motornumber; n++ )
+      if ( n % 2 == 0 )
       {
-	dJointSetUniversalParam ( getJointAt(n/2) , dParamVel , *(motors++)*geschwindigkeitsfaktor );
-	dJointSetUniversalParam ( jointliste[n/2] , dParamFMax , maxmotorkraft );
+      dJointSetUniversalParam ( getJointAt(n/2) , dParamVel , *(motors++)*geschwindigkeitsfaktor );
+      dJointSetUniversalParam ( jointliste[n/2] , dParamFMax , maxmotorkraft );
       }
-    else
+      else
       {
-	dJointSetUniversalParam ( jointliste[n/2] , dParamVel2 , *(motors++)*geschwindigkeitsfaktor );
-	dJointSetUniversalParam ( jointliste[n/2] , dParamFMax2 , maxmotorkraft );
+      dJointSetUniversalParam ( jointliste[n/2] , dParamVel2 , *(motors++)*geschwindigkeitsfaktor );
+      dJointSetUniversalParam ( jointliste[n/2] , dParamFMax2 , maxmotorkraft );
       }
   */
 }	
@@ -168,19 +171,19 @@ void SchlangeForce::setMotors ( const motor* motors, int motornumber )
  **/
 int SchlangeForce::getSensors ( sensor* sensors, int sensornumber )
 {
-	sensoraktualisierung ();
-	for ( int n = 0; n < sensornumber; n++ )
-	{
-		if ( conf.ausgabeArt == angle )
-			*sensors = sensorfeld[n].istwinkel/(2*M_PI);
-		if ( conf.ausgabeArt == anglerate )
-			getWinkelDifferenz ( n , sensors );
-		*sensors *= conf.factorSensors;
-		//		*sensors = 3*tanh((1/3) * (*sensors));  // keep sensorvalues in the range [-3,3]
-		sensors++;
-	}
+  sensoraktualisierung ();
+  for ( int n = 0; n < sensornumber; n++ )
+    {
+      if ( conf.ausgabeArt == angle )
+	*sensors = sensorfeld[n].istwinkel/(2*M_PI);
+      if ( conf.ausgabeArt == anglerate )
+	getWinkelDifferenz ( n , sensors );
+      *sensors *= conf.factorSensors;
+      //		*sensors = 3*tanh((1/3) * (*sensors));  // keep sensorvalues in the range [-3,3]
+      sensors++;
+    }
 	
-	return getSensorfeldGroesse (); //es sind immer alle Sensorwerte durchgeschrieben, da  alle in einem Schritt aktualisiert werden
+  return getSensorfeldGroesse (); //es sind immer alle Sensorwerte durchgeschrieben, da  alle in einem Schritt aktualisiert werden
 }
 
 
