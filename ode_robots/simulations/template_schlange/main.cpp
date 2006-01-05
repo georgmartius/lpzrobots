bc6bb80282c6baf4eefc461615d2f0154c8824e5
fhesse <fhesse>
2006-01-05 11:41:01 +0000

    moved to osg

--- ode_robots/simulations/template_schlange/main.cpp
+++ ode_robots/simulations/template_schlange/main.cpp
@@ -20,107 +20,107 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.16.4.1  2005-11-15 12:30:09  martius
- *   new selforg structure and OdeAgent, OdeRobot ...
- *
- *   Revision 1.16  2005/11/09 14:34:14  fhesse
- *   snake 2 working
- *
- *   Revision 1.15  2005/11/09 13:41:25  martius
- *   GPL'ised
- *
+ *   Revision 1.16.4.2  2006-01-05 11:41:01  fhesse
+ *   moved to osg
+ *                                                    *
+ *                                                                         *
  ***************************************************************************/
-#include <stdio.h>
-#include <drawstuff/drawstuff.h>
-#include <ode/ode.h>
+
+
+#include <selforg/noisegenerator.h>
 
 #include "simulation.h"
-#include <selforg/noisegenerator.h>
 #include "odeagent.h"
 #include <selforg/one2onewiring.h>
+#include <selforg/derivativewiring.h>
 #include "playground.h"
+#include "passivesphere.h"
 
 #include <selforg/invertnchannelcontroller.h>
-#include <selforg/noisegenerator.h>
+#include <selforg/invertmotorspace.h>
+#include <selforg/invertmotornstep.h>
+#include <selforg/sinecontroller.h>
 
-#include "schlange.h"
+#include "schlangeservo.h"
+#include "schlangeforce.h"
+#include "schlangevelocity.h"
 
-ConfigList configs;
 list<PlotOption> plotoptions;
 
-//Startfunktion die am Anfang der Simulationsschleife, einmal ausgefuehrt wird
-void start(const OdeHandle& odeHandle, GlobalData& global) 
-{
-  dsPrint ( "\nWelcome to the virtual ODE - robot simulator of the Robot Group Leipzig\n" );
-  dsPrint ( "------------------------------------------------------------------------\n" );
-  dsPrint ( "Press Ctrl-C for an basic commandline interface.\n\n" );
+using namespace lpzrobots;
 
-  //Anfangskameraposition und Punkt auf den die Kamera blickt
-  float KameraXYZ[3]= {2.1640f,-1.3079f,1.7600f};
-  float KameraViewXYZ[3] = {125.5000f,-17.0000f,0.0000f};;
-  dsSetViewpoint ( KameraXYZ , KameraViewXYZ );
-  dsSetSphereQuality (2); //Qualitaet in der Sphaeren gezeichnet werden
+class ThisSim : public Simulation {
+public:
+	
 
-  // initialization
-  global.odeConfig.noise=0.1;
-    
-  Playground* playground = new Playground(odeHandle);
-  playground->setGeometry(7.0, 0.2, 1.5);
-  playground->setPosition(0,0,0); // playground positionieren und generieren
-  global.obstacles.push_back(playground);
-    
-  // snake1
-  SchlangenConf conf = Schlange::getStandartConf();  
-  Schlange* schlange1 = new Schlange ( 1 , odeHandle, conf);
-  Color col(0,0.5,0.8);
-  schlange1->place(Position(0,0,0),&col);
-  AbstractController *controller = new InvertNChannelController(10);  
+  /// start() is called at the start and should create all the object (obstacles, agents...).
+  virtual void start(const OdeHandle& odeHandle, const OsgHandle& osgHandle, GlobalData& global){
+    setCameraHomePos(Pos(-10.7854, -7.41751, 5.92078),  Pos(-50.6311, -5.00218, 0));
+
+    // conf for schlange1 and schlange2
+    SchlangeConf conf = Schlange::getDefaultConf();
+    conf.motorPower=0.3;
+    conf.jointLimit=conf.jointLimit*3;
+    conf.segmDia    = 0.2;
+    conf.segmNumber = 2; 
+
+
+//     // SchlangeServo
+//     SchlangeServo* schlange1 = 
+//       new SchlangeServo ( odeHandle, osgHandle.changeColor(Color(0.8, 0.3, 0.5)),
+//   			  conf, "Servo");
+//     ((OdeRobot*)schlange1)->place(Pos(2,2,0)); 
+//     AbstractController *controller1 = new SineController();  
+//     AbstractWiring* wiring1 = new One2OneWiring(new ColorUniformNoise(0.1));
+//     OdeAgent* agent1 = new OdeAgent(plotoptions);
+//     agent1->init(controller1, schlange1, wiring1);
+//     global.agents.push_back(agent1);
+//     global.configs.push_back(controller1);
+//     global.configs.push_back(schlange1);
+
+
+    //SchlangeForce
+     SchlangeForce* schlange2 = 
+       new SchlangeForce ( odeHandle, osgHandle.changeColor(Color(0.8, 0.3, 0.5)),
+ 			  conf, "Force");
+     ((OdeRobot*)schlange2)->place(Pos(3,3,0)); 
+     AbstractController *controller2 = new SineController();  
+     AbstractWiring* wiring2 = new One2OneWiring(new ColorUniformNoise(0.1));
+     OdeAgent* agent2 = new OdeAgent(plotoptions);
+     agent2->init(controller2, schlange2, wiring2);
+     global.agents.push_back(agent2);
+     global.configs.push_back(controller2);
+     global.configs.push_back(schlange2);
+
+
+    //SchlangeVelocity
+//      SchlangeConf conf3 = SchlangeVelocity::getDefaultConf();
+//      conf3.jointLimit=conf3.jointLimit*3;
+//      conf3.segmDia    = 0.2;
+//      conf3.segmNumber = 2;    
+//      SchlangeVelocity* schlange3 = 
+//        new SchlangeVelocity ( odeHandle, osgHandle.changeColor(Color(0.8, 0.3, 0.5)),
+//  			     conf3, "Velocity");
+//      ((OdeRobot*)schlange3)->place(Pos(2,2,0)); 
+//      AbstractController *controller3 = new SineController();  
+//      AbstractWiring* wiring3 = new One2OneWiring(new ColorUniformNoise(0.1));
+//      OdeAgent* agent3 = new OdeAgent(plotoptions);
+//      agent3->init(controller3, schlange3, wiring3);
+//      global.agents.push_back(agent3);
+//      global.configs.push_back(controller3);
+//      global.configs.push_back(schlange3);
   
-  One2OneWiring* wiring = new One2OneWiring(new ColorUniformNoise());
-  OdeAgent* agent = new OdeAgent(plotoptions);
-  agent->init(controller, schlange1, wiring);
-  global.agents.push_back(agent);
-  configs.push_back(controller);
-  
-  //snake2
-  Schlange* schlange2 = new Schlange ( 2 , odeHandle, conf);
-  Position p2 (0,2,0);
-  Color col2 (0.5,0,0.5);
-  schlange2->place(p2,&col2);
-  AbstractController *controller2 = new InvertNChannelController(10);  
-  
-  One2OneWiring* wiring2 = new One2OneWiring(new ColorUniformNoise());
-  OdeAgent* agent2 = new OdeAgent(NoPlot);
-  agent2->init(controller2, schlange2, wiring2);
-  global.agents.push_back(agent2);
-  configs.push_back(controller2);
  
-    
-  showParams(configs);
-}
+//     global.odeConfig.setParam("controlinterval",5);
+//     global.odeConfig.setParam("gravity", -9.81); 
 
-void end(GlobalData& global){
-  for(ObstacleList::iterator i=global.obstacles.begin(); i != global.obstacles.end(); i++){
-    delete (*i);
+    showParams(global.configs);
   }
-  global.obstacles.clear();
-  for(OdeAgentList::iterator i=global.agents.begin(); i != global.agents.end(); i++){
-    delete (*i)->getRobot();
-    delete (*i)->getController();
-    delete (*i);
-  }
-  global.agents.clear();
-}
 
-
-// this function is called if the user pressed Ctrl-C
-void config(GlobalData& global){
-  changeParams(configs);
-}
+};
 
 void printUsage(const char* progname){
   printf("Usage: %s [-g] [-l]\n\t-g\tuse guilogger\n\t-l\tuse guilogger with logfile", progname);
-  exit(0);
 }
 
 int main (int argc, char **argv)
@@ -129,12 +129,10 @@ int main (int argc, char **argv)
   if(contains(argv, argc, "-l")) plotoptions.push_back(PlotOption(GuiLogger_File));
   if(contains(argv, argc, "-h")) printUsage(argv[0]);
 
-  // initialise the simulation and provide the start, end, and config-function
-  simulation_init(&start, &end, &config);
-  // start the simulation (returns, if the user closes the simulation)
-  simulation_start(argc, argv);
-  simulation_close();  // tidy up.
-  return 0;
+  ThisSim sim;
+  return sim.run(argc, argv) ? 0 : 1;
 }
 
+
  
+  
