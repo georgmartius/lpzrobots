bebf84060cdbddc409401c592eb092f8d41b1320
fhesse <fhesse>
2006-01-10 09:36:38 +0000

    moved to osg

--- ode_robots/simulations/muscled_arm/main.cpp
+++ ode_robots/simulations/muscled_arm/main.cpp
@@ -20,7 +20,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.1.4.3  2005-12-16 16:32:32  fhesse
+ *   Revision 1.1.4.4  2006-01-10 09:36:38  fhesse
+ *   moved to osg
+ *
+ *   Revision 1.1.4.3  2005/12/16 16:32:32  fhesse
  *   command function for manual control added
  *
  *   Revision 1.1.4.2  2005/11/24 16:16:40  fhesse
@@ -39,12 +42,6 @@
 #include <stdio.h>
 #include <iostream>
 
-// include drawstuff library
-#include <drawstuff/drawstuff.h>
-
-// include ode library
-#include <ode/ode.h>
-
 // include noisegenerator (used for adding noise to sensorvalues)
 #include <selforg/noisegenerator.h>
 
@@ -58,13 +55,13 @@
 #include <selforg/one2onewiring.h>
 
 // used robot
-#include "nimm4.h"
+//#include "nimm4.h"
 #include "arm2segm.h"
 #include "muscledarm.h"
 
 // used arena
 #include "playground.h"
-#include "sphere.h"
+#include "passivesphere.h"
 
 // used controller
 #include <selforg/invertnchannelcontroller.h>
@@ -75,166 +72,143 @@
 // if the list is empty no online gnuplot windows and no logging to file occurs.
 // The list is modified with commandline options, see main() at the bottom of this file
 list<PlotOption> plotoptions;
+
+
+using namespace lpzrobots;
+
 MuscledArm* arm;
 
-// starting function (executed once at the beginning of the simulation loop)
-void start(const OdeHandle& odeHandle, GlobalData& global) 
-{
-  dsPrint ( "\nWelcome to the virtual ODE - robot simulator of the Robot Group Leipzig\n" );
-  dsPrint ( "------------------------------------------------------------------------\n" );
-  dsPrint ( "Press Ctrl-C for an basic commandline interface.\n\n" );
-
-  // initial camera position and viewpoint
-  float KameraXYZ[3]= {0.340f,-0.0779f,1.51f};
-  float KameraViewXYZ[3] = {179.90f,-97.69f,0.0000f};;
-  dsSetViewpoint ( KameraXYZ , KameraViewXYZ );
-
-  // quality in which spheres are drawn
-  dsSetSphereQuality (2); 
-
-  // initialization
-  // - set noise to 0.1
-  // - register file chess.ppm as a texture called chessTexture (used for the wheels)
-  global.odeConfig.noise=0.1;
-  int chessTexture = dsRegisterTexture("chess.ppm");
-
-  // use Playground as boundary:
-  // - create pointer to playground (odeHandle contains things like world and space the 
-  //   playground should be created in; odeHandle is generated in simulation.cpp)
-  // - setting geometry for each wall of playground: 
-  //   setGeometry(double length, double width, double	height)
-  // - setting initial position of the playground: setPosition(double x, double y, double z)
-  // - push playground in the global list of obstacles(globla list comes from simulation.cpp)
-  Playground* playground = new Playground(odeHandle);
-  playground->setGeometry(7.0, 0.2, 1.5);
-  playground->setPosition(0,0,0); // playground positionieren und generieren
-  global.obstacles.push_back(playground);
+class ThisSim : public Simulation {
+public:
 
 
-  Sphere* sphere =new Sphere(odeHandle);
-  sphere->setGeometry(0.2,0,0);
-  sphere->setPosition(0,1,0); 
-  global.obstacles.push_back(sphere);
+  /// start() is called at the start and should create all the object (obstacles, agents...).
+  virtual void start(const OdeHandle& odeHandle, const OsgHandle& osgHandle, GlobalData& global)
+  {
+    // initial camera position and viewpoint
+    setCameraHomePos(Pos(0.340f,-0.0779f,1.51f), Pos(179.9, -97.69, 0));
+
+
+    // initialization
+    // - set noise to 0.1
+    // - register file chess.ppm as a texture called chessTexture (used for the wheels)
+    global.odeConfig.noise=0.1;
+
+
+    // use Playground as boundary:
+    // - create pointer to playground (odeHandle contains things like world and space the 
+    //   playground should be created in; odeHandle is generated in simulation.cpp)
+    // - setting geometry for each wall of playground: 
+    //   setGeometry(double length, double width, double	height)
+    // - setting initial position of the playground: setPosition(double x, double y, double z)
+    // - push playground in the global list of obstacles(globla list comes from simulation.cpp)
+//     Playground* playground = new Playground(odeHandle, osgHandle, osg::Vec3(7.0, 0.2, 1.5));
+//     playground->setPosition(osg::Vec3(0,0,0)); // playground positionieren und generieren
+//     global.obstacles.push_back(playground);
+
+
+//     PassiveSphere* sphere =new PassiveSphere(odeHandle, osgHandle,0.2);
+//     //  sphere->setGeometry(0.2,0,0);
+//     sphere->setPosition(osg::Vec3(0,1,0)); 
+//     global.obstacles.push_back(sphere);
   
-  MuscledArmConf  conf = MuscledArm::getDefaultConf();
-  conf.drawSphere=true;
-  conf.drawMuscles=false;
-  conf.strained=false;
-  conf.jointAngleSensors=false;
-  conf.jointAngleRateSensors=false;
-  conf.MuscleLengthSensors=true;
-  arm = new MuscledArm(odeHandle, conf);
-  arm->setParam("damping",20);
-  arm->setParam("factorMotors",5);
-  arm->place(Position(0,0,0));
-  global.configs.push_back(arm);
+     MuscledArmConf  conf = MuscledArm::getDefaultConf();
+     conf.drawSphere=true;
+     conf.drawMuscles=false;
+     conf.strained=false;
+     conf.jointAngleSensors=false;
+     conf.jointAngleRateSensors=false;
+     conf.MuscleLengthSensors=true;
+     arm = new MuscledArm(odeHandle, osgHandle, conf);
+     arm->setParam("damping",20);
+     arm->setParam("factorMotors",5);
+     ((OdeRobot*)arm)->place(Position(0,0,0));
+     global.configs.push_back(arm);
 
-  // create pointer to controller
-  // push controller in global list of configurables
-  //  InvertMotorNStep(int buffersize, double cInit = 0.1, bool useS = false, bool someInternalParams = true);
+    // create pointer to controller
+    // push controller in global list of configurables
+    //  InvertMotorNStep(int buffersize, double cInit = 0.1, bool useS = false, bool someInternalParams = true);
 
-  InvertMotorNStepConf cc = InvertMotorNStep::getDefaultConf();
-  cc.buffersize=30;
-  //AbstractController *controller = new InvertMotorNStep(cc);
-  AbstractController *controller = new   SineController();
-  global.configs.push_back(controller);
+    InvertMotorNStepConf cc = InvertMotorNStep::getDefaultConf();
+    cc.buffersize=30;
+    //AbstractController *controller = new InvertMotorNStep(cc);
+    AbstractController *controller = new   SineController();
+    global.configs.push_back(controller);
   
-  // create pointer to one2onewiring
-  One2OneWiring* wiring = new One2OneWiring(new ColorUniformNoise(0.1));
+    // create pointer to one2onewiring
+    One2OneWiring* wiring = new One2OneWiring(new ColorUniformNoise(0.1));
 
-  // create pointer to agent
-  // initialize pointer with controller, robot and wiring
-  // push agent in globel list of agents
-  OdeAgent* agent = new OdeAgent(plotoptions);
-  agent->init(controller, arm, wiring);
-  //  agent->setTrackOptions(TrackRobot(true, false, false,50));
-  global.agents.push_back(agent);
+    // create pointer to agent
+    // initialize pointer with controller, robot and wiring
+    // push agent in globel list of agents
+    OdeAgent* agent = new OdeAgent(plotoptions);
+    agent->init(controller, arm, wiring);
+    //  agent->setTrackOptions(TrackRobot(true, false, false,50));
+    global.agents.push_back(agent);
 
-  // switch off gravity
-  global.odeConfig.setParam("gravity",0);
-  global.odeConfig.setParam("realtimefactor",0);
+    // switch off gravity
+    global.odeConfig.setParam("gravity",0);
+    global.odeConfig.setParam("realtimefactor",0);
 
-  // show (print to console) params of all objects in configurable list 
-  showParams(global.configs);
-}
-
-// executed once at the end of the simulation loop
-void end(GlobalData& global){
-  // clear obstacles list
-  for(ObstacleList::iterator i=global.obstacles.begin(); i != global.obstacles.end(); i++){
-    delete (*i);
+    // show (print to console) params of all objects in configurable list 
+    showParams(global.configs);
   }
-  global.obstacles.clear();
-  
-  // clear agents list
-  for(OdeAgentList::iterator i=global.agents.begin(); i != global.agents.end(); i++){
-    delete (*i)->getRobot();
-    delete (*i)->getController();
-    delete (*i);
+};
+
+
+  // print command line options
+  void printUsage(const char* progname){
+    printf("Usage: %s [-g] [-l]\n\t-g\tuse guilogger\n\t-l\tuse guilogger with logfile\n", progname);
   }
-  global.agents.clear();
-}
 
+  //Funktion die eingegebene Befehle/kommandos verarbeitet
+  void command (const OdeHandle&, GlobalData& globalData, int cmd)
+  {
+    //printp ( "Eingabe erfolgt %d (`%c')\n" , cmd , cmd );
+    switch ( (char) cmd )
+      {
+      case '1' : arm->force_[0]+=0.5; break;
+      case '!' : arm->force_[0]-=0.5; break;
 
-// this function is called if the user pressed Ctrl-C
-void config(GlobalData& global){
-  changeParams(global.configs);
-}
+      case '2' : arm->force_[1]+=0.5; break;
+      case '@' : arm->force_[1]-=0.5; break;
 
-// print command line options
-void printUsage(const char* progname){
-  printf("Usage: %s [-g] [-l]\n\t-g\tuse guilogger\n\t-l\tuse guilogger with logfile\n", progname);
-}
+      case '3' : arm->force_[2]+=0.5; break;
+      case '#' : arm->force_[2]-=0.5; break;
 
-//Funktion die eingegebene Befehle/kommandos verarbeitet
-void command (const OdeHandle&, GlobalData& globalData, int cmd)
-{
-  //dsPrint ( "Eingabe erfolgt %d (`%c')\n" , cmd , cmd );
-  switch ( (char) cmd )
-    {
-    case '1' : arm->force_[0]+=0.5; break;
-    case '!' : arm->force_[0]-=0.5; break;
+      case '4' : arm->force_[3]+=0.5; break;
+      case '$' : arm->force_[3]-=0.5; break;
 
-    case '2' : arm->force_[1]+=0.5; break;
-    case '@' : arm->force_[1]-=0.5; break;
+      case '5' : arm->force_[4]+=0.5; break;
+      case '%' : arm->force_[4]-=0.5; break;
 
-    case '3' : arm->force_[2]+=0.5; break;
-    case '#' : arm->force_[2]-=0.5; break;
+      case '6' : arm->force_[5]+=0.5; break;
+      case '^' : arm->force_[5]-=0.5; break;
 
-    case '4' : arm->force_[3]+=0.5; break;
-    case '$' : arm->force_[3]-=0.5; break;
+      case 'q' : arm->force_[0]=0; break;
+      case 'w' : arm->force_[1]=0; break;
+      case 'e' : arm->force_[2]=0; break;
+      case 'r' : arm->force_[3]=0; break;
+      case 't' : arm->force_[4]=0; break;
+      case 'y' : arm->force_[5]=0; break;
 
-    case '5' : arm->force_[4]+=0.5; break;
-    case '%' : arm->force_[4]-=0.5; break;
+      case 'a' : for (int i=0; i<6; i++) arm->force_[i]=0; break;
 
-    case '6' : arm->force_[5]+=0.5; break;
-    case '^' : arm->force_[5]-=0.5; break;
-
-    case 'q' : arm->force_[0]=0; break;
-    case 'w' : arm->force_[1]=0; break;
-    case 'e' : arm->force_[2]=0; break;
-    case 'r' : arm->force_[3]=0; break;
-    case 't' : arm->force_[4]=0; break;
-    case 'y' : arm->force_[5]=0; break;
-
-    case 'a' : for (int i=0; i<6; i++) arm->force_[i]=0; break;
-
-    case 'z' : 
-      arm->force_[0]=0;
-      arm->force_[1]=0;
-      arm->force_[2]=0.5;
-      arm->force_[3]=0.5;
-      arm->force_[4]=-0.5;
-      arm->force_[5]=-0.5; 
-      break;
+      case 'z' : 
+	arm->force_[0]=0;
+	arm->force_[1]=0;
+	arm->force_[2]=0.5;
+	arm->force_[3]=0.5;
+	arm->force_[4]=-0.5;
+	arm->force_[5]=-0.5; 
+	break;
+      }
+    for (int i=0; i<6; i++){
+      std::cout<<arm->force_[i]<<"  ";
     }
-  for (int i=0; i<6; i++){
-    std::cout<<arm->force_[i]<<"  ";
-  }
-  std::cout<<"\n";
+    std::cout<<"\n";
     
-}
-
+  }
 
 int main (int argc, char **argv)
 { 
@@ -248,61 +222,55 @@ int main (int argc, char **argv)
   if(contains(argv, argc, "-h")) printUsage(argv[0]);
 
 
-  // initialise the simulation and provide the start, end, and config-function
-  simulation_init(&start, &end, &config, &command , 0 , 0 ); 
+  ThisSim sim;
+  return sim.run(argc, argv) ? 0 : 1;
 
-  // start the simulation (returns, if the user closes the simulation)
-  simulation_start(argc, argv);
-  
-  // tidy up
-  simulation_close();  
-  return 0;
 }
 
 /*
- [Simulation Environment:  odeconfig.h,v - 1.12 ][1804289383]
- noise=              0.100000
- simstepsize=        0.010000
- realtimefactor=     0.000000
- drawinterval=       50.000000
- controlinterval=    5.000000
- gravity=            0.000000
- [muscledarm.cpp,v - 1.2 ][2031419189]
- factorMotors=       0.100000
- factorSensors=      4.000000
- damping=            1.000000
- [invertmotornstep.cpp,v - 1.19 ][240828263]
- epsA=               0.001299
- epsC=               0.000000
- adaptrate=          0.010000
- nomupdate=          0.010000
- desens=             0.000000
- s4delay=            1.000000
- s4avg=              5.000000
- steps=              4.000000
- zetaupdate=         0.000000
- logaE=              0.000000
- rootE=              0.000000
- relativeE=          0.000000
- factorB=            0.200000
- noiseB=             0.001000
+  [Simulation Environment:  odeconfig.h,v - 1.12 ][1804289383]
+  noise=              0.100000
+  simstepsize=        0.010000
+  realtimefactor=     0.000000
+  drawinterval=       50.000000
+  controlinterval=    5.000000
+  gravity=            0.000000
+  [muscledarm.cpp,v - 1.2 ][2031419189]
+  factorMotors=       0.100000
+  factorSensors=      4.000000
+  damping=            1.000000
+  [invertmotornstep.cpp,v - 1.19 ][240828263]
+  epsA=               0.001299
+  epsC=               0.000000
+  adaptrate=          0.010000
+  nomupdate=          0.010000
+  desens=             0.000000
+  s4delay=            1.000000
+  s4avg=              5.000000
+  steps=              4.000000
+  zetaupdate=         0.000000
+  logaE=              0.000000
+  rootE=              0.000000
+  relativeE=          0.000000
+  factorB=            0.200000
+  noiseB=             0.001000
 */
 
 /*
-[Simulation Environment:  odeconfig.h,v - 1.12 ][1804289383]
- noise=              0.000000
- simstepsize=        0.010000
- realtimefactor=     0.000000
- drawinterval=       50.000000
- controlinterval=    5.000000
- gravity=            0.000000
-[ muscledarm.cpp,v - 1.2 ][715246296]
- factorMotors=       0.000000
- factorSensors=      10.000000
- damping=            10.000000
-[ sinecontroller.hpp,v - 1.4 ][394186711]
- sinerate=           400.000000
- phaseshift=         1.000000
+  [Simulation Environment:  odeconfig.h,v - 1.12 ][1804289383]
+  noise=              0.000000
+  simstepsize=        0.010000
+  realtimefactor=     0.000000
+  drawinterval=       50.000000
+  controlinterval=    5.000000
+  gravity=            0.000000
+  [ muscledarm.cpp,v - 1.2 ][715246296]
+  factorMotors=       0.000000
+  factorSensors=      10.000000
+  damping=            10.000000
+  [ sinecontroller.hpp,v - 1.4 ][394186711]
+  sinerate=           400.000000
+  phaseshift=         1.000000
 */
 
 
@@ -312,31 +280,31 @@ int main (int argc, char **argv)
 Use random number seed: 1132245941
 
 [Simulation Environment:  odeconfig.h,v - 1.12 ][1804289383]
- noise=              0.100000
- simstepsize=        0.010000
- realtimefactor=     0.000000
- drawinterval=       50.000000
- controlinterval=    5.000000
- gravity=            0.000000
+noise=              0.100000
+simstepsize=        0.010000
+realtimefactor=     0.000000
+drawinterval=       50.000000
+controlinterval=    5.000000
+gravity=            0.000000
 [ muscledarm.cpp,v - 1.3 ][1577223465]
- factorMotors=       2.000000
- factorSensors=      4.000000
- damping=            20.000000
+factorMotors=       2.000000
+factorSensors=      4.000000
+damping=            20.000000
 [ invertmotornstep.cpp,v - 1.19 ][686205722]
- epsA=               0.005501
- epsC=               0.000000
- adaptrate=          0.010000
- nomupdate=          0.010000
- desens=             0.000000
- s4delay=            1.000000
- s4avg=              1.000000
- steps=              3.000000
- zetaupdate=         0.000000
- logaE=              0.000000
- rootE=              0.000000
- relativeE=          0.000000
- factorB=            0.200000
- noiseB=             0.001000
+epsA=               0.005501
+epsC=               0.000000
+adaptrate=          0.010000
+nomupdate=          0.010000
+desens=             0.000000
+s4delay=            1.000000
+s4avg=              1.000000
+steps=              3.000000
+zetaupdate=         0.000000
+logaE=              0.000000
+rootE=              0.000000
+relativeE=          0.000000
+factorB=            0.200000
+noiseB=             0.001000
 
 
 
