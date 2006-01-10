b956ad8af0d80429d65f68a5d7ab746ef8e3ac48
martius <martius>
2006-01-10 20:11:12 +0000

    moved to osg

--- ode_robots/obstacles/octaplayground.h
+++ ode_robots/obstacles/octaplayground.h
@@ -20,7 +20,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.4  2005-11-09 13:29:21  fhesse
+ *   Revision 1.4.4.1  2006-01-10 20:11:12  martius
+ *   moved to osg
+ *
+ *   Revision 1.4  2005/11/09 13:29:21  fhesse
  *   GPL added
  *                                                                 *
  *                                                                         *
@@ -28,21 +31,21 @@
 #ifndef __OCTAPLAYGROUND_H
 #define __OCTAPLAYGROUND_H
 
-
-#include <stdio.h>
 #include <math.h>
 #include <vector>
+#include <osg/Matrix>
 
+#include "primitive.h"
 #include "abstractobstacle.h"
-#include <drawstuff/drawstuff.h>
 
+namespace lpzrobots {
 
 class OctaPlayground : public AbstractObstacle {
 
   double radius, width, height;
-  double base_x, base_y, base_z;
+  Pos pos;
 
-  vector<dGeomID> obst; //obstacles
+  vector<Primitive*> obst; //obstacles
 
   bool obstacle_exists;
 
@@ -50,20 +53,16 @@ class OctaPlayground : public AbstractObstacle {
   double angle;    
   double box_length;
 
-
 public:
   
-  OctaPlayground(const OdeHandle& odehandle, int numberCorners=8):
-    AbstractObstacle::AbstractObstacle(odehandle){
-
-    base_x=0.0;
-    base_y=0.0;
-    base_z=0.0;
+  OctaPlayground(const OdeHandle& odeHandle, const OsgHandle& osgHandle, 
+		 const Pos& geometry = Pos(7,0.2,0.5), int numberCorners=8):
+    AbstractObstacle::AbstractObstacle(odeHandle, osgHandle){
     
-    radius=7.0;
-    width=0.2;
-    height=0.5;
-  
+    radius = geometry.x();
+    width  = geometry.y();
+    height = geometry.z();
+
     obstacle_exists=false;
         
     number_elements=numberCorners;
@@ -71,79 +70,56 @@ public:
     obst.resize(number_elements);    
     
     calcBoxLength();
-    setColor(226 / 255.0, 103 / 255.0, 66 / 255.0);
   };
   
   virtual ~OctaPlayground(){
+    for (unsigned int i=0; i < obst.size(); i++){
+      if(obst[i]) delete obst[i];
+    }
+    obst.clear();
   }
 
-  /**
-   * draws the obstacle (4 boxes for the playground)
-   */
-  virtual void draw(){
-    double box[3];
-    //dsSetTexture (DS_WOOD);    
-    dsSetColor (color.r, color.g, color.b);
-    box[0] = width; box[1] = box_length; box[2] = height;    
-
-    for(int i=0; i<number_elements; i++){
-      dsDrawBox ( dGeomGetPosition ( obst[i] ) , dGeomGetRotation ( obst[i] ) , box );
+  virtual void update(){
+    for (unsigned int i=0; i<obst.size(); i++){
+      if(obst[i]) obst[i]->update();
     }
   };
   
-  
-  virtual void setPosition(double x, double y, double z){
-    base_x=x;
-    base_y=y;
-    base_z=z;
+
+  virtual void setPosition(const osg::Vec3& pos){
+    this->pos = pos;
     if (obstacle_exists){
       destroy();
     }
     create();
   };
 
-  virtual void getPosition(double& x, double& y, double& z){
-    x=base_x;
-    y=base_y;
-    z=base_z;
-  }
-  
-  virtual void setGeometry(double radius_, double width_, double height_){
-    radius=radius_;
-    width=width_;
-    height =height_;
-    calcBoxLength();  
-  };
-
-  virtual void setColor(double r, double g, double b){
-    color.r=r;
-    color.g=g;
-    color.b=b;
+  virtual osg::Vec3 getPosition(){
+    return pos;
   };
 
 protected:
+
   virtual void create(){
     // radius for positioning is smaller than radius since we use secants. 
     //  r is the smallest distance of the secant to the center of the circle.
     double r = sqrt(pow((1+cos(angle))/2, 2) + pow( sin(angle)/2 ,2)) * radius;
     for (int i=0; i<number_elements; i++){
-      obst[i] = dCreateBox ( space, width , box_length , height);
-      dMatrix3 R;
-      dRFromEulerAngles(R, 0,0, i*angle);
-      dGeomSetRotation ( obst[i], R);
-
-      dGeomSetPosition ( obst[i], 
-       			 base_x + cos(M_PI - i*angle) * r, 
- 			 base_y + sin(M_PI - i*angle) * r, 
- 			 height/2 +base_z);
-
+      obst[i] = new Box(width , box_length , height);
+      obst[i]->init(odeHandle, 0, osgHandle, Primitive::Geom | Primitive::Draw);
+      osg::Matrix R = osg::Matrix::rotate(- i*angle, 0,0,1) * 
+	osg::Matrix::translate( pos.x() + cos(M_PI - i*angle) * r, 
+				pos.y() + sin(M_PI - i*angle) * r, 
+				pos.z() + height/2);
+      obst[i]->setPose(R);
+            
     }
   };
 
 
   virtual void destroy(){
-    for(int i=0; i<10; i++){
-      dGeomDestroy( obst[i] );
+    for(int i=0; i < number_elements; i++){
+      if(obst[i]) delete obst[i];
     }
     obstacle_exists=false;
   };
@@ -155,5 +131,7 @@ protected:
   }
 
 };
+
+}
 
 #endif
