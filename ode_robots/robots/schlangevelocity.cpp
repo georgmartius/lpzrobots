d65607901b6378ff6b30be209b42f07e2ccb39f5
fhesse <fhesse>
2006-01-10 13:55:12 +0000

    snake powered by directly setting angular velocities

---
+++ ode_robots/robots/schlangevelocity.cpp
@@ -0,0 +1,176 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1.2.1  2006-01-10 13:55:12  fhesse
+ *   snake powered by directly setting angular velocities
+ *                                        *
+ *                                                                         * 
+ ***************************************************************************/
+
+#include "schlangevelocity.h"
+
+
+namespace lpzrobots {
+
+  SchlangeVelocity::SchlangeVelocity ( const OdeHandle& odeHandle, const OsgHandle& osgHandle,
+				 const SchlangeConf& conf, const char* n) 
+    : Schlange(odeHandle, osgHandle, conf, n)
+  {
+    Configurable::insertCVSInfo(name, "$RCSfile$", 
+				"$Revision$");
+
+    factor_motors=1;
+    factor_sensors=1;
+    friction_joint=0.0;
+  }
+
+
+  SchlangeVelocity::~SchlangeVelocity() { }
+	
+
+  /**
+   *Reads the actual motor commands from an array, and sets all motors (forces) of the snake to this values.
+   *It is an linear allocation.
+   *@param motors pointer to the array, motor values are scaled to [-1,1] 
+   *@param motornumber length of the motor array
+   **/
+  void SchlangeVelocity::setMotors ( const motor* motors, int motornumber )
+  {
+    assert(created);
+    // there will always be an even number of motors
+    // (two sensors/motors per joint)
+    int len = min(motornumber/2, (int)joints.size());
+    // controller output as torques; friction added
+    for (int i = 0; i < len; i++){
+      // motorcommand
+      ((UniversalJoint*)joints[i])->setParam ( dParamVel, factor_motors * motors[2*i]);
+      ((UniversalJoint*)joints[i])->setParam ( dParamFMax , conf.motorPower );
+      //friction
+      ((UniversalJoint*)joints[i])->addTorques
+ 	(- friction_joint * ((UniversalJoint*)joints[i])->getPosition1Rate(), 
+ 	 - friction_joint * ((UniversalJoint*)joints[i])->getPosition2Rate());
+    }
+  }	
+
+  /**
+   *Writes the sensor values to an array in the memory.
+   *@param sensor* pointer to the arrays
+   *@param sensornumber length of the sensor array
+   *@return number of actually written sensors
+   **/
+  int SchlangeVelocity::getSensors ( sensor* sensors, int sensornumber )
+  {
+    assert(created);
+    // there will always be an even number of senors
+    // (two sensors/motors per joint)
+    int len = min(sensornumber/2, (int)joints.size()); 
+    // read angle of joints
+    /*
+      for (int n = 0; n < len; n++) {
+      sensors[2*n]   = joints[n]->getPosition1();
+      sensors[2*n+1] = joints[n]->getPosition2();
+      }
+    */
+    // or read anglerate of joints
+    for (int n = 0; n < len; n++) {
+      sensors[2*n]   = factor_sensors * ((UniversalJoint*)joints[n])->getPosition1Rate();
+      sensors[2*n+1] = factor_sensors * ((UniversalJoint*)joints[n])->getPosition2Rate();
+    }
+    return len*2;
+  }
+
+  /** The list of all parameters with there value as allocated lists.
+      @param keylist,vallist will be allocated with malloc (free it after use!)
+      @return length of the lists
+  */
+  Configurable::paramlist SchlangeVelocity::getParamList() const{
+    paramlist list;
+    list += Schlange::getParamList();                         
+    list += pair<paramkey, paramval> (string("factormotors"), factor_motors);
+    list += pair<paramkey, paramval> (string("factorsensors"), factor_sensors);
+    list += pair<paramkey, paramval> (string("frictionjoint"), friction_joint);
+    list += pair<paramkey, paramval> (string("motorpower"), conf.motorPower);
+    return list;
+  }
+  
+  
+  Configurable::paramval SchlangeVelocity::getParam(const paramkey& key) const{    
+    if(key == "factormotors") return factor_motors; 
+    else if(key == "factorsensors") return factor_sensors; 
+    else if(key == "frictionjoint") return friction_joint; 
+    else if(key == "motorpower") return conf.motorPower; 
+    else  return Schlange::getParam(key) ;
+  }
+  
+  bool SchlangeVelocity::setParam(const paramkey& key, paramval val){
+    if(key == "factormotors") factor_motors = val; 
+    else if(key == "factorsensors") factor_sensors = val; 
+    else if(key == "frictionjoint") friction_joint = val; 
+    else if(key == "motorpower") conf.motorPower = val; 
+    else return Schlange::setParam(key, val);
+    return true;
+  }
+
+
+
+  /** creates vehicle at desired position 
+      @param pos struct Position with desired position
+  */
+  void SchlangeVelocity::create(const osg::Matrix& pose){
+    Schlange::create(pose);
+    
+    //*****************joint definition***********
+    for ( int n = 0; n < conf.segmNumber-1; n++ ) {		
+
+      Pos p1(objects[n]->getPosition());
+      Pos p2(objects[n]->getPosition());
+      UniversalJoint* j = new UniversalJoint(objects[n], objects[n+1],
+					     (objects[n]->getPosition() + objects[n+1]->getPosition())/2,
+					     osg::Vec3(0,0,1)* pose, osg::Vec3(0,1,0)* pose);
+      j->init(odeHandle, osgHandle, true, conf.segmDia * 1.02);
+        
+      // setting stops at universal joints		
+      j->setParam(dParamLoStop, -conf.jointLimit*1.5);
+      j->setParam(dParamHiStop,  conf.jointLimit*1.5);
+      j->setParam(dParamLoStop2, -conf.jointLimit*1.5);
+      j->setParam(dParamHiStop2,  conf.jointLimit*1.5);
+    
+      // making stops bouncy
+          j->setParam (dParamBounce, 0.9 );
+          j->setParam (dParamBounce2, 0.9 ); // universal
+
+      joints.push_back(j); 
+    }	  
+  }
+
+
+  /** destroys vehicle and space
+   */
+  void SchlangeVelocity::destroy(){  
+    if (created){
+      Schlange::destroy();
+    }
+  }
+
+}
+
+
