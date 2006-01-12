c5a98e8dad7d335861c128eedf01bd3fe3ba2f3d
martius <martius>
2006-01-12 14:37:39 +0000

    access functions

--- ode_robots/utils/color.h
+++ ode_robots/utils/color.h
@@ -20,7 +20,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.2.4.1  2005-12-06 10:13:26  martius
+ *   Revision 1.2.4.2  2006-01-12 14:37:39  martius
+ *   access functions
+ *
+ *   Revision 1.2.4.1  2005/12/06 10:13:26  martius
  *   openscenegraph integration started
  *
  *   Revision 1.2  2005/11/09 13:31:51  martius
@@ -38,10 +41,23 @@ class Color : public osg::Vec4
 {
 public:
   Color() {};
-  Color(double r, double g, double b)
+  Color(const osg::Vec4& color) 
+    : osg::Vec4(color)  {};
+  Color(float r, float g, float b)
     : osg::Vec4(r, g, b, 1.0){} 
-  Color(double r, double g, double b, double a)
+  Color(float r, float g, float b, float a)
     : osg::Vec4(r, g, b, a){} 
+
+  float r() const { return x(); }
+  float& r() { return x(); }
+  float g() const { return y(); }
+  float& g() { return y(); }
+  float b() const { return z(); }
+  float& b() { return z(); }
+  float a() const { return w(); }
+  float& a() { return w(); }
+  float alpha() const { return w(); }
+  float& alpha() { return w(); }
 };
 
 }
