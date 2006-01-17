a7f69f5c6ecce03c3bb1cee7c791519f88d6e981
martius <martius>
2006-01-17 09:11:19 +0000

    commented since not compileable with OSG 1.0

--- ode_robots/osg/rendertotexturecallback.h
+++ ode_robots/osg/rendertotexturecallback.h
@@ -23,7 +23,10 @@
  *  DESCRIPTION                                                            *
  *                                                                         *
  *   $Log$
- *   Revision 1.1.2.1  2006-01-12 14:22:37  martius
+ *   Revision 1.1.2.2  2006-01-17 09:11:19  martius
+ *   commented since not compileable with OSG 1.0
+ *
+ *   Revision 1.1.2.1  2006/01/12 14:22:37  martius
  *   deep shadow implementation
  *
  *   Revision 1.1.2.1  2005/12/06 17:38:21  martius
@@ -34,51 +37,51 @@
 #ifndef __RENDERTOTEXTURECALLBACK_H
 #define __RENDERTOTEXTURECALLBACK_H
 
-#include <osg/ref_ptr>
-#include "osgforwarddecl.h"
-//#include <osg/Node>
-#include <osg/NodeCallback>
-#include <osgUtil/CullVisitor>
-#include <osg/Texture2D>
-//#include <osg/StateSet> 
-#include <osg/Viewport>
-//#include <osg/LightSource>
-#include <osg/TexGen>
+/* #include <osg/ref_ptr> */
+/* #include "osgforwarddecl.h" */
+/* //#include <osg/Node> */
+/* #include <osg/NodeCallback> */
+/* #include <osgUtil/CullVisitor> */
+/* #include <osg/Texture2D> */
+/* //#include <osg/StateSet>  */
+/* #include <osg/Viewport> */
+/* //#include <osg/LightSource> */
+/* #include <osg/TexGen> */
 
-namespace lpzrobots {
+/* namespace lpzrobots { */
 
-  class RenderToTextureCallback: public osg::NodeCallback {
-  public:
-    RenderToTextureCallback(osg::Node* subgraph, 
-			    osg::Texture2D* texture, 
-			    osg::LightSource* light_source,
-			    osg::TexGen* tex_gen); 
+/*   class RenderToTextureCallback: public osg::NodeCallback { */
+/*   public: */
+/*     RenderToTextureCallback(osg::Node* subgraph,  */
+/* 			    osg::Texture2D* texture,  */
+/* 			    osg::LightSource* light_source, */
+/* 			    osg::TexGen* tex_gen);  */
     
-    virtual void operator()(osg::Node* node, osg::NodeVisitor* nv)
-    {
+/*     virtual void operator()(osg::Node* node, osg::NodeVisitor* nv) */
+/*     { */
       
-      osgUtil::CullVisitor* cullVisitor = dynamic_cast<osgUtil::CullVisitor*>(nv);
-      if (cullVisitor && _texture.valid() && _subgraph.valid())
-	{            
-	  _request_render_to_depth_texture(*node, *cullVisitor);
-	}
+/*       osgUtil::CullVisitor* cullVisitor = dynamic_cast<osgUtil::CullVisitor*>(nv); */
+/*       if (cullVisitor && _texture.valid() && _subgraph.valid()) */
+/* 	{             */
+/* 	  _request_render_to_depth_texture(*node, *cullVisitor); */
+/* 	} */
       
-      // must traverse the subgraph            
-      traverse(node,nv);
-    }
+/*       // must traverse the subgraph             */
+/*       traverse(node,nv); */
+/*     } */
     
-    void _request_render_to_depth_texture(osg::Node& node, osgUtil::CullVisitor& cv);
+/*     void _request_render_to_depth_texture(osg::Node& node, osgUtil::CullVisitor& cv); */
     
     
-    osg::ref_ptr<osg::Node>                     _subgraph;
-    osg::ref_ptr<osg::Texture2D>                _texture;
-    osg::ref_ptr<osg::StateSet>                 _local_stateset;
-    osg::ref_ptr<osg::Viewport>                 _viewport;
-    osg::ref_ptr<osg::RefMatrix>                _light_projection;
-    osg::ref_ptr<osg::LightSource>              _light_source;
-    osg::ref_ptr<osg::TexGen>                   _tex_gen;
-  };
+/*     osg::ref_ptr<osg::Node>                     _subgraph; */
+/*     osg::ref_ptr<osg::Texture2D>                _texture; */
+/*     osg::ref_ptr<osg::StateSet>                 _local_stateset; */
+/*     osg::ref_ptr<osg::Viewport>                 _viewport; */
+/*     osg::ref_ptr<osg::RefMatrix>                _light_projection; */
+/*     osg::ref_ptr<osg::LightSource>              _light_source; */
+/*     osg::ref_ptr<osg::TexGen>                   _tex_gen; */
+/*   }; */
 
-}
+/* } */
 
 #endif
