80169d5fd3bf8b7c02ac2c56fab4bb6e62340253
martius <martius>
2006-01-18 16:46:56 +0000

    moved to osg

--- ode_robots/simulations/nimm2_arena/main.cpp
+++ ode_robots/simulations/nimm2_arena/main.cpp
@@ -20,139 +20,174 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.5.4.1  2005-11-15 12:29:50  martius
- *   new selforg structure and OdeAgent, OdeRobot ...
+ *   Revision 1.5.4.2  2006-01-18 16:46:56  martius
+ *   moved to osg
  *
- *   Revision 1.5  2005/11/09 14:08:48  martius
+ *   Revision 1.1.2.3  2006/01/17 17:02:47  martius
  *   *** empty log message ***
  *
- *   Revision 1.4  2005/11/09 13:41:10  fhesse
- *   GPL added
- *                                                                 *
- *                                                                         * 
-***************************************************************************/
-#include <stdio.h>
-#include <drawstuff/drawstuff.h>
-#include <ode/ode.h>
+ *   Revision 1.1.2.2  2006/01/13 12:33:16  martius
+ *   *** empty log message ***
+ *
+ *   Revision 1.1.2.1  2006/01/13 12:24:06  martius
+ *   env for external teaching input to the controller
+ *
+ *
+ ***************************************************************************/
 
-#include <selforg/noisegenerator.h>
 #include "simulation.h"
+
 #include "odeagent.h"
-#include <selforg/one2onewiring.h>
-#include "nimm2.h"
-#include "playground.h"
 #include "octaplayground.h"
+#include "passivesphere.h"
 
-#include <selforg/invertnchannelcontroller.h>
 #include <selforg/invertmotornstep.h>
+#include <selforg/invertmotorspace.h>
+#include <selforg/invertnchannelcontroller.h>
+#include <selforg/noisegenerator.h>
+#include <selforg/one2onewiring.h>
 
-ConfigList configs;
+#include "nimm2.h"
+
+// fetch all the stuff of lpzrobots into scope
+using namespace lpzrobots;
+
+// plotoptions is a list of possible online output, 
+// if the list is empty no online gnuplot windows and no logging to file occurs.
+// The list is modified with commandline options, see main() at the bottom of this file
 list<PlotOption> plotoptions;
 
-//Startfunktion die am Anfang der Simulationsschleife, einmal ausgefuehrt wird
-void start(const OdeHandle& odeHandle, GlobalData& global) 
-{
-  dsPrint ( "\nWelcome to the virtual ODE - robot simulator of the Robot Group Leipzig\n" );
-  dsPrint ( "------------------------------------------------------------------------\n" );
-  dsPrint ( "Press Ctrl-C for an basic commandline interface.\n\n" );
+AbstractController* controller;
+motor teaching[2];
 
-  //Anfangskameraposition und Punkt auf den die Kamera blickt
-  float KameraXYZ[3]= {0.25f,-6.0f,3.79f};
-  float KameraViewXYZ[3] = {90.00f,-43.50f,0.0000f};;
-  dsSetViewpoint ( KameraXYZ , KameraViewXYZ );
-  dsSetSphereQuality (2); //Qualitaet in der Sphaeren gezeichnet werden
+class ThisSim : public Simulation {
+public:
 
-  // initialization
-  global.odeConfig.noise=0.1;
+  // starting function (executed once at the beginning of the simulation loop)
+  void start(const OdeHandle& odeHandle, const OsgHandle& osgHandle, GlobalData& global) 
+  {
+    setCameraHomePos(Pos(5.2728, 7.2112, 3.31768), Pos(140.539, -13.1456, 0));
+    // initialization
+    // - set noise to 0.1
+    // - register file chess.ppm as a texture called chessTexture (used for the wheels)
+    global.odeConfig.noise=0.05;
+    //    global.odeConfig.setParam("gravity", 0);
+    global.odeConfig.setParam("controlinterval", 5);
+
+    // use Playground as boundary:
+    OctaPlayground* playground = new OctaPlayground(odeHandle, osgHandle, osg::Vec3(11, 0.2, 1), 12);
+    playground->setPosition(osg::Vec3(0,0,0)); // playground positionieren und generieren
+    global.obstacles.push_back(playground);
+
+    // for(int i=0; i<50; i++){
+//       PassiveSphere* s = new PassiveSphere(odeHandle, osgHandle.changeColor(Color(0.0,1.0,0.0)), 0.5);
+//       s->setPosition(osg::Vec3(-4+(i/10),-4+(i%10),1)); 
+//       global.obstacles.push_back(s);    
+//     }
     
-  Playground* playground = new Playground(odeHandle, /*factorxy=1*/ -4);
-  playground->setGeometry(8.0, 0.09, 0.25);
-  playground->setPosition(0,0,0); // playground positionieren und generieren
-  global.obstacles.push_back(playground);
-  
-
-//   OctaPlayground* octaplayground = new OctaPlayground(odeHandle, 330);
-//   octaplayground->setGeometry(6.5, 0.09, 0.25);
-//   octaplayground->setPosition(0,0,0); // playground positionieren und generieren
-//   global.obstacles.push_back(octaplayground);
-
-  Nimm2* nimm2;
-  AbstractController* controller;
-  AbstractWiring* wiring;
-  OdeAgent* agent;
-
-  int chessTexture = dsRegisterTexture("chess.ppm");
-
-  for (int j=-1; j<2; j++){ 
-    for (int i=-4; i<5; i++){
-      //      nimm2 = new Nimm2(odeHandle);
-      Nimm2Conf conf = Nimm2::getDefaultConf();
-      conf.speed=20;
-      conf.force=0.5;
-      conf.bumper=true;
-      conf.cigarMode=true;
-      nimm2 = new Nimm2(odeHandle, conf);
-      nimm2->setTextures(DS_WOOD, chessTexture); 
-      Color c(2,2,0);
-      if ((i==0) && (j==0)) {nimm2->place(Position(j*0.26,i*0.26,0), &c);}
-      else {
-	nimm2->place(Position(j*2.5,i*1.26,0));
+    OdeRobot* nimm2;
+    AbstractController* contrl;
+    AbstractWiring* wiring;
+    OdeAgent* agent;
+        
+    OsgHandle osgHandle_yellow = osgHandle.changeColor(Color(1.0,1.0,0));
+    OsgHandle osgHandle_orange = osgHandle.changeColor(Color(2, 156/255.0, 0));
+    for (int j=-0; j<1; j++){ 
+      for (int i=-0; i<1; i++){
+	//      nimm2 = new Nimm2(odeHandle);
+	Nimm2Conf conf = Nimm2::getDefaultConf();
+	conf.speed=20;
+	conf.force=3.0;
+	conf.bumper=true;
+	conf.cigarMode=true;
+	wiring = new One2OneWiring(new ColorUniformNoise(0.1));
+	if ((i==0) && (j==0)) {
+	  controller = new InvertMotorNStep();  
+	  //	  controller = new InvertMotorSpace(10);  
+	  agent = new OdeAgent(plotoptions);
+	  nimm2 = new Nimm2(odeHandle, osgHandle_yellow, conf);
+	  global.configs.push_back(controller);
+	  agent->init(controller, nimm2, wiring);
+	  controller->setParam("adaptrate", 0.000);
+	  //    controller->setParam("nomupdate", 0.0005);
+	  controller->setParam("epsC", 0.05);
+	  controller->setParam("epsA", 0.01);
+	  controller->setParam("epsC", 0.05);
+	  controller->setParam("rootE", 0);
+	  controller->setParam("steps", 2);
+	  controller->setParam("s4avg", 5);
+	  controller->setParam("s4del", 5);
+	  //	  controller->setParam("factorB",0);
+	} else {
+	  contrl = new InvertNChannelController(10);  		
+	  agent = new OdeAgent(NoPlot);	  
+	  nimm2 = new Nimm2(odeHandle, osgHandle_orange, conf);
+	  agent->init(contrl, nimm2, wiring);
+	  contrl->setParam("adaptrate", 0.000);
+	  //    controller->setParam("nomupdate", 0.0005);
+	  contrl->setParam("epsC", 0.005);
+	  contrl->setParam("epsA", 0.001);
+	  contrl->setParam("rootE", 0);
+	  contrl->setParam("steps", 2);
+	  contrl->setParam("s4avg", 5);
+	  contrl->setParam("factorB",0);
+	}
+	nimm2->place(Pos(j*2.5,i*1.26,0));
+	global.agents.push_back(agent);
+	
       }
-      //controller = new InvertMotorNStep(10);  
-      controller = new InvertNChannelController(10);  
-
-      configs.push_back(controller);
-      
-      wiring = new One2OneWiring(new ColorUniformNoise(0.1));
-      if ((i==0) && (j==0)) {agent = new OdeAgent(plotoptions);}
-      else {
-	agent = new OdeAgent(NoPlot);
-      }
-      agent->init(controller, nimm2, wiring);
-      global.agents.push_back(agent);
-
-      controller->setParam("factorB",0);
     }
+      
+    showParams(global.configs);
   }
 
-  showParams(configs);
-}
-
-void end(GlobalData& global){
-  for(ObstacleList::iterator i=global.obstacles.begin(); i != global.obstacles.end(); i++){
-    delete (*i);
+  //Funktion die eingegebene Befehle/kommandos verarbeitet
+  virtual bool command (const OdeHandle&, const OsgHandle&, GlobalData& globalData, int key, bool down)
+  {
+    if (!down) return false;    
+    bool handled = false;
+    switch ( key )
+      {
+      case 's' :
+	controller->store("test") && printf("Controller stored\n");
+	handled = true; break;	
+      case 'l' :
+	controller->restore("test") && printf("Controller loaded\n");
+	handled = true; break;	
+      }
+    fflush(stdout);
+    return handled;
   }
-  global.obstacles.clear();
-  for(OdeAgentList::iterator i=global.agents.begin(); i != global.agents.end(); i++){
-    delete (*i)->getRobot();
-    delete (*i)->getController();
-    delete (*i);
+
+  virtual void bindingDescription(osg::ApplicationUsage & au) const {
+    au.addKeyboardMouseBinding("Teachung: t","toggle mode");
+    au.addKeyboardMouseBinding("Teaching: u","forward");
+    au.addKeyboardMouseBinding("Teaching: j","backward");
+    au.addKeyboardMouseBinding("Simulation: s","store");
+    au.addKeyboardMouseBinding("Simulation: l","load");
   }
-  global.agents.clear();
-}
 
+};
 
-// this function is called if the user pressed Ctrl-C
-void config(GlobalData& global){
-  changeParams(configs);
-}
-
+// print command line options
 void printUsage(const char* progname){
-  printf("Usage: %s [-g] [-l]\n\t-g\tuse guilogger\n\t-l\tuse guilogger with logfile", progname);
-  exit(0);
+  printf("Usage: %s [-g] [-f]\n\t-g\tuse guilogger\n\t-f\tuse guilogger with logfile\n", progname);
 }
 
 int main (int argc, char **argv)
-{  
+{ 
+  // start with online windows (default: start without plotting and logging)
   if(contains(argv, argc, "-g")) plotoptions.push_back(PlotOption(GuiLogger));
-  if(contains(argv, argc, "-l")) plotoptions.push_back(PlotOption(GuiLogger_File));
+  
+  // start with online windows and logging to file
+  if(contains(argv, argc, "-f")) plotoptions.push_back(PlotOption(GuiLogger_File));
+  
+  // display help
   if(contains(argv, argc, "-h")) printUsage(argv[0]);
 
-  // initialise the simulation and provide the start, end, and config-function
-  simulation_init(&start, &end, &config);
-  // start the simulation (returns, if the user closes the simulation)
-  simulation_start(argc, argv);
-  simulation_close();  // tidy up.
-  return 0;
+  ThisSim sim;
+  return sim.run(argc, argv) ? 0 :  1;
 }
+
+ 
  
