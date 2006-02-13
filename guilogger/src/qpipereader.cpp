dcffc1be38ee841e83ad888f9e656065e5af5684
martius <martius>
2006-02-13 13:25:50 +0000

    much more performant read in using fgets

--- guilogger/src/qpipereader.cpp
+++ guilogger/src/qpipereader.cpp
@@ -22,43 +22,24 @@
 #include <unistd.h>  //für usleep
 #include <stdlib.h>
 
-QPipeReader::QPipeReader(char bt, int delay)
-{   blockterminator = bt;    // default parameter = 100
-    this->delay = delay;
-//    setbuf(stdin, NULL);  // nützt auch nix
+QPipeReader::QPipeReader(int delay)
+{   
+    this->delay = delay;// default parameter = 100
 }
 
 void QPipeReader::run()
 {
-    char *s = NULL;
-    int size = 0;
+  int size = 65536;
+  char *s = (char*)malloc(size*sizeof(char));
 
-    while(1)
-    {
-        char c;
-        int i=0;
-
-        do{
-//            i = fscanf(stdin, "%c", &c);
-            i = fread(&c, 1, 1, stdin);
-            if(i==0) usleep(delay);  // mal ein bissel schlafen wenn nix da ist um die Last zu senken
-        } while(i!=1);
-
-        size++;
-        s = (char*) realloc( s, size);
-        s[size-1] = c;
-
-        if(c==blockterminator)   // check if we got a line ending
-        {   // s now contains a complete line readed from serial port
-            s[size-1] = '\0';       // make s a zero terminated string (ZTS)
-            emit newData(s);
-//            printf("Readed: %s\n", s);
-            
-            free(s);
-            s=NULL;
-            size=0;
-            usleep(delay);     // wenn Line gelesen, mal ein bissel warten, damit nicht alles gleich durchpfeift
-        }
-    }
-
+  while(1) {
+    char* ctrl;
+    
+    ctrl = fgets(s, size, stdin);
+        
+    emit newData(s);            
+    //     usleep(delay);     // wenn Line gelesen, mal ein bissel warten, damit nicht alles gleich durchpfeift
+  }
+  free(s);  
 }
+
