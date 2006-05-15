72504d9c2f6372c8385609f85259b74f5879370b
robot3 <robot3>
2006-05-15 12:29:43 +0000

    handling of starting the guilogger moved to simulation.cpp (is in internal simulation routine now)

--- ode_robots/simulations/sphericalrobot/main.cpp
+++ ode_robots/simulations/sphericalrobot/main.cpp
@@ -20,7 +20,11 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.6.4.4  2006-03-29 15:10:22  martius
+ *   Revision 1.6.4.5  2006-05-15 12:29:43  robot3
+ *   handling of starting the guilogger moved to simulation.cpp
+ *   (is in internal simulation routine now)
+ *
+ *   Revision 1.6.4.4  2006/03/29 15:10:22  martius
  *   *** empty log message ***
  *
  *   Revision 1.6.4.3  2006/02/17 16:47:55  martius
@@ -60,10 +64,6 @@
 // fetch all the stuff of lpzrobots into scope
 using namespace lpzrobots;
 
-// plotoptions is a list of possible online output, 
-// if the list is empty no online gnuplot windows and no logging to file occurs.
-// The list is modified with commandline options, see main() at the bottom of this file
-list<PlotOption> plotoptions;
 
 AbstractController *controller;
 
@@ -151,28 +151,27 @@ public:
 // 	break;
 //       }
 //   }
+
+  // add own key handling stuff here, just insert some case values
+  bool command(const OdeHandle&, const OsgHandle&, GlobalData& globalData, int key, bool down)
+  {
+    if (down) { // only when key is pressed, not when released
+      switch ( (char) key )
+	{
+	default:
+	  return false;
+	  break;
+	}
+    }
+    return false;
+  }
   
-
 };
-
-// print command line options
-void printUsage(const char* progname){
-  printf("Usage: %s [-g] [-l]\n\t-g\tuse guilogger\n\t-l\tuse guilogger with logfile\n", progname);
-}
 
 int main (int argc, char **argv)
 { 
-  // start with online windows (default: start without plotting and logging)
-  if(contains(argv, argc, "-g")) plotoptions.push_back(PlotOption(GuiLogger));
-  
-  // start with online windows and logging to file
-  if(contains(argv, argc, "-f")) plotoptions.push_back(PlotOption(GuiLogger_File, Controller, 5));
-  
-  // display help
-  if(contains(argv, argc, "-h")) printUsage(argv[0]);
-
   ThisSim sim;
+  // run simulation
   return sim.run(argc, argv) ? 0 : 1;
-
 }
  
