e57e0df1afbd10e73d67191670463bd29f1f8603
robot3 <robot3>
2006-05-18 14:16:03 +0000

    -inserted SphereRobot3Masses -inserted CaterPillar -inserted SchlangeServo2 -removed multiple same robots (performance issues) -inserted new passiveCapsules -passiveBoxes look more interesting now -wall of playground is now transparent (0.2)

--- ode_robots/simulations/zoo/main.cpp
+++ ode_robots/simulations/zoo/main.cpp
@@ -20,7 +20,16 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.11.4.9  2006-05-18 10:26:50  robot3
+ *   Revision 1.11.4.10  2006-05-18 14:16:03  robot3
+ *   -inserted SphereRobot3Masses
+ *   -inserted CaterPillar
+ *   -inserted SchlangeServo2
+ *   -removed multiple same robots (performance issues)
+ *   -inserted new passiveCapsules
+ *   -passiveBoxes look more interesting now
+ *   -wall of playground is now transparent (0.2)
+ *
+ *   Revision 1.11.4.9  2006/05/18 10:26:50  robot3
  *   -made the playground smaller (for shadowing issues)
  *   -changed camera homepos
  *
@@ -65,8 +74,10 @@
 
 #include "odeagent.h"
 #include "playground.h"
+
 #include "passivesphere.h"
 #include "passivebox.h"
+#include "passivecapsule.h"
 
 #include <selforg/invertnchannelcontroller.h>
 #include <selforg/invertmotornstep.h>
@@ -76,14 +87,14 @@
 #include <selforg/one2onewiring.h>
 
 #include "hurlingsnake.h"
-#include "schlangeforce.h"
+#include "schlangeservo2.h"
 #include "caterpillar.h"
 #include "nimm2.h"
 #include "nimm4.h"
+#include "sphererobot3masses.h"
 
 // fetch all the stuff of lpzrobots into scope
 using namespace lpzrobots;
-
 
 class ThisSim : public Simulation {
 public:
@@ -91,7 +102,7 @@ public:
   // starting function (executed once at the beginning of the simulation loop)
   void start(const OdeHandle& odeHandle, const OsgHandle& osgHandle, GlobalData& global) 
   {
-    setCameraHomePos(Pos(-18.00f, 13.0f, 6.9f),  Pos(-125.0f, -20.0f, 0));
+    setCameraHomePos(Pos(-19.15, 13.9, 6.9),  Pos(-126.1, -17.6, 0));
     // initialization
     // - set noise to 0.1
     // - register file chess.ppm as a texture called chessTexture (used for the wheels)
@@ -100,7 +111,9 @@ public:
     global.odeConfig.setParam("controlinterval",1);
     // initialization
     
-    Playground* playground = new Playground(odeHandle, osgHandle, osg::Vec3(18, 0.2, 1.0));
+    Playground* playground = 
+      new Playground(odeHandle, osgHandle,osg::Vec3(18, 0.2, 1.0));
+    playground->setColor(Color(0.88f,0.4f,0.26f,0.2f));
     playground->setPosition(osg::Vec3(0,0,0)); // playground positionieren und generieren
     global.obstacles.push_back(playground);
     
@@ -116,10 +129,20 @@ public:
     for(int i=0; i<5; i++){
       PassiveBox* b = 
 	new PassiveBox(odeHandle, 
-			  osgHandle.changeColor(Color(184 / 255.0, 233 / 255.0, 237 / 255.0)), osg::Vec3(0.2+i*0.1,0.2+i*0.1,0.2+i*0.1));
+			  osgHandle, osg::Vec3(0.2+i*0.1,0.2+i*0.1,0.2+i*0.1));
       b->setPosition(Pos(i*0.5-5, i*0.5, 1.0)); 
-      b->setTexture("Images/dusty.rgb");
+      b->setColor(Color(1.0f,0.2f,0.2f,0.5f));
+      b->setTexture("Images/light_chess.rgb");
       global.obstacles.push_back(b);    
+    }
+
+    for(int i=0; i<5; i++){
+      PassiveCapsule* c = 
+	new PassiveCapsule(odeHandle, osgHandle, 0.2f, 0.3f, 0.3f);
+      c->setPosition(Pos(i-1, -i, 1.0)); 
+      c->setColor(Color(0.2f,0.2f,1.0f,0.5f));
+      c->setTexture("Images/light_chess.rgb");
+      global.obstacles.push_back(c);    
     }
         
     OdeAgent* agent;
@@ -127,52 +150,52 @@ public:
     OdeRobot* robot;
     AbstractController *controller;
     
-//     CaterPillar* myCaterPillar;
-//     CaterPillarConf myCaterPillarConf = DefaultCaterPillar::getDefaultConf();
-//     //******* R A U P E  *********/
-//     myCaterPillarConf.segmNumber=4;
-//     myCaterPillarConf.jointLimit=M_PI/3;
-//     myCaterPillarConf.motorPower=0.2;
-//     myCaterPillarConf.frictionGround=0.01;
-//     myCaterPillar = new CaterPillar ( odeHandle, osgHandle.changeColor(Color(1.0f,0.0,0.0)), myCaterPillarConf, "Raupe1");
-//     ((OdeRobot*) myCaterPillar)->place(Pos(-5,-5,0.2)); 
+    //******* R A U P E  *********/
+    CaterPillar* myCaterPillar;
+    CaterPillarConf myCaterPillarConf = DefaultCaterPillar::getDefaultConf();
+    myCaterPillarConf.segmNumber=3;
+    myCaterPillarConf.jointLimit=M_PI/3;
+    myCaterPillarConf.motorPower=0.2;
+    myCaterPillarConf.frictionGround=0.01;
+    myCaterPillarConf.frictionJoint=0.01;
+    myCaterPillar =
+      new CaterPillar ( odeHandle, osgHandle.changeColor(Color(1.0f,0.0,0.0)), myCaterPillarConf, "Raupe");
+    ((OdeRobot*) myCaterPillar)->place(Pos(-5,-5,0.2)); 
     
      InvertMotorNStepConf invertnconf = InvertMotorNStep::getDefaultConf();
      invertnconf.cInit=2.0;
-//     controller = new InvertMotorNStep(invertnconf);    
-//     wiring = new One2OneWiring(new ColorUniformNoise(0.1));
-//     agent = new OdeAgent( plotoptions );
-//     agent->init(controller, myCaterPillar, wiring);
-//     global.agents.push_back(agent);
-//     global.configs.push_back(controller);
-//     global.configs.push_back(myCaterPillar);   
-//     myCaterPillar->setParam("gamma",/*gb");
-//      global.obstacles.push_back(s)0.0000*/ 0.0);
+     controller = new InvertMotorSpace(15);
+    wiring = new One2OneWiring(new ColorUniformNoise(0.1));
+    agent = new OdeAgent( plotoptions );
+    agent->init(controller, myCaterPillar, wiring);
+    global.agents.push_back(agent);
+    global.configs.push_back(controller);
+    global.configs.push_back(myCaterPillar);   
+    myCaterPillar->setParam("gamma",/*gb");
+    global.obstacles.push_back(s)0.0000*/ 0.0);
   
 
     //******* S C H L A N G E  (Long)  *********/
-//     SchlangeForce* snake;
-//     SchlangeConf snakeConf = SchlangeForce::getDefaultConf();
-//     snakeConf.motorPower=0.5;
-//     snakeConf.segmNumber=8;
-//     snakeConf.jointLimit=M_PI/3;
-//     snakeConf.frictionGround=0.1;
-//     snake = new SchlangeForce ( odeHandle, osgHandle.changeColor(Color(0,0.5,0.8)), snakeConf, "Schlange2" );
-//     ((OdeRobot*) snake)->place(Pos(0,0,0)); 
-//     controller = new InvertMotorNStep(invertnconf);     
-//     wiring = new One2OneWiring(new ColorUniformNoise(0.1));
-//     agent = new OdeAgent( list<PlotOption>() );
-//     agent->init(controller, snake, wiring);
-//     global.agents.push_back(agent);
-//     global.configs.push_back(controller);
-//     global.configs.push_back(snake);   
-//     snake->setParam("gamma",0.0000 0.0);
+    SchlangeServo2* snake;
+    SchlangeConf snakeConf = SchlangeServo2::getDefaultConf();
+    snakeConf.segmNumber=4;
+    snakeConf.frictionGround=0.01;
+
+    snake = new SchlangeServo2 ( odeHandle, osgHandle.changeColor(Color(0,0.5,0.8)), snakeConf, "Schlange" );
+    ((OdeRobot*) snake)->place(Pos(4,4,0)); 
+    controller = new InvertMotorNStep(invertnconf);     
+    wiring = new One2OneWiring(new ColorUniformNoise(0.1));
+    agent = new OdeAgent( list<PlotOption>() );
+    agent->init(controller, snake, wiring);
+    global.agents.push_back(agent);
+    global.configs.push_back(controller);
+    global.configs.push_back(snake);   
   
 
     //******* N I M M  2 *********/
     Nimm2Conf nimm2conf = Nimm2::getDefaultConf();
     nimm2conf.size = 1.6;
-    for(int r=0; r < 3; r++) {    
+    for(int r=0; r < 1; r++) {    
       robot = new Nimm2(odeHandle, osgHandle, nimm2conf);
       robot->place(Pos ((r-1)*5,5,0));
       //    controller = new InvertMotorNStep(10);   
@@ -200,7 +223,7 @@ public:
     }
 
     //****** H U R L I N G **********/
-    for(int r=0; r < 2; r++) {
+    for(int r=0; r < 1; r++) {
       HurlingSnake* snake;
       Color c;    
       if (r==0) c=Color(0.8, 0.8, 0);
@@ -226,7 +249,24 @@ public:
 			       global.configs.push_back(controller);
 			       global.agents.push_back(agent);     
     }
-      showParams(global.configs);
+
+    //****** S P H E R E **********/
+    Sphererobot3MassesConf conf = Sphererobot3Masses::getDefaultConf();  
+    Sphererobot3Masses* sphere1 = 
+      new Sphererobot3Masses ( odeHandle, osgHandle.changeColor(Color(1.0,0.0,0)), 
+				       conf, "Sphere1", 0.2); 
+    ((OdeRobot*)sphere1)->place ( Pos( 0 , 0 , 0.1 ));
+    controller = new InvertMotorSpace(15);
+    controller->setParam("sinerate", 40);  
+    controller->setParam("phaseshift", 0.0);
+    One2OneWiring* wiring2 = new One2OneWiring ( new ColorUniformNoise() );
+    agent = new OdeAgent ( plotoptions );
+    agent->init ( controller , sphere1 , wiring2 );
+    global.agents.push_back ( agent );
+    global.configs.push_back ( controller );
+
+    
+    showParams(global.configs);
   }
   
   // add own key handling stuff here, just insert some case values
