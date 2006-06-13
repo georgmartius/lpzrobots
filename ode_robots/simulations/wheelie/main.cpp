598b1727907acd8b74cb2c94659f3b385686e184
robot5 <robot5>
2006-06-13 04:36:32 +0000

    First version.

---
+++ ode_robots/simulations/wheelie/main.cpp
@@ -0,0 +1,118 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *    frankguettler@gmx.de                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *
+ ***************************************************************************/
+#include "simulation.h"
+
+#include "odeagent.h"
+#include "playground.h"
+#include "passivesphere.h"
+
+#include <selforg/invertmotornstep.h>
+#include <selforg/noisegenerator.h>
+#include <selforg/one2onewiring.h>
+
+#include "wheelie.h"
+
+// fetch all the stuff of lpzrobots into scope
+using namespace lpzrobots;
+
+
+class ThisSim : public Simulation {
+public:
+
+  // starting function (executed once at the beginning of the simulation loop)
+  void start(const OdeHandle& odeHandle, const OsgHandle& osgHandle, GlobalData& global) 
+  {
+    setCameraHomePos(Pos(5.2728, 7.2112, 3.31768), Pos(140.539, -13.1456, 0));
+    // initialization
+    // - set noise to 0.1
+    // - register file chess.ppm as a texture called chessTexture (used for the wheels)
+
+    global.odeConfig.setParam("noise",0.05);
+    global.odeConfig.setParam("controlinterval",1);
+    global.odeConfig.setParam("gravity:",1); // normally at -9.81
+    // initialization
+    
+    Playground* playground = new Playground(odeHandle, osgHandle, osg::Vec3(25, 0.2, 1.5));
+    playground->setPosition(osg::Vec3(0,0,0)); // playground positionieren und generieren
+    global.obstacles.push_back(playground);
+    
+    for(int i=0; i<5; i++){
+      PassiveSphere* s = 
+	new PassiveSphere(odeHandle, 
+			  osgHandle.changeColor(Color(184 / 255.0, 233 / 255.0, 237 / 255.0)), 0.2);
+      s->setPosition(Pos(i*0.5-2, i*0.5, 1.0)); 
+      s->setTexture("Images/dusty.rgb");
+      global.obstacles.push_back(s);    
+    }
+        
+    OdeAgent *agent;
+    AbstractWiring* wiring;
+//    OdeRobot* robot;
+    AbstractController *controller;
+    
+    Wheelie *myWheelie;
+    WheelieConf myWheelieConf = DefaultWheelie::getDefaultConf();
+    //******* w H E E L I E *********/
+    myWheelieConf.jointLimit=M_PI/4;
+    myWheelieConf.motorPower=0.8;
+    myWheelieConf.frictionGround=0.04;
+    myWheelie = new Wheelie(odeHandle, osgHandle.changeColor(Color(0.0f,1.0f,0.0f)), myWheelieConf, "Wheelie1");
+    ((OdeRobot*) myWheelie)->place(Pos(-5,-5,0.2)); 
+    
+    InvertMotorNStepConf invertnconf = InvertMotorNStep::getDefaultConf();
+    invertnconf.cInit=2.0;
+    controller = new InvertMotorNStep(invertnconf);    
+    wiring = new One2OneWiring(new ColorUniformNoise(0.1));
+    agent = new OdeAgent( plotoptions );
+    agent->init(controller, myWheelie, wiring);
+    global.agents.push_back(agent);
+    global.configs.push_back(controller);
+    global.configs.push_back(myWheelie);   
+    myWheelie->setParam("gamma",/*gb");
+    global.obstacles.push_back(s)0.0000*/ 0.0);
+
+  }
+  // add own key handling stuff here, just insert some case values
+  virtual bool command(const OdeHandle&, const OsgHandle&, GlobalData& globalData, int key, bool down)
+  {
+    if (down) { // only when key is pressed, not when released
+      switch ( (char) key )
+	{
+	default:
+	  return false;
+	  break;
+	}
+    }
+    return false;
+  }
+  
+};
+
+int main (int argc, char **argv)
+{ 
+  ThisSim sim;
+  // run simulation
+  return sim.run(argc, argv) ? 0 : 1;
+}
