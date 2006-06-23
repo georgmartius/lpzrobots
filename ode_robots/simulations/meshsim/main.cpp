8c74e6463d51703ba5e0a0875456cbd05beeff45
robot3 <robot3>
2006-06-23 08:55:51 +0000

    mesh tested

--- ode_robots/simulations/meshsim/main.cpp
+++ ode_robots/simulations/meshsim/main.cpp
@@ -20,7 +20,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.1.2.2  2006-05-24 12:21:44  robot3
+ *   Revision 1.1.2.3  2006-06-23 08:55:51  robot3
+ *   mesh tested
+ *
+ *   Revision 1.1.2.2  2006/05/24 12:21:44  robot3
  *   demo of the new passive mesh
  *   CVS
  *
@@ -143,7 +146,7 @@ public:
 					   "cow.osg", // the filename of the mesh
 					   0.15, // the scale factor to be used
 					   1.0); // the mass of the mesh
-    myMesh->setPosition(osg::Vec3(-1,0,0));
+    myMesh->setPosition(osg::Vec3(-1,0,2.0f));
     global.obstacles.push_back(myMesh);
 
     
