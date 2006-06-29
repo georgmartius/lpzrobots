e38182c55ec6cae0f5960abdb854ea805116ba44
robot3 <robot3>
2006-06-29 16:32:44 +0000

    implementation of odeconfig moved to odeconfig.cpp

--- ode_robots/odeconfig.h
+++ ode_robots/odeconfig.h
@@ -20,7 +20,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.12.4.7  2006-05-08 12:12:36  robot3
+ *   Revision 1.12.4.8  2006-06-29 16:32:44  robot3
+ *   implementation of odeconfig moved to odeconfig.cpp
+ *
+ *   Revision 1.12.4.7  2006/05/08 12:12:36  robot3
  *   changes from Revision 1.40.4.27 reverted
  *
  *   Revision 1.12.4.6  2006/04/27 16:17:38  robot3
@@ -52,129 +55,54 @@
 #ifndef __ODECONFIG_H
 #define __ODECONFIG_H
 
-#include <ode/ode.h>
 #include <selforg/configurable.h>
 #include "odehandle.h"
-#include "mathutils.h"
 
 namespace lpzrobots {
 
-class OdeConfig : public Configurable {
-public:
-  OdeConfig() :
-    name ("Simulation Environment: ")
-  {
-    simStepSize=0.01;
-    controlInterval=1;
-    realTimeFactor=1.0;
-    noise=0.1;
-    gravity=-9.81;
-    drawInterval=calcDrawInterval25();
-    cameraSpeed=100;
-    motionPersistence=0.09;
-      // prepare name;
-    Configurable::insertCVSInfo(name, "$RCSfile$", "$Revision$");
-    videoRecordingMode=false;
-  }
+  class OdeConfig : public Configurable {
+  public:
+    
+    // creates new instance of OdeConfig with default values
+    OdeConfig();
+    
+    ~OdeConfig() {}
+    
+    virtual paramkey getName() const;
+    
+    virtual paramlist getParamList() const;
 
-  virtual ~OdeConfig(){}
-
-  virtual paramkey getName() const {
-    return name;
-  }
-
-  virtual paramlist getParamList() const{
-    paramlist list;
-    list.push_back(std::pair<paramkey, paramval> (std::string("noise"), noise));
-    list.push_back(std::pair<paramkey, paramval> (std::string("simstepsize"), simStepSize));
-    list.push_back(std::pair<paramkey, paramval> (std::string("realtimefactor"), realTimeFactor));
-    list.push_back(std::pair<paramkey, paramval> (std::string("drawinterval"), drawInterval));
-    list.push_back(std::pair<paramkey, paramval> (std::string("controlinterval"), controlInterval));
-    list.push_back(std::pair<paramkey, paramval> (std::string("cameraspeed"), cameraSpeed));
-    list.push_back(std::pair<paramkey, paramval> (std::string("motionpersistence"), motionPersistence));
-    list.push_back(std::pair<paramkey, paramval> (std::string("gravity"), gravity));
-    return list;
-  } 
-
-
-  paramval getParam(const paramkey& key) const {
-    if(key == "noise") return noise; 
-    else if(key == "simstepsize") return simStepSize; 
-    else if(key == "realtimefactor") return realTimeFactor; 
-    else if(key == "drawinterval") return drawInterval; 
-    else if(key == "controlinterval") return controlInterval; 
-    else if(key == "cameraspeed") return cameraSpeed; 
-    else if(key == "motionpersistence") return motionPersistence; 
-    else if(key == "gravity") return gravity; 
-    else  return 0.0;
- }
+    virtual paramval getParam(const paramkey& key) const;
         
-  bool setParam(const paramkey& key, paramval val){
-    if(key == "noise") noise = val; 
-    else if(key == "simstepsize") {
-      simStepSize=std::max(0.0000001,val); 
-      drawInterval=calcDrawInterval50();
-    }else if(key == "realtimefactor"){
-      realTimeFactor=std::max(0.0,val); 
-      //      if (videoRecordingMode)
-	drawInterval=calcDrawInterval25();
-	//      else
-	//	drawInterval=calcDrawInterval50();
-    }
-    else if(key == "drawinterval") drawInterval=(int)val; 
-    else if(key == "controlinterval") controlInterval=(int)val;
-    else if(key == "motionpersistence") motionPersistence=abs(val);
-    else if(key == "cameraspeed") cameraSpeed=(int)val; 
-    else if(key == "gravity") {
-      gravity=val; 
-      dWorldSetGravity ( odeHandle.world , 0 , 0 , gravity );
-    }
-    else return false;
-    return true;
-  }
+    virtual bool setParam(const paramkey& key, paramval val);
 
-  void setOdeHandle(const OdeHandle& odeHandle){
-    this->odeHandle = odeHandle;
-  }
+    virtual void setOdeHandle(const OdeHandle& odeHandle);
 
-  void setVideoRecordingMode(bool mode) {
-    if (mode)
-      drawInterval=calcDrawInterval25();
-    else
-      drawInterval=calcDrawInterval50();
-    videoRecordingMode=mode;
-  }
+    virtual void setVideoRecordingMode(bool mode);
 
-private:
-  /// calculates the draw interval with simStepSize and realTimeFactor so that we have 25 frames/sec
-  int calcDrawInterval25(){
-    if(realTimeFactor>0 && simStepSize>0){
-      return int(ceil(1/(25.0*simStepSize/realTimeFactor)));
-    }else return 50;
-  }
+  private:
+    /// calculates the draw interval with simStepSize and realTimeFactor so that we have 25 frames/sec
+    virtual int calcDrawInterval25();
 
-  /// calculates the draw interval with simStepSize and realTimeFactor so that we have 50 frames/sec
-  /// this is much better for graphical visualization (smoother)
-  int calcDrawInterval50(){
-    if(realTimeFactor>0 && simStepSize>0){
-      return int(ceil(1/(50.0*simStepSize/realTimeFactor)));
-    }else return 50;
-  }
+    /// calculates the draw interval with simStepSize and realTimeFactor so that we have 50 frames/sec
+    /// this is much better for graphical visualization (smoother)
+    virtual int calcDrawInterval50();
 
 
-public:
-  bool videoRecordingMode;
-  double simStepSize;
-  double realTimeFactor;
-  int drawInterval;
-  int controlInterval;
-  double motionPersistence;
-  double noise;
-  double gravity;
-  double cameraSpeed;
-  std::string name;
-  OdeHandle odeHandle;
-};
+  public:
+    bool videoRecordingMode;
+    double simStepSize;
+    double realTimeFactor;
+    int drawInterval;
+    int controlInterval;
+    double motionPersistence;
+    double noise;
+    double gravity;
+    double cameraSpeed;
+    bool drawBoundings;
+    std::string name;
+    OdeHandle odeHandle;
+  };
 
 }
 
