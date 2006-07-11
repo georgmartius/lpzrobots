5b9242638975a5962956db2ba2c49ed2aa3533f1
robot5 <robot5>
2006-07-11 04:24:19 +0000

    Walls now display repeating textures.

--- ode_robots/obstacles/playground.h
+++ ode_robots/obstacles/playground.h
@@ -21,7 +21,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.10.4.12  2006-06-29 16:39:55  robot3
+ *   Revision 1.10.4.13  2006-07-11 04:24:19  robot5
+ *   Walls now display repeating textures.
+ *
+ *   Revision 1.10.4.12  2006/06/29 16:39:55  robot3
  *   -you can now see bounding shapes if you type ./start -drawboundings
  *   -includes cleared up
  *   -abstractobstacle and abstractground have now .cpp-files
@@ -97,7 +100,11 @@
 
 #include "mathutils.h"
 #include "abstractground.h"
- 
+
+#include <osg/Geometry>
+#include <osg/Texture2D>
+#include <osg/TexEnv>
+
 namespace lpzrobots {
 
 //Fixme: playground creates collisions with ground and itself
@@ -110,8 +117,8 @@ protected:
   double factorlength2;
 
 public:
-  
-  Playground(const OdeHandle& odeHandle, const OsgHandle& osgHandle , 
+
+  Playground(const OdeHandle& odeHandle, const OsgHandle& osgHandle ,
 	     const osg::Vec3& dimension = osg::Vec3(7.0, 0.2, 0.5) ,
 	     double factorxy = 1, bool createGround=true):
     AbstractGround::AbstractGround(odeHandle, osgHandle, createGround){
@@ -124,22 +131,135 @@ public:
   };
 
  protected:
-  virtual void create(){
+  void realPlane(float x1, float y1, float z1,
+                 float x2, float y2, float z2,
+                 float x3, float y3, float z3,
+                 float x4, float y4, float z4,
+                 float tlength, float theight) {
+   osg::Vec3Array *coords = new osg::Vec3Array(4);
+   (*coords)[0].set(x1,y1,z1);
+   (*coords)[1].set(x2,y2,z2);
+   (*coords)[2].set(x3,y3,z3);
+   (*coords)[3].set(x4,y4,z4);
+
+   osg::Vec2Array *tcoords = new osg::Vec2Array(4);
+   (*tcoords)[0].set(   0.0f,theight);
+   (*tcoords)[1].set(   0.0f,   0.0f);
+   (*tcoords)[2].set(tlength,theight);
+   (*tcoords)[3].set(tlength,   0.0f);
+
+   osg::Geometry *geom = new osg::Geometry;
+   geom->setVertexArray(coords);
+   geom->setTexCoordArray(0,tcoords);
+   geom->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::TRIANGLE_STRIP,0,4));
+
+   osg::Texture2D *tex = new osg::Texture2D;
+   tex->setImage(osgDB::readImageFile(wallTextureFileName));
+   tex->setWrap(osg::Texture2D::WRAP_S,osg::Texture2D::REPEAT);
+   tex->setWrap(osg::Texture2D::WRAP_T,osg::Texture2D::REPEAT);
+   osg::StateSet *dstate = new osg::StateSet;
+   dstate->setMode(GL_LIGHTING,osg::StateAttribute::OFF);
+   dstate->setTextureAttributeAndModes(0,tex,osg::StateAttribute::ON);
+   dstate->setTextureAttribute(0,new osg::TexEnv);
+   geom->setStateSet(dstate);
+   osg::Geode *geode = new osg::Geode;
+   geode->addDrawable(geom);
+   osgHandle.scene->addChild(geode);
+  }
+
+  virtual void create() {
+   const float w=width;
+   const float h=height+0.0101f;
+   //inner walls
+   float x1=-0.5f*length+0.0002f, y1=-0.5f*length+0.0002f;
+   float x2= 0.5f*length-0.0002f, y2=-0.5f*length+0.0002f;
+   float x3= 0.5f*length-0.0002f, y3= 0.5f*length-0.0002f;
+   float x4=-0.5f*length+0.0002f, y4= 0.5f*length-0.0002f;
+   realPlane(x1-w,y1,0.0f,
+             x1-w,y1,   h,
+             x2+w,y2,0.0f,
+             x2+w,y2,   h,
+             length+w,h);
+   realPlane(x2,y2-w,0.0f,
+             x2,y2-w,   h,
+             x3,y3+w,0.0f,
+             x3,y3+w,   h,
+             length+w,h);
+   realPlane(x3+w,y3,0.0f,
+             x3+w,y3,   h,
+             x4-w,y4,0.0f,
+             x4-w,y4,   h,
+             length+w,h);
+   realPlane(x4,y4+w,0.0f,
+             x4,y4+w,   h,
+             x1,y1-w,0.0f,
+             x1,y1-w,   h,
+             length+w,h);
+
+   //outer walls
+   x1=-0.5f*length-w-0.0002f; y1=-0.5f*length-w-0.0002f;
+   x2= 0.5f*length+w+0.0002f; y2=-0.5f*length-w-0.0002f;
+   x3= 0.5f*length+w+0.0002f; y3= 0.5f*length+w+0.0002f;
+   x4=-0.5f*length-w-0.0002f; y4= 0.5f*length+w+0.0002f;
+   realPlane(x1,y1,0.0f,
+             x1,y1,   h,
+             x2,y2,0.0f,
+             x2,y2,   h,
+             length+w,h);
+   realPlane(x2,y2,0.0f,
+             x2,y2,   h,
+             x3,y3,0.0f,
+             x3,y3,   h,
+             length+w,h);
+   realPlane(x3,y3,0.0f,
+             x3,y3,   h,
+             x4,y4,0.0f,
+             x4,y4,   h,
+             length+w,h);
+   realPlane(x4,y4,0.0f,
+             x4,y4,   h,
+             x1,y1,0.0f,
+             x1,y1,   h,
+             length+w,h);
+
+   //top of walls
+   realPlane(x1,  y1,h+0.0001f,
+             x1,y1+w,h+0.0001f,
+             x2,  y2,h+0.0001f,
+             x2,y2+w,h+0.0001f,
+             length+w,0.4f*w);
+   realPlane(  x2,y2,h,
+             x2-w,y2,h,
+               x3,y3,h,
+             x3-w,y3,h,
+             length+w,0.4f*w);
+   realPlane(x3,  y3,h+0.0001f,
+             x3,y3-w,h+0.0001f,
+             x4,  y4,h+0.0001f,
+             x4,y4-w,h+0.0001f,
+             length+w,0.4f*w);
+   realPlane(  x4,y4,h,
+             x4+w,y4,h,
+               x1,y1,h,
+             x1+w,y1,h,
+             length+w,0.4f*w);
+
+
     Box* box;
     osg::Vec3 offset(- (length/2 + width/2),
 		     0,
-		     height/2+0.01f/*reduces graphic errors and ode collisions*/);
+		     height/2+0.01f); //reduces graphic errors and ode collisions
     box = new Box( width , (length * factorlength2) + 2 * width , height);
     box->init(odeHandle, 0, osgHandle, Primitive::Geom | Primitive::Draw);
     box->setPose(osg::Matrix::translate(offset) * pose);
-    box->getOSGPrimitive()->setTexture(wallTextureFileName);
+    //box->getOSGPrimitive()->setTexture(wallTextureFileName);
     obst.push_back(box);
 
     offset.x() = length/2 + width/2;
     box = new Box( width , (length * factorlength2) + 2 * width , height);
     box->init(odeHandle, 0, osgHandle, Primitive::Geom | Primitive::Draw);
     box->setPose(osg::Matrix::translate(offset) * pose);
-    box->getOSGPrimitive()->setTexture(wallTextureFileName);
+    //box->getOSGPrimitive()->setTexture(wallTextureFileName);
     obst.push_back(box);
 
     offset.x() = 0;
@@ -147,16 +267,16 @@ public:
     box = new Box( length, width, height);
     box->init(odeHandle, 0, osgHandle, Primitive::Geom | Primitive::Draw);
     box->setPose(osg::Matrix::translate(offset) * pose);
-    box->getOSGPrimitive()->setTexture(wallTextureFileName);
+    //box->getOSGPrimitive()->setTexture(wallTextureFileName);
     obst.push_back(box);
 
     offset.y() = (length*factorlength2)/2 +width/2;
     box = new Box( length, width, height);
     box->init(odeHandle, 0, osgHandle, Primitive::Geom | Primitive::Draw);
     box->setPose(osg::Matrix::translate(offset) * pose);
-    box->getOSGPrimitive()->setTexture(wallTextureFileName);
+    //box->getOSGPrimitive()->setTexture(wallTextureFileName);
     obst.push_back(box);
-    
+
     // size of groundplane
     ground_length=length;
 
