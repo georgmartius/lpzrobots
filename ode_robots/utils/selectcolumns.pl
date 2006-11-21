88e99cecb5617b164a07e5a8ed8ab6aa204d31e2
martius <martius>
2006-11-21 14:51:39 +0000

    multiple lines with fields

--- ode_robots/utils/selectcolumns.pl
+++ ode_robots/utils/selectcolumns.pl
@@ -17,25 +17,26 @@ while(<>){
     my $line=$_;
     if(/^#[^C]/ )
        { print $line; 
-    }elsif($line =~ /^#C/) 
-    { 
-       print STDERR "I use the following fields: \n";
-       print "#C";
-       my @Fields = split(/ /,$line);       
-       shift @Fields;
-       my $i =0;
-       foreach my $f (@Fields) {	     
-	   foreach my $a (@args) {	     
-	       if($f =~ /$a/) {
-		   print STDERR $f . " ";
-		   print " " . $f;
-		   push @columns, $i;
-	       }
-	   }
-	   $i++; 
-       }
-       print STDERR "\nColumns: " . (join(" ", @columns)) . "\n";
-       print "\n";
+     }elsif($line =~ /^#C/) 
+     { 
+	 @columns=();
+	 print STDERR "I use the following fields: \n";
+	 print "#C";
+	 my @Fields = split(/ /,$line);       
+	 shift @Fields;
+	 my $i =0;
+	 foreach my $f (@Fields) {	     
+	     foreach my $a (@args) {	     
+		 if($f =~ /$a/) {
+		     print STDERR $f . " ";
+		     print " " . $f;
+		     push @columns, $i;
+		 }
+	     }
+	     $i++; 
+	 }
+	 print STDERR "\nColumns: " . (join(" ", @columns)) . "\n";
+	 print "\n";	 
    } else{ # normal data line
        $line =~ s/^\s+//; # remove leading space
        my @values=split(/ /, $line);
