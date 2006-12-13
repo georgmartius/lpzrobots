06bf7c0f1692269d64a24c7767c2b729b85ecc3c
martius <martius>
2006-12-13 09:12:18 +0000

    better cmd's

--- ode_robots/utils/encodevideo.sh
+++ ode_robots/utils/encodevideo.sh
@@ -7,16 +7,16 @@ fi
 
 NAME=$1;
 
-for F in $NAME*.bmp; do echo "convert $F"; convert "$F" "${F%bmp}sgi"; rm "$F"; done
+# for F in $NAME*.bmp; do echo "convert $F"; convert "$F" "${F%bmp}sgi"; rm "$F"; done
 
 echo -e "*********************** mjpeg encoding **************************";
-mencoder mf://$NAME*.sgi -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mjpeg -oac copy -o $NAME.mjpeg
+mencoder mf://$NAME*.jpg -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mjpeg -oac copy -o $NAME.mjpeg
 echo -e "*********************** to mpeg2 **************************";
-transcode -i $NAME.mjpeg -o $NAME.mpg -y mpeg2enc 
-mv -f $NAME.mpg.m1v $NAME.mpg 
+transcode -i $NAME.mjpeg -o $NAME -y mpeg2enc,null -F 3 -w 800 
 
 echo -e "*********************** to mpeg4 xvid 4 **************************";
-transcode -i $NAME.mjpeg -o $NAME.avi -y xvid4 
+transcode -i $NAME.mjpeg -o $NAME.avi -y xvid4,null -w 400 
+
 
 
 
