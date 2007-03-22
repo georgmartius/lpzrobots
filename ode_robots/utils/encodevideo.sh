19deb860951d4360587721ebf3c914268c3bf258
der <der>
2007-03-22 09:05:30 +0000

    sgi input type changed to jpg for mjpeg encoder

--- ode_robots/utils/encodevideo.sh
+++ ode_robots/utils/encodevideo.sh
@@ -6,11 +6,13 @@ if test -z "$1"; then
 fi
 
 NAME=$1;
+#NAME="frame";
 
 # for F in $NAME*.bmp; do echo "convert $F"; convert "$F" "${F%bmp}sgi"; rm "$F"; done
 
 echo -e "*********************** mjpeg encoding **************************";
-mencoder mf://$NAME*.jpg -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mjpeg -oac copy -o $NAME.mjpeg
+#mencoder mf://$NAME*.jpg -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mjpeg -oac copy -o $NAME.mjpeg
+mencoder mf://frame*.jpg -mf fps=25:type=jpg -ovc lavc -lavcopts vcodec=mjpeg -oac copy -o $NAME.mjpeg
 echo -e "*********************** to mpeg2 **************************";
 transcode -i $NAME.mjpeg -o $NAME -y mpeg2enc,null -F 3 -w 800 
 
