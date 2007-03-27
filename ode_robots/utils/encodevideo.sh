55f740e1599e0543a33642634667177c67b5638f
martius <martius>
2007-03-27 10:10:56 +0000

    bugfixing

--- ode_robots/utils/encodevideo.sh
+++ ode_robots/utils/encodevideo.sh
@@ -7,18 +7,18 @@ fi
 
 NAME=$1;
 TARGET=$1;
-if test -z "$2"; then 
+if test -n "$2"; then 
     TARGET=$2;
 fi
 
 # for F in $NAME*.bmp; do echo "convert $F"; convert "$F" "${F%bmp}sgi"; rm "$F"; done
 # copy first frame as screenshot
-FRAME=`ls "$NAME*" -1 | head -1`;
+FRAME=`ls $NAME* -1 | head -1`;
 cp "$FRAME" "$TARGET.jpg";
 
 echo -e "*********************** mjpeg encoding **************************";
 #mencoder mf://$NAME*.jpg -mf fps=25:type=sgi -ovc lavc -lavcopts vcodec=mjpeg -oac copy -o $NAME.mjpeg
-mencoder mf://frame*.jpg -mf fps=25:type=jpg -ovc lavc -lavcopts vcodec=mjpeg -oac copy -o "$TARGET.mjpeg"
+mencoder mf://$NAME*.jpg -mf fps=25:type=jpg -ovc lavc -lavcopts vcodec=mjpeg -oac copy -o "$TARGET.mjpeg"
 echo -e "*********************** to mpeg2 **************************";
 transcode -i "$TARGET.mjpeg" -o "$TARGET" -y mpeg2enc,null -F 3 -w 800 
 
