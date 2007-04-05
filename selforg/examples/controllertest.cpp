861e022e9d8230bc377272e3c8a9f2f5bf38968f
martius <martius>
2007-04-05 15:20:48 +0000

    new test for all controllers (good for valgrind)

--- selforg/examples/controllertest.cpp
+++ selforg/examples/controllertest.cpp
@@ -1,54 +1,92 @@
+#include<assert.h>
 #include<iostream>
 #include<vector>
 using namespace std;
 
-#include <selforg/sinecontroller.h>
-#include <selforg/invertnchannelcontroller.h>
-#include <selforg/invertmotornstep.h>
+#include "selforg/sinecontroller.h" 
+#include "selforg/invertmotorspace.h" 
+#include "selforg/invertmotornstep.h" 
+#include "selforg/invertmotorbigmodel.h" 
+#include "selforg/multilayerffnn.h" 
+#include "selforg/dercontroller.h" 
 
-int main(){
-  //AbstractController* controller = new InvertNChannelController(5 );
-  //  AbstractController* controller = new SineController();
-  InvertMotorNStep* controller = new InvertMotorNStep();
-  controller->init(2,2);
-  controller->print(stderr,0);
-  sensor sensors[3] = {0.1,-0.1,0.3};
-  motor motors[2];
-  // short circuit
-  for(int i=0; i < 5; i++){ 
-    sensors[0]=motors[0]+(double(rand())/RAND_MAX-0.5)*0.3;
-    sensors[1]=motors[1]+(double(rand())/RAND_MAX-0.5)*0.3;
-    sensors[2]=motors[1]+(double(rand())/RAND_MAX-0.5)*0.3;
-    controller->stepNoLearning(sensors, 2, motors, 2);
-  }
-  for(int i=0; i < 50000; i++){
-    sensors[0]=motors[0]+(double(rand())/RAND_MAX-0.5)*0.3;
-    sensors[1]=motors[1]+(double(rand())/RAND_MAX-0.5)*0.3;
-    sensors[2]=motors[1]+(double(rand())/RAND_MAX-0.5)*0.3;
-    //    cout << i << " S0: " << sensors[0] << ", " <<" S1: " << sensors[1];
-    list<Inspectable::iparamval> list_ = controller->getInternalParams();  
-    vector<Inspectable::iparamval> v(list_.begin(), list_.end());  
-//     cout << i << " C00: " << list_[6] << ", " <<" C01: " << list_[7] << ", " <<
-//       " C02: " << list_[8] << ", " <<" C10: " << list_[9] << ", " <<
-//       " C11: " << list_[10] << ", " <<" C12: " << list_[11] << endl;
-    cout << i << " C00: " << v[4] << ", " <<" C01: " << v[5] << ", " <<
-      " C10: " << v[6] << ", " <<" C11: " << v[7]  << endl;
+const int MNumber = 2;
+const int SNumber = 2;
 
-    controller->step(sensors, 2, motors, 2);    
-    cout << i << " Motor values: " << motors[0] << ", " << motors[1] << endl;
-  }
-  cout << "Motor values: " << motors[0] << ", " << motors[1] << endl;
+/** The robot control should go here
+    @param sensors list of sensor values (to be written) (doubles)
+    @param sensornumber length of the sensors vector
+    @param motors list of motor values (doubles) (to send to the robot)
+    @param motornumber length of the motors vector
 
-//   double data[8] = {0.01,1.1,0.05,0.3,0.5,0.3,0.4,0.2};
-//   matrix::Matrix m(2,4,data);
-//   matrix::Matrix m2(2,4,data);
-//   cout << m << endl << endl;
-//   controller->kwtaInhibition(m,1,0.1);  
-//   cout <<m << endl<< endl;
-//   controller->kwtaInhibition(m2,4,0.1);  
-//   cout <<m2 << endl;
+ */
+void myrobot(sensor* sensors, int sensornumber, const motor* motors, int motornumber){  
+  assert(sensornumber >= 2 && motornumber >= 2); // check dimensions
+  //the robot consits here just of a bit noise
+  sensors[0]=motors[0]+(double(rand())/RAND_MAX-0.5)*0.3;
+  sensors[1]=motors[1]+(double(rand())/RAND_MAX-0.5)*0.3;
 
-  return 0;
 }
 
 
+int test(AbstractController* controller){
+
+  controller->init(2,2); // initialise with 2 motors and 2 sensors
+  controller->setParam("epsA",0.01); // set parameter epsA (learning rate for Model A)
+  controller->print(stderr,0); // print parameters (see Configurable) to stderr
+
+  
+  // sensor and motor arrays (doubles*)
+  sensor sensors[SNumber];
+  motor motors[MNumber];
+  
+  memset(motors,0,sizeof(motor)*MNumber);  // clear motors
+
+  // the robot is here respresented by the function myrobot
+  for(int i=0; i < 100; i++){
+    // call robot with motors and receive sensors 
+    myrobot(sensors, SNumber, motors, MNumber);
+    cout << i << " S0: " << sensors[0] << ", " <<" S1: " << sensors[1];
+
+    // print some internal parameters of the controller
+    list<Inspectable::iparamval> list_ = controller->getInternalParams();  
+    vector<Inspectable::iparamval> v(list_.begin(), list_.end());  
+    cout << i << " C00: " << v[4] << ", " <<" C01: " << v[5] << ", " <<
+      " C10: " << v[6] << ", " <<" C11: " << v[7]  << endl;
+    
+    // call controller with sensors and receive motors (both dimension 2)    
+    controller->step(sensors, SNumber, motors, MNumber); 
+    cout << i << " Motor values: " << motors[0] << ", " << motors[1] << endl;
+  }
+  delete controller;
+  return 1;
+}
+
+
+int main(){
+  AbstractController* controller;
+
+  controller= new SineController;
+  test(controller);
+  controller = new InvertMotorSpace(10);
+  test(controller);
+  controller = new InvertMotorNStep();
+  test(controller);
+
+  InvertMotorBigModelConf cc = InvertMotorBigModel::getDefaultConf();  
+  std::vector<Layer> layers;
+  layers.push_back(Layer(6, 0.5 , FeedForwardNN::tanh));
+  layers.push_back(Layer(2,0.5));
+  MultiLayerFFNN* net = new MultiLayerFFNN(0.01, layers);
+  cc.model = net;
+  cc.modelInit = 1.0;
+  cc.useS = true;
+  controller = new InvertMotorBigModel(cc);   
+  test(controller);
+
+  controller = new DerController();
+  test(controller);
+
+  return 0;
+
+}
