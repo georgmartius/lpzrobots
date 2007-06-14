f50cf7f41cf80808340a37ba7151cf763075d778
martius <martius>
2007-06-14 13:53:11 +0000

    a robot, that just replays a logfile

---
+++ ode_robots/robots/replayrobot.cpp
@@ -0,0 +1,153 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1  2007-06-14 13:53:25  martius
+ *   a robot, that just replays a logfile
+ *
+
+ ***************************************************************************/
+#include <assert.h>
+
+#include "simulation.h"
+
+#include "replayrobot.h"
+using namespace std;
+using namespace matrix;
+
+namespace lpzrobots {
+
+  ReplayRobot::ReplayRobot(const OdeHandle& odeHandle, 
+		 const OsgHandle& osgHandle, 
+		 const char* filename)
+    : OdeRobot(odeHandle, osgHandle, "ReplayRobot", "$Id$"),
+      filename(filename){
+
+    f=fopen(filename,"r");
+    if(!f){
+      cerr<< "ReplayRobot: error while opening file " << filename << endl;
+      exit(1);
+    }
+    if(!parseDataFileForHeader(f, sensorStart, sensorEnd,  motorStart, motorEnd)){
+      cerr<< "ReplayRobot: error while seaching for header in file " << filename << endl;
+      exit(1);
+    }
+    printf("Test: %i, %i, %i, %i\n", sensorStart, sensorEnd, motorStart, motorEnd);
+  
+  };
+
+  ReplayRobot::~ReplayRobot(){
+    if(f) fclose(f);
+  }
+
+  void ReplayRobot::setMotors(const motor* _motors, int motornumber){
+  };
+
+  int ReplayRobot::getSensors(sensor* s, int sensornumber){
+    assert(sensornumber == (sensorEnd-sensorStart + 1));  
+    if(!parseDataLine(sensors,f)){
+      cout << "ReplayRobot: no datafile in file" << endl;      
+    }else{
+      sensors=sensors.rows(sensorStart, sensorEnd);
+    }
+    sensors.convertToBuffer(s,sensorEnd-sensorStart + 1);
+    return sensorEnd-sensorStart + 1;
+  };
+
+  bool ReplayRobot::parseDataFileForHeader(FILE* f, int & sensorstart, int& sensorend,  int& motorstart, int& motorend){
+    char buffer[1024];  
+    int i;
+    sensorstart=-1;
+    sensorend=-1;
+    motorstart=-1;
+    motorend=-1;
+    
+    while(fgets(buffer, 1024, f)) {    
+      if(buffer[0]=='#' && buffer[1]=='C'){
+	// scan line and return      
+	i=0;
+	char* p;
+	p=strtok(buffer," ");
+	if(!p) return false; // frist one is #C
+	while((p=strtok(NULL," "))!=NULL )  {
+	  if(p[0]=='x' && p[1]=='['){
+	    if(sensorstart==-1) sensorstart=i;
+	    sensorend=i;
+	  }
+	  if(p[0]=='y' && p[1]=='['){
+	    if(motorstart==-1) motorstart=i;
+	    motorend=i;
+	  }      
+	  i++;
+	}
+	return true;
+      }
+    }
+    return false;
+  }
+
+  bool ReplayRobot::isEmpty(const char* c){
+    const char* p = c;
+    bool foundsomething = false;
+    while(*p != 0){
+      if(*p > ' ') foundsomething = true;
+      p++;
+    }
+    return !foundsomething;
+  }
+
+
+  bool ReplayRobot::check4Number(const char* c){
+    const char* p = c;
+    while(*p != 0){
+      if(*p >= '0' && *p <= '9') return true;
+      p++;
+    }
+    return false;
+  }
+
+  bool ReplayRobot::parseDataLine(Matrix& data, FILE* f){
+    char buffer[1024];  
+    int i;
+    double dat[1024];
+    while(fgets(buffer, 1024, f)){    
+      if(buffer[0]=='#' || isEmpty(buffer)){
+	continue;
+      }else{
+	i=0;
+	char* p;
+	p=strtok(buffer," ");
+	if(!p) return false;
+	dat[i] = atof(p);    
+	i++;
+	while((p=strtok(NULL," "))!=NULL )  {
+	  if(!check4Number(p)) continue;
+	  dat[i] = atof(p);
+	  i++;
+	};
+	data.set(i,1,dat);
+	return true;
+      }
+    };
+    return false;
+  }
+  
+}
---
+++ ode_robots/robots/replayrobot.h
@@ -0,0 +1,107 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1  2007-06-14 13:53:11  martius
+ *   a robot, that just replays a logfile
+ *
+ *
+ ***************************************************************************/
+#ifndef __REPLAYROBOT_H
+#define __REPLAYROBOT_H
+
+
+#include "oderobot.h"
+
+namespace lpzrobots {
+
+  /**
+   * 
+   */
+  class ReplayRobot : public OdeRobot{
+  public:
+    ReplayRobot(const OdeHandle& odeHandle, const OsgHandle& osgHandle, const char* filename);
+
+    ~ReplayRobot();
+
+    virtual void update() {}
+
+    /** sets the pose of the vehicle
+	@param pose desired 4x4 pose matrix
+    */
+    virtual void place(const osg::Matrix& pose) {}
+
+    /** returns actual sensorvalues
+	@param sensors sensors scaled to [-1,1] 
+	@param sensornumber length of the sensor array
+	@return number of actually written sensors
+    */
+    virtual int getSensors(sensor* sensors, int sensornumber);
+
+    /** sets actual motorcommands
+	@param motors motors scaled to [-1,1] 
+	@param motornumber length of the motor array
+    */
+    virtual void setMotors(const motor* motors, int motornumber);
+
+    /** returns number of sensors
+     */
+    virtual int getSensorNumber() {return sensorEnd - sensorStart + 1; }
+
+    /** returns number of motors
+     */
+    virtual int getMotorNumber() {return motorEnd - motorStart + 1; }
+
+    /** this function is called in each timestep. It should perform robot-internal checks, 
+	like space-internal collision detection, sensor resets/update etc.
+	@param globalData structure that contains global data from the simulation environment
+    */
+    virtual void doInternalStuff(const GlobalData& globalData) {}
+
+    virtual bool collisionCallback(void *data, dGeomID o1, dGeomID o2) { return false;}
+
+  protected:
+    /** the main object of the robot, which is used for position and speed tracking */
+    virtual Primitive* getMainPrimitive() const { return 0; }
+
+    static bool parseDataFileForHeader(FILE* f, int & sensorstart, int& sensorend,  int& motorstart, int& motorend);
+    static bool parseDataLine(matrix::Matrix& data, FILE* f);
+    static bool isEmpty(const char* c);
+    static bool check4Number(const char* c);
+
+
+  protected:
+    int sensorStart;
+    int sensorEnd;
+    int motorStart;
+    int motorEnd;
+    
+    matrix::Matrix sensors;
+    const char* filename;
+    FILE* f;
+
+
+  };
+
+}
+
+#endif
+ 
