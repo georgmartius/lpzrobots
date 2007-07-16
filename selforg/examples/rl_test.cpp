08cc7a293c1b00e46de2992d1820483da28d6624
martius <martius>
2007-07-16 20:19:47 +0000

    qlearning works nice now

--- selforg/examples/rl_test.cpp
+++ selforg/examples/rl_test.cpp
@@ -8,21 +8,43 @@ using namespace matrix;
 
 
 void testQLearning(){
-  QLearning q(0.1,0.9,0.05, 1);
-  q.init(5,2);
+  int size=10;
+  QLearning q(0.2,0.9,0.1, 5);
+  q.init(size,2);
   cout << "Q Learning 5x2\n";
+  cerr << q.getQ();
     
   int x=0;
-  for(int i=0; i < 100; i++){
-    int a = q.select(x);
+  int a = 1;
+  for(int i=0; i < 1000; i++){
     if(a==0) x--;
     if(a==1) x++;
     if(x<0) x=0;
-    if(x>4) x=4;
-    double r = (x==4) ? 1 : 0;
-    q.learn(x,a,r);        
-    if(x==4) x=0;
+    if(x>=size) x=size-1;
+    printf("action: %i state: %i\n",a, x);
+    double r = (x==size-1) ? 1 : 0;
+    a = q.select(x);
+    q.learn(x,a,r);
+    if(x==size-1) {
+      x=0;
+      q.learn(x,a,r);
+      q.reset();
+      
+    }
   }
+
+//   for(int i=0; i < 6; i++){
+//     int a = q.select(x);
+//     if(a==0) x--;
+//     if(a==1) x++;    
+//     if(x<0) x=0;
+//     if(x>4) x=4;
+//     x=i%5;
+//     printf("action: %i state: %i\n",a, x);
+//     double r = (x==4) ? 1 : 0;
+//     q.learn(x,a,r);        
+//     if(x==4) x=0;
+//   }
 
   cerr << q.getQ();
   fprintf(stderr,"\nReward: %lf\n", q.collectedReward);
@@ -35,7 +57,7 @@ void testQLearning(){
 int main(){
   srand(time(0));
   
-   cout << "******************** TEST Q-Learning\n";
+  cout << "******************** TEST Q-Learning\n";
   testQLearning();
  
   return 0;
