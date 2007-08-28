c7c31f00f50856cf5ef6cfe8ea93f772fbcb182d
martius <martius>
2007-08-28 09:23:14 +0000

    initial

---
+++ ode_robots/obstacles/complexplayground.cpp
@@ -0,0 +1,180 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *    frankguettler@gmx.de                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
+ *                                                                         *
+ *  DESCRIPTION                                                            *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1  2007-08-28 09:23:20  martius
+ *   initial
+ *
+ *
+ *                                                                 *
+ ***************************************************************************/
+
+#include "complexplayground.h"
+#include "primitive.h"
+#include "osgprimitive.h"
+#include <selforg/stl_adds.h>
+#include <selforg/matrix.h>
+#include <selforg/controller_misc.h>
+
+namespace lpzrobots {
+
+  using namespace std;
+  using namespace matrix;
+
+  int PolyLine::parse(list<char*> lines){
+    int pen_color, fill_color;
+    list<char*>::iterator l = lines.begin();
+    if(lines.size()<2) return 1;    
+    int r = sscanf(*l,"%i %i %i %i %i %i %i", &object_code, &sub_type,&line_style, &thickness, 
+		   &pen_color, &fill_color, &depth);
+    int i=0;
+    int dat[2];
+    if(r==7 && object_code==2){
+      l++;
+      int linecnt=1;
+      while(l!=lines.end()){
+	char* line = *l;
+	if(line[0]=='\t'){
+	  char* p;
+	  p=strtok(line+1," ");
+	  if(!p) return false;
+	  dat[i] = atoi(p);    
+	  i++;
+	  while((p=strtok(NULL," "))!=NULL )  {
+	    dat[i] = atoi(p);
+	    i++;
+	    if(i==2){
+	      points.push_back(Pos(dat[0]/450.0,-dat[1]/450.0,0));
+	      i=0;
+	    }
+	  };
+	}else break;
+	l++;
+	linecnt++;
+      }
+      return linecnt;
+    }else return 1;    
+  }
+
+  void PolyLine::print(){
+    printf("%i %i %i %i\n\t", object_code, sub_type, line_style, thickness);
+    FOREACH(list<Pos>, points, p){
+      printf("%f %f", p->x(), p->y() );
+    }
+    printf("\n");
+  }
+  
+
+  ComplexPlayground::ComplexPlayground(const OdeHandle& odeHandle, const OsgHandle& osgHandle , 
+				       const string filename,
+				       double factor, double heightfactor, bool createGround)
+    : AbstractGround(odeHandle, osgHandle, createGround, 1,1,0.1), 
+      filename(filename), factor(factor), heightfactor(heightfactor) {
+    
+    FILE* f=fopen(filename.c_str(),"r");
+    assert(f);
+    list<char*> lines;    
+    char* buffer = (char*)malloc(sizeof(char)*4096);
+    while(fgets(buffer, 4096, f)){
+      lines.push_back(buffer);
+      buffer = (char*)malloc(sizeof(char)*4096);
+    }
+    while(lines.size()>0){
+      PolyLine p;     
+      int consumed = p.parse(lines);
+      if(consumed>1){	
+	polylines.push_back(p);	
+      }
+      for(int i=0; i<consumed; i++){
+	free(*lines.begin());
+	lines.pop_front();
+      }
+    }
+    FOREACH(list<PolyLine>, polylines, p){
+      p->print();
+    }
+  };
+
+
+  void ComplexPlayground::create(){
+    PolyLine boundary;
+    bool hasboundary=false;
+    FOREACH(list<PolyLine>, polylines, p){
+      if(p->line_style==1){
+	boundary = *p;
+	hasboundary=true;
+	break;
+      }
+    }
+    if(hasboundary){
+      int l = boundary.points.size();
+      Matrix xs(l,1);
+      Matrix ys(l,1);
+      int i=0;
+      FOREACH(list<Pos>, boundary.points, p){
+	xs.val(i,0)=p->x();
+	ys.val(i,0)=p->y();
+      }
+      double xsize = max(xs.map(fabs));
+      double ysize = max(ys.map(fabs));
+      groundLength = 2*xsize*factor;
+      groundWidth  = 2*ysize*factor;
+    }    
+    createGround();
+    
+    FOREACH(list<PolyLine>, polylines, p){
+      //      if(p->line_style==0){
+	createPolyline(*p);
+	//      }
+    }        
+    obstacle_exists=true;
+  };
+  
+  void ComplexPlayground::createPolyline(const PolyLine& polyline){
+    typedef list<pair<Pos,Pos> > pospairs;
+    pospairs pairs;
+    Pos ps[2];
+    int i=0;
+    FOREACHC(list<Pos>, polyline.points, p){
+      ps[i%2]=*p;
+      if(i>=1){
+	pairs.push_back(pair<Pos, Pos>(ps[(i-1)%2]*factor, ps[i%2]*factor));
+      }
+      i++;
+    }
+    FOREACHC(pospairs, pairs, p){
+      Pos size = p->second - p->first;
+      double length = sqrt(size.x()*size.x()+size.y()*size.y());
+      Pos offset = (p->second + p->first)/2;
+      Box* box = new Box( length, polyline.thickness*.03175*factor , polyline.depth*heightfactor); 
+      box->init(odeHandle, 0, osgHandle, Primitive::Geom | Primitive::Draw);
+      double angle = atan2(size.y(),size.x());
+      box->setPose(osg::Matrix::rotate(angle,Pos(0,0,1)) *  osg::Matrix::translate(offset) * pose);
+      box->getOSGPrimitive()->setTexture(wallTextureFileName);
+      obst.push_back(box);
+    }
+  }
+
+}
---
+++ ode_robots/obstacles/complexplayground.h
@@ -0,0 +1,188 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *    frankguettler@gmx.de                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   Playground that is loaded from xfig file with polylines               *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1  2007-08-28 09:23:14  martius
+ *   initial
+ *
+ *                                                                 *
+ ***************************************************************************/
+#ifndef __COMPLEXPLAYGROUND_H
+#define __COMPLEXPLAYGROUND_H
+
+#include "mathutils.h"
+#include "abstractground.h"
+#include "pos.h"
+ 
+namespace lpzrobots {
+
+// Taken from XFIG documentation
+// .....
+// (2) The first non-comment line consists of the following:
+
+//         string  orientation             ("Landscape" or "Portrait")
+//         string  justification           ("Center" or "Flush Left")
+//         string  units                   ("Metric" or "Inches")
+//         string  papersize               ("Letter", "Legal", "Ledger", "Tabloid",
+//                                          "A", "B", "C", "D", "E",
+//                                          "A4",   "A3", "A2", "A1", "A0" and "B5")
+//         float   magnification           (export and print magnification, %)
+//         string  multiple-page           ("Single" or "Multiple" pages)
+//         int     transparent color       (color number for transparent color for GIF
+//                                          export. -3=background, -2=None, -1=Default,
+//                                          0-31 for standard colors or 32- for user colors)
+//         # optional comment              (An optional set of comments may be here,
+//                                          which are associated with the whole figure)
+//         int     resolution coord_system (Fig units/inch and coordinate system:
+//                                            1: origin at lower left corner (NOT USED)
+//                                            2: upper left)
+
+//     Fig_resolution is the resolution of the figure in the file.
+//     Xfig will always write the file with a resolution of 1200ppi so it
+//     will scale the figure upon reading it in if its resolution is different
+//     from 1200ppi.  Pixels are assumed to be square.
+
+//     Xfig will read the orientation string and change the canvas to match
+//     either the Landscape or Portrait mode of the figure file.
+
+//     The units specification is self-explanatory.
+
+//     The coordinate_system variable is ignored - the origin is ALWAYS the
+//     upper-left corner.
+
+//     ** Coordinates are given in "fig_resolution" units.
+//     ** Line thicknesses are given in 1/80 inch (0.3175mm) or 1 screen pixel.
+//        When exporting to EPS, PostScript or any bitmap format (e.g. GIF),  the
+//        line thickness is reduced to 1/160 inch (0.159mm) to "lighten" the look.
+//     ** dash-lengths/dot-gaps are given in 80-ths of an inch.
+
+
+
+
+// (3) The rest of the file contains various objects.  An object can be one
+//     of six classes (or types).
+
+//         0)      Color pseudo-object.
+//         1)      Ellipse which is a generalization of circle.
+//         2)      Polyline which includes polygon and box.
+//         3)      Spline which includes
+//                 closed/open approximated/interpolated/x-spline spline.
+//         4)      Text.
+//         5)      Arc.
+//         6)      Compound object which is composed of one or more objects.
+// ........
+//    (3.5) POLYLINE
+
+//     First line:
+//         type    name                    (brief description)
+//         ----    ----                    -------------------
+//         int     object_code             (always 2)
+//         int     sub_type                (1: polyline
+//                                          2: box
+//                                          3: polygon
+//                                          4: arc-box)
+//                                          5: imported-picture bounding-box)
+//         int     line_style              (enumeration type)
+//         int     thickness               (1/80 inch)
+//         int     pen_color               (enumeration type, pen color)
+//         int     fill_color              (enumeration type, fill color)
+//         int     depth                   (enumeration type)
+//         int     pen_style               (pen style, not used)
+//         int     area_fill               (enumeration type, -1 = no fill)
+//         float   style_val               (1/80 inch)
+//         int     join_style              (enumeration type)
+//         int     cap_style               (enumeration type, only used for POLYLINE)
+//         int     radius                  (1/80 inch, radius of arc-boxes)
+//         int     forward_arrow           (0: off, 1: on)
+//         int     backward_arrow          (0: off, 1: on)
+//         int     npoints                 (number of points in line)
+
+//     Forward arrow line: same as ARC object
+
+//     Backward arrow line: same as ARC object
+
+//     For picture (type 5) the following line follows:
+//         type    name                    (brief description)
+//         ----    ----                    -------------------
+//         boolean flipped                 orientation = normal (0) or flipped (1)
+//         char    file[]                  name of picture file to import
+
+//     Points line(s).  The x,y coordinates follow, any number to a line, with
+//     as many lines as are necessary:
+//         type    name                    (brief description)
+//         ----    ----                    -------------------
+//         int     x1, y1                  (Fig units)
+//         int     x2, y2                  (Fig units)
+//           .
+//           .
+//         int     xnpoints ynpoints       (this will be the same as the 1st
+//                                         point for polygon and box)
+
+  class PolyLine {
+  public:
+    PolyLine(){};
+    /** try to parses a PolyLine at the start of the list of lines and returns the consumed lines
+	1: no success; >=2 success
+     */
+    int parse(std::list<char*> lines);
+    void print();
+
+    int     object_code; //             (always 2)
+    int     sub_type;    //             (we require 1: polyline)                                        
+    int     line_style;  //             (0: normal wall, 1: border, rest ignored)
+    int     thickness;   //             (1/80 inch means factor of .3175 )
+    int     depth;       //             (multiple of height)
+    std::list<Pos> points;
+  };
+
+  /** Playground that uses xfig file with polylines
+      linetype 0 is normal line
+      linetype 1 is border
+      thickness is used as well, thickness is multiplied with wallthickness
+  */
+  class ComplexPlayground : public AbstractGround {
+
+  protected:
+
+    std::string filename;
+    double length, factor, heightfactor;
+    
+    std::list<PolyLine> polylines;
+   
+  public:
+  
+    ComplexPlayground(const OdeHandle& odeHandle, const OsgHandle& osgHandle , 
+		      const std::string filename, 
+		      double factor = 1, double heightfactor=0.02, bool createGround=true);
+
+    void createPolyline(const PolyLine&);
+
+  protected:
+    virtual void create();   
+    
+  };
+
+}
+
+#endif
