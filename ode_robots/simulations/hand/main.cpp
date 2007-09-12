d5c5ed1070bfe1c98dd02406e694cc269da6214d
fhesse <fhesse>
2007-09-12 11:32:14 +0000

    partly cleaned up changed to use of fixator joint

--- ode_robots/simulations/hand/main.cpp
+++ ode_robots/simulations/hand/main.cpp
@@ -20,7 +20,11 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.6  2007-07-11 10:00:19  fhesse
+ *   Revision 1.7  2007-09-12 11:32:14  fhesse
+ *   partly cleaned up
+ *   changed to use of fixator joint
+ *
+ *   Revision 1.6  2007/07/11 10:00:19  fhesse
  *   texture of hand palm deactivated
  *   new camera position
  *
@@ -97,223 +101,96 @@ motor teaching[3];
 class ThisSim : public Simulation {
 public:
 
+  Joint* fixator;
+
   // starting function (executed once at the beginning of the simulation loop)
   void start(const OdeHandle& odeHandle, const OsgHandle& osgHandle, GlobalData& global) 
   {
-    //----for the first hand, the view from the right
-    //    setCameraHomePos(Pos(8.28458, -0.182447, 3.5),  Pos(84.1294, -6.46484, 0));
-    //----for both hands, the view from the top
-    //setCameraHomePos(Pos(0.608728, -9.5055, 14.8199),  Pos(11.8571, -45.3423, 0));
-    //----for the first hand the view from the top
-    //setCameraHomePos(Pos(3.25291, -0.147082, 14.8199),  Pos(-92.7015, -98.0146, 0));
-    //----to observe the ball on the palm of the hand.
-    //setCameraHomePos(Pos(-4.56577, -0.127742, 5.61211),  Pos(-88.1353, -45.1827, 0));
-    // the second hand 
-    //setCameraHomePos(Pos(-17.9977, 3.4133, 17.9206),  Pos(-88.7149, -43.5015, 0));
     setCameraHomePos(Pos(9.48066, -6.96316, 9.95062),  Pos(56.9904, -10.8096, 0));
 
 
     // initialization
-    // - set noise to 0.1
-    // - register file chess.ppm as a texture called chessTexture (used for the wheels)
     global.odeConfig.noise=0.05;
-    global.odeConfig.setParam("gravity", -0.5);// dWorldSetGravity (world,0,0,-0.5);
+    global.odeConfig.setParam("gravity", -0.5);
     global.odeConfig.setParam("controlinterval", 5);
 
 
-    // for(int i=0; i<50; i++){
-    //      PassiveSphere* s = new PassiveSphere(odeHandle, osgHandle.changeColor(Color(0.0,1.0,0.0)), 0.5);
-    //       s->setPosition(osg::Vec3(-4+(i/10),-4+(i%10),1)); 
-    //       global.obstacles.push_back(s);    
-    //     }
-    
-    OdeRobot *hand,*hand1;
-    AbstractController *controller,*controller1;
-    AbstractWiring *wiring,*wiring1;
-    OdeAgent *agent,*agent1;
+    OdeRobot *hand;
+    AbstractController *controller;
+    AbstractWiring *wiring;
+    OdeAgent *agent;
 
     HandConf conf = Hand::getDefaultConf();  
     conf.invert = 1; 
     conf.irRange = 0.7;
     conf.set_typ_of_motor = With_servo_motor;
     conf.show_contacts = true;
-    conf.ir_sensor_used = true;
+    conf.ir_sensor_used = false;
     conf.number_of_ir_sensors = conf.ir_sensor_used*15;
     conf.Draw_part_of_ir_sensor = Draw_All;
-    conf.hand_is_drawn_under_angel = Is_Draw_under_180_degree;
     conf.jointLimit1 = -M_PI/180;
     conf.jointLimit2 = M_PI/2	;
     conf.servo_motor_Power = 1.2;
     conf.finger_winkel = M_PI/2;
+    conf.fix_palm_joint=true;
+    conf.one_finger_as_one_motor=true;
+    conf.x = -1.45;
+    conf.y = 0.8;
+    conf.z = 6.01;	   
 
-    if(conf.hand_is_drawn_under_angel == Is_Draw_under_90_degree)
-      {
-	conf.x = -0.35;
-	conf.y = 0;
-	conf.z = 2;	   
-      }
-    else if (conf.hand_is_drawn_under_angel == Is_Draw_under_180_degree)
-      {
-	conf.x = -1.45;
-	conf.y = 0.8;
-	conf.z = 6.01;	   
-
-      }
     //.changeColor(1,183,172)   
     wiring = new One2OneWiring(new ColorUniformNoise(0.1));
     InvertMotorNStepConf cc5 = InvertMotorNStep::getDefaultConf();
     cc5.cInit=1.5;
-    controller = new InvertMotorNStep(cc5); //SineController();//InvertMotorNStep(cc5);  
+    //controller = new InvertMotorNStep(cc5); //SineController();//InvertMotorNStep(cc5);  
     //	  controller = new InvertMotorSpace(10);  
+    controller = new InvertNChannelController(100); 
+
     agent = new OdeAgent(plotoptions);
     hand = new Hand(odeHandle, osgHandle,conf,"Hand");
     hand->setColor(Color(1.0,0.5,1.0));
     agent->init(controller, hand, wiring);
-    controller->setParam("adaptrate", 0.000);
-    //    controller->setParam("nomupdate", 0.0005);
-    controller->setParam("epsC", 0.05);
-    controller->setParam("epsA", 0.01);
-    controller->setParam("epsC", 0.05);
-    controller->setParam("rootE", 0);
-    controller->setParam("steps", 2);
-    controller->setParam("s4avg", 5);
-    controller->setParam("s4del", 5);
-    //	  controller->setParam("factorB",0);
+    //controller->setParam("adaptrate", 0.000);
+    ////    controller->setParam("nomupdate", 0.0005);
+    //controller->setParam("epsC", 0.05);
+    //controller->setParam("epsA", 0.01);
+    //controller->setParam("rootE", 0);
+    //controller->setParam("steps", 2);
+    //controller->setParam("s4avg", 5);
+    //controller->setParam("s4del", 5);
+    ////	  controller->setParam("factorB",0);
 	 
     hand->place(Pos(2.5,1.26,0));
     global.agents.push_back(agent);
     global.configs.push_back(controller); 
     global.configs.push_back(hand);
 
-    	
-     /* PassiveSphere* s = 
-      new PassiveSphere(odeHandle, 
-      osgHandle.changeColor(Color(184 / 255.0, 233 / 255.0, 237 / 255.0)), 0.2);
-      s->setPosition(Pos(0,0.05,0.5+3)); 
-      s->setTexture("Images/dusty.rgb");
-      global.obstacles.push_back(s);         */
-      
-/*
-    //---For-the-reverse-hand
+    Primitive* trunk = hand->getMainPrimitive();
+    fixator = new FixedJoint(trunk, global.environment);
+    fixator->init(odeHandle, osgHandle);
 
-    //one hand directly above the over, so that the fingers can touch each other 
-    conf.x = -3.45;
-    conf.y = 4.8;
-    conf.z = 2.9;	   
-    conf.invert = -1; 
-    conf.irRange = 0.7;
-    conf.set_typ_of_motor = With_servo_motor;
-    conf.show_contacts = true;
-    conf.ir_sensor_used = true;
-    conf.number_of_ir_sensors = conf.ir_sensor_used*15;
-    conf.Draw_part_of_ir_sensor = Draw_All;
-    conf.hand_is_drawn_under_angel = Is_Draw_under_180_degree;
-    conf.jointLimit1 = -M_PI/180;
-    conf.jointLimit2 = M_PI/2;
-    conf.servo_motor_Power = 1.2;
-    conf.finger_winkel = M_PI/2;
 
-    wiring1 = new One2OneWiring(new ColorUniformNoise(0.1));
-	
-    controller1 = new InvertMotorNStep();  
-    //	  controller = new InvertMotorSpace(10);  
-    agent1 = new OdeAgent(plotoptions);
-    hand1 = new Hand(odeHandle, osgHandle,conf,"Hand");
-    hand1->setColor(Color(1.0,0.5,1.0));
-    global.configs.push_back(controller1);
-    agent1->init(controller1, hand1, wiring1);
-    controller->setParam("adaptrate", 0.000);
-    //    controller->setParam("nomupdate", 0.0005);
-    controller->setParam("epsC", 0.05);
-    controller->setParam("epsA", 0.01);
-    controller->setParam("epsC", 0.05);
-    controller->setParam("rootE", 0);
-    controller->setParam("steps", 2);
-    controller->setParam("s4avg", 5);
-    controller->setParam("s4del", 5);
-    //	  controller->setParam("factorB",0);
-	 
-    hand1->place(Pos(2.5,1.26,0));
-    global.agents.push_back(agent1);
-*/
-
+     
     ClosedPlayground* playground;
-    if(conf.hand_is_drawn_under_angel == Is_Draw_under_180_degree){
-      // use Playground as boundary:
-      playground = new ClosedPlayground(odeHandle, osgHandle, osg::Vec3(4.7, 0.2, 26), 0.9);
-      playground->setColor(Color(1.0f,0.0f,0.26f,0.0f));
-      playground->setPosition(osg::Vec3(0-0.5,0,0)); // playground positionieren und generieren
-      global.obstacles.push_back(playground);
-
-    } 
-/*
-    else if(conf.hand_is_drawn_under_angel == Is_Draw_under_90_degree)
-      {
-	// use Playground as boundary:
-	//    playground = new ClosedPlayground(odeHandle, osgHandle, osg::Vec3(2.7, 0.2, 7), 1.2);
-
-	//Version with closed playground 
-	/*
-	  playground = new ClosedPlayground(odeHandle, osgHandle, osg::Vec3(3.2, 0.2, 14), 1.0);
-
-	  playground->setColor(Color(1.0f,0.0f,0.26f,0.1f));
-	  playground->setPosition(osg::Vec3(0,0,0)); // playground positionieren und generieren
-	* /
-	Playground *playground1 = new Playground(odeHandle, osgHandle, osg::Vec3(4.7, 0.2, 10), 0.9);
-	playground1->setColor(Color(1.0f,0.0f,0.26f,0.2f));
-	playground1->setPosition(osg::Vec3(0,0,0)); // playground positionieren und g
-	global.obstacles.push_back(playground1);
-      }
-  */ 
-
-/*    
-      PassiveSphere* s = 
-      new PassiveSphere(odeHandle, 
-      osgHandle,1.3);
-      s->setPosition(Pos(-0.4,0.45,0.5+7.4)); 
-      s->setColor(Color(1.0f,0.2f,0.2f,0.5f));
-      s->setTexture("Images/light_chess.rgb");
-      global.obstacles.push_back(s); 
-      showParams(global.configs);
-  */  
+    // use Playground as boundary:
+    playground = new ClosedPlayground(odeHandle, osgHandle, osg::Vec3(4.7, 0.2, 26), 0.9);
+    playground->setColor(Color(1.0f,0.0f,0.26f,0.0f));
+    playground->setPosition(osg::Vec3(0-0.5,0,0)); // playground positionieren und generieren
+    global.obstacles.push_back(playground);
 
 
-    // the flashing box within a hand.
-    /*
-      PassiveBox* b = 
-      new PassiveBox(odeHandle, 
-      osgHandle, osg::Vec3(1.4,1.4,0.3),2);
 
-      b->setPose(osg::Matrix::translate(conf.invert*(0+conf.x),(0.05+conf.y),(0.35+conf.z)));
-      //setPosition(Pos(0,0.15,0.5+0.4)); 
-      b->setColor(Color(1.0f,0.2f,0.2f,0.5f));
-      b->setTexture("Images/light_chess.rgb");
-      global.obstacles.push_back(b); 
-    */
-    /*
-      PassiveBox* b = 
-      new PassiveBox(odeHandle, 
-      osgHandle, osg::Vec3(1.8,2.8,1.8),5);
-      b->setPosition(Pos(0,0.15,0.5+0.4)); 
-      b->setColor(Color(1.0f,0.2f,0.2f,0.5f));
-      b->setTexture("Images/light_chess.rgb");
-      global.obstacles.push_back(b); 
-
-    */
 
     
-      PassiveCapsule* c = 
-      new PassiveCapsule(odeHandle, 
-      osgHandle, 1,1,5);
-      c->setPosition(Pos(0-1.75,0.05+0.75,0.5+2.4+4)); 
-      c->setColor(Color(1.0f,0.2f,0.2f,0.5f));
-      //c->setTexture("Images/light_chess.rgb");
-      //c->setTexture("Images/dusty.rgb");
-      //c->setTexture("Images/sandyground.rgb");
-      c->setTexture("Images/furry_toy.jpg");
-      global.obstacles.push_back(c); 
+    PassiveCapsule* c = new PassiveCapsule(odeHandle, osgHandle, 1,1,5);
+    c->setPosition(Pos(0-1.75,0.05+0.75,0.5+2.4+4)); 
+    c->setColor(Color(1.0f,0.2f,0.2f,0.5f));
+    //c->setTexture("Images/light_chess.rgb");
+    //c->setTexture("Images/dusty.rgb");
+    //c->setTexture("Images/sandyground.rgb");
+    c->setTexture("Images/furry_toy.jpg");
+    global.obstacles.push_back(c); 
     
-
     showParams(global.configs);
   }
 
@@ -324,6 +201,11 @@ public:
     bool handled = false;
     switch ( key )
       {
+	case 'x': 
+	  if(fixator) delete fixator;
+	  fixator=0;	 
+	  handled = true;
+	  break;
       case 's' :
 	/*	controller->store("test") && printf("Controller stored\n");
 		handled = true; 	*/
@@ -338,10 +220,12 @@ public:
       	c->setColor(Color(1.0f,0.2f,0.2f,0.5f));
       	c->setTexture("Images/furry_toy.jpg");
       	globalData.obstacles.push_back(c); }
+	handled = true;
 	break;
       case 'v' :
         ClosedPlayground* playground=(ClosedPlayground* )globalData.obstacles.back();
 	playground->setColor(Color(1.0f,0.0f,0.26f,0.0f));
+	handled = true;
 	break;
  	
 	/*      
