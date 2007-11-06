df764c5bcfc07e408efd6758c1c297c063f3f606
martius <martius>
2007-11-06 15:14:27 +0000

    new class that composes controller and wiring

---
+++ selforg/wiredcontroller.cpp
@@ -0,0 +1,305 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1  2007-11-06 15:14:27  martius
+ *   new class that composes controller and wiring
+ *
+ *                                                                 *
+ ***************************************************************************/
+
+#include "wiredcontroller.h"
+#include <signal.h>
+#include "printInternals.h"
+
+#include "abstractcontroller.h"
+#include "abstractwiring.h"
+#include <time.h>
+#include <string.h>
+
+#include "callbackable.h"
+
+using namespace std;
+
+WiredController::WiredController(const PlotOption& plotOption, double noisefactor){
+  internInit();
+  if(plotOption.mode!=NoPlot) plotOptions.push_back(plotOption);
+  this->noisefactor = noisefactor;
+}
+
+
+WiredController::WiredController(const std::list<PlotOption>& plotOptions, double noisefactor)
+  : noisefactor(noisefactor), plotOptions(plotOptions){
+  internInit();
+}
+
+void WiredController::internInit(){
+  controller = 0;
+  wiring     = 0;
+
+  cmotors=0;
+  csensors=0;
+
+  initialised = false;
+  t=0;
+}
+
+WiredController::~WiredController(){
+  // closes all pipes of the agents due to pause mode or so
+  for (int i = NoPlot; i < LastPlot; i++){ 
+    removePlotOption((PlotMode)i);
+  }
+  if(csensors) free(csensors);
+  if(cmotors)  free(cmotors);
+}
+
+
+/// initializes the object with the given controller, robot and wiring
+//  and initializes pipe to guilogger
+bool WiredController::init(AbstractController* controller, AbstractWiring* wiring,
+			   int robotsensornumber, int robotmotornumber){
+  this->controller = controller;
+  this->wiring     = wiring;
+  assert(controller && wiring);
+
+  rsensornumber = robotsensornumber;
+  rmotornumber  = robotmotornumber;
+  wiring->init(rsensornumber, rmotornumber);
+  csensornumber = wiring->getControllerSensornumber();
+  cmotornumber  = wiring->getControllerMotornumber();
+  controller->init(csensornumber, cmotornumber);
+
+  csensors      = (sensor*) malloc(sizeof(sensor) * csensornumber);
+  cmotors       = (motor*)  malloc(sizeof(motor)  * cmotornumber);
+
+  inspectables.push_back(controller);
+  inspectables.push_back(wiring);
+
+  // copy plotoption list and add it one by one
+  list<PlotOption> po_copy(plotOptions);
+  plotOptions.clear();
+  // open the all outputs
+  for(list<PlotOption>::iterator i=po_copy.begin(); i != po_copy.end(); i++){
+    addPlotOption(*i);
+  }
+  initialised = true;
+  return true;
+}
+
+void WiredController::addPlotOption(const PlotOption& plotOption) {
+  PlotOption po = plotOption;
+  // if plotoption with the same mode exists -> delete it
+  removePlotOption(po.mode);
+
+  // this prevents the simulation to terminate if the child  closes
+  // or if we fail to open it.
+  signal(SIGPIPE,SIG_IGN);
+  po.open();
+  if(po.pipe){
+    // print start
+    time_t t = time(0);
+    fprintf(po.pipe,"# Start %s", ctime(&t));    
+    // print network description given by the structural information of the controller
+    printNetworkDescription(po.pipe, "Selforg"/*controller->getName()*/, controller);
+    // print interval
+    fprintf(po.pipe, "# Recording every %dth dataset\n", po.interval);
+    // print all configureables
+    for(list<const Configurable*>::iterator i = po.configureables.begin(); i!= po.configureables.end(); i++){
+      (*i)->print(po.pipe, "# ");
+    }
+    // print all parameters of the wiring if confirable
+    Configurable* c = dynamic_cast<Configurable*>(wiring);
+    if(c) c->print(po.pipe, "# ");
+    // print all parameters of the controller
+    controller->print(po.pipe, "# ");
+    // print head line with all parameter names
+    unsigned int snum = plotOption.whichSensors == Robot ? rsensornumber : csensornumber;
+    printInternalParameterNames(po.pipe, snum, cmotornumber, inspectables);
+  }
+  plotOptions.push_back(po);
+}
+
+bool WiredController::removePlotOption(PlotMode mode) {
+  // if plotoption with the same mode exists -> delete it
+  list<PlotOption>::iterator po
+      = find_if(plotOptions.begin(), plotOptions.end(), PlotOption::matchMode(mode));
+  if(po != plotOptions.end()){
+    (*po).close();
+    plotOptions.erase(po);
+    return true;
+  }
+  return false;
+}
+
+
+// Plots controller sensor- and motorvalues and internal controller parameters.
+void WiredController::plot(const sensor* rx, int rsensornumber, 
+			   const sensor* cx, int csensornumber,
+			   const motor* y, int motornumber, double time){
+  assert(controller && rx && cx && y);
+
+  for(list<PlotOption>::iterator i=plotOptions.begin(); i != plotOptions.end(); i++){
+    if( ((*i).pipe) && (t % (*i).interval == 0) ){
+      
+      if((*i).whichSensors == Robot){
+		printInternalParameters((*i).pipe, time, rx, rsensornumber, y, motornumber, inspectables);
+      }else{
+		printInternalParameters((*i).pipe, time, cx, csensornumber, y, motornumber, inspectables);
+      }
+      if(t% ((*i).interval * 10)) fflush((*i).pipe);
+    } // else {
+      //      if (!(*i).pipe) { // if pipe is closed
+      // std::cout << "pipe is closed!" << std::endl;
+      //      }
+      // }
+  }
+};
+
+
+void WiredController::writePlotComment(const char* cmt){
+  for(list<PlotOption>::iterator i=plotOptions.begin(); i != plotOptions.end(); i++){
+    if( ((*i).pipe) && (t % (*i).interval == 0) && (strlen(cmt)>0)){ // for the guilogger pipe
+      char last = cmt[strlen(cmt)-1];
+      fprintf((*i).pipe, "# %s", cmt);
+      if(last!=10 && last!=13) // print with or without new line
+	fprintf((*i).pipe, "\n");
+    }
+  }
+}
+
+//  Performs an step of the wired controller: 
+//   pushing sensor values through wiring,
+//  controller step, pushing controller steps back through wiring
+void WiredController::step(const sensor* sensors, int sensornumber,
+			   motor* motors, int motornumber, 
+			   double noise, double time){
+  assert(controller && wiring && sensors && csensors && cmotors && motors);
+
+  if(sensornumber != rsensornumber){
+    fprintf(stderr, "%s:%i: Got wrong number of sensors, expected %i, got %i!\n", __FILE__, __LINE__,
+	    rsensornumber, sensornumber);
+  }
+
+  wiring->wireSensors(sensors, rsensornumber, csensors, csensornumber, noise * noisefactor);
+  controller->step(csensors, csensornumber, cmotors, cmotornumber);
+  wiring->wireMotors(motors, rmotornumber, cmotors, cmotornumber);
+  plot(sensors, rsensornumber, csensors, csensornumber, cmotors, cmotornumber,time);
+  // do a callback for all registered Callbackable classes
+  FOREACH(list<Callbackable*>, callbackables, i){
+    (*i)->doOnCallBack();
+  }
+  t++;
+}
+
+void WiredController::addInspectable(const Inspectable* inspectable){
+  if(!initialised){
+    inspectables.push_back(inspectable);
+  } else {
+    std::cerr << "WiredController::addInspectable(const Inspectable* inspectable); failed, because WiredController was already initialised! " << std::endl;
+  }
+}
+
+void WiredController::addCallbackable(Callbackable* callbackable){
+  callbackables.push_back(callbackable);
+}
+
+bool PlotOption::open(){
+  char cmd[255];
+  // this prevents the simulation to terminate if the child  closes
+  // or if we fail to open it.
+  signal(SIGPIPE,SIG_IGN);
+  switch(mode){
+  case File:
+      struct tm *t;
+      time_t tnow;
+      time(&tnow);
+      t = localtime(&tnow);
+      char logfilename[255];
+      sprintf(logfilename,"%s_%02i-%02i-%02i_%02i-%02i-%02i-%02i.log",
+	      name.c_str(), t->tm_year%100, t->tm_mon+1 , t->tm_mday,
+	      t->tm_hour, t->tm_hour, t->tm_min, t->tm_sec);
+      pipe=fopen(logfilename,"w");
+      if (pipe)
+	std::cout << "Now logging to file \"" << logfilename << "\"." << std::endl;
+      break;
+  case GuiLogger_File:
+    pipe=popen("guilogger -m pipe -l","w");
+    break;
+  case GuiLogger:
+    pipe=popen("guilogger -m pipe","w");
+    break;
+  case NeuronViz:
+    pipe=popen("neuronviz > /dev/null","w");  // TODO: Platform dependent
+    break;
+  case SoundMan:
+    sprintf(cmd,"soundMan %s",parameter.c_str());
+    pipe=popen(cmd,"w");
+    break;
+  default: // and NoPlot
+    return false;
+  }
+  if(pipe==0){
+    fprintf(stderr, "%s:%i: could not open plot tool!\n", __FILE__, __LINE__);
+    return false;
+  }else return true;
+}
+
+
+void PlotOption::close(){
+  if (pipe) {
+    switch(mode){
+    case File:
+      std::cout << "logfile closing...SUCCESSFUL" << std::endl;
+      fclose(pipe);
+      break;
+    case GuiLogger:
+    case GuiLogger_File:
+      //std::cout << "guilogger pipe closing...maybe you must manually close the guilogger first!"
+      //          << std::endl;
+      // send quit message to pipe
+      fprintf(pipe, "#QUIT\n");
+      pclose(pipe);
+      std::cout << "guilogger pipe closing...SUCCESSFUL" << std::endl;
+      break;
+    case NeuronViz:
+      //std::cout << "neuronviz pipe closing...maybe you must manually close the neuronviz first!"
+      //          << std::endl;
+      // send quit message to pipe
+      fprintf(pipe, "#QUIT\n");
+      pclose(pipe);
+      std::cout << "neuronviz pipe closing...SUCCESSFUL" << std::endl;
+      break;
+
+    case SoundMan:
+      std::cout << "SoundMan closing...SUCCESSFUL" << std::endl;
+      fclose(pipe);
+      break;
+
+    default:
+      break;
+    }
+    pipe=0;
+  }
+}
+
+void PlotOption::addConfigurable(const Configurable* c){
+  configureables.push_back(c);
+}
---
+++ selforg/wiredcontroller.h
@@ -0,0 +1,250 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   The Idea of this class is a controller wrapped in the wiring          *
+ *   with all the plotoptions and so on, just without robot. It            *
+ *   seams to be a much better architecture for connecting the controller  *
+ *   to a hardware robot or other platforms.                               *
+ *                                                                         *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1  2007-11-06 15:14:41  martius
+ *   new class that composes controller and wiring
+ *
+ *                                                                 *
+ ***************************************************************************/
+/*
+** Started on  Mon Oct 22 10:50:47 2007 Georg Martius
+** Last update Mon Oct 22 10:50:47 2007 Georg Martius
+*/
+#ifndef   	WIREDCONTROLLER_H_
+# define   	WIREDCONTROLLER_H_
+
+#include <stdio.h>
+#include <list>
+#include <utility>
+#include <string>
+
+class AbstractController;
+class AbstractWiring;
+class Configurable;
+class Inspectable;
+class Callbackable;
+
+class WiredController;
+
+/** Output mode for agent.
+ */
+enum PlotMode {
+  /// dummy (does nothing) is there for compatibility, might be removed later
+  NoPlot,
+  /// write into file
+  File,
+  /// plotting with guilogger (gnuplot)
+  GuiLogger,
+  /// plotting with guiscreen (gnuplot) in file logging mode
+  GuiLogger_File,
+  /// net visualiser
+  NeuronViz,
+
+  /// Acustic output of robotic values via external SoundMan
+  SoundMan,
+
+  /// dummy used for upper bound of plotmode type
+  LastPlot
+};
+
+/** Output either sensors from robot or from controller
+    (there can be a difference depending on the used wiring)
+ */
+enum PlotSensors {Robot, Controller};
+
+/** This class contains options for the use of an external plot utility like guilogger or neuronviz
+    or just simply file output
+ */
+class PlotOption {
+public:
+  friend class WiredController;
+
+  PlotOption(){ mode=NoPlot; whichSensors=Controller; interval=1; pipe=0; parameter="";}
+  PlotOption( PlotMode mode, PlotSensors whichSensors = Controller,
+	      int interval = 1, 
+	      std::list<const Configurable*> confs = std::list<const Configurable*>(),
+	      std::string parameter="")
+    : mode(mode), whichSensors(whichSensors), interval(interval), 
+    configureables(confs), parameter(parameter)
+    { pipe=0; }
+
+  virtual ~PlotOption(){}
+
+  /// nice predicate function for finding by mode
+  struct matchMode : public std::unary_function<const PlotOption&, bool> {
+    matchMode(PlotMode mode) : mode(mode) {}
+    int mode;
+    bool operator()(const PlotOption& m) { return m.mode == mode; }
+  };
+
+  void addConfigurable(const Configurable*);
+  void setName(const std::string& name) { this->name = name;}
+private:
+
+  bool open(); ///< opens the connections to the plot tool
+  void close();///< closes the connections to the plot tool
+
+  FILE* pipe;
+  long t;
+  std::string name;
+
+  PlotMode mode;
+  PlotSensors whichSensors;
+  int interval;
+  std::list< const Configurable* > configureables;
+  std::string parameter; ///< additional parameter for external command
+};
+
+
+/** The WiredController contains a controller and a wiring, which
+    connects the controller with the robot.
+    Additionally there are some ways to keep track of internal information.
+    You have the possibility to keep track of sensor values,
+     motor values and internal parameters of the controller with PlotOptions.
+    The name PlotOptions is a bit missleaded, it should be "OutputOptions",
+     however you can write the data into a file or send it to 
+     visualisation tools like guilogger or neuronviz.
+ */
+class WiredController {
+public:
+  typedef double sensor;
+  typedef double motor;
+
+  /** constructor. PlotOption as output setting.
+      noisefactor is used to set the relative noise strength of this agent
+   */
+  WiredController(const PlotOption& plotOption = PlotOption(NoPlot), double noisefactor = 1);
+  /** constructor. A list of PlotOption can given.
+      noisefactor is used to set the relative noise strength of this agent
+   */
+  WiredController(const std::list<PlotOption>& plotOptions, double noisefactor = 1);
+
+  /** destructor
+   */
+  virtual ~WiredController();
+
+  /** initializes the object with the given controller and wiring
+      and initializes the output options
+  */
+  bool init(AbstractController* controller, AbstractWiring* wiring, 
+	    int robotsensornumber, int robotmotornumber);
+
+  /** Performs an step of the controller, which includes
+      pushing sensor values through the wiring,
+      controller step, 
+      pushing controller outputs (= motorcommands) back through the wiring
+      @param sensors sensors inputs scaled to [-1,1]
+      @param sensornumber length of the sensor array
+      @param motors motors outputs. MUST have enough space for motor values!
+      @param motornumber length of the provided motor array
+
+      @param noise Noise strength.
+      @param time (optional) current simulation time (used for logging)
+  */
+  void step(const sensor* sensors, int sensornumber,
+                    motor* motors, int motornumber, 
+		    double noise, double time=-1);
+
+  /** Returns a pointer to the controller.
+   */
+  virtual AbstractController* getController() { return controller;}
+
+  /** Returns a pointer to the wiring.
+   */
+  virtual AbstractWiring* getWiring() { return wiring;}
+
+  /** adds the PlotOptions to the list of plotoptions
+      If a plotoption with the same Mode exists, then the old one is deleted first
+   */
+  virtual void addPlotOption(const PlotOption& plotoption);
+
+  /** removes the PlotOptions with the given type
+      @return true if sucessful, false otherwise
+   */
+  virtual bool removePlotOption(PlotMode mode);
+
+  /**
+     write comment to output streams (PlotOptions). For instance changes in parameters.
+  */
+  virtual void writePlotComment(const char* cmt);
+
+  /** adds an inspectable object for logging. Must be called before addPlotOption and before init!
+   */
+  virtual void addInspectable(const Inspectable* inspectable);
+
+  /** adds an Callbackable object for getting a callback every step.
+   */
+  virtual void addCallbackable(Callbackable* callbackable);
+
+protected:
+  /**
+   * Plots controller sensor- and motorvalues and internal controller parameters.
+   * @param rx actual sensorvalues from robot (used for generation of motorcommand in actual timestep)
+   * @param rsensornumber length of rx
+   * @param cx actual sensorvalues which are passed to controller (used for generation of motorcommand in actual timestep)
+   * @param csensornumber length of cx
+   * @param y actual motorcommand (generated in the actual timestep)
+   * @param motornumber length of y
+   * @param time simulation time
+   */
+  virtual void plot(const sensor* rx, int rsensornumber, const sensor* cx, int csensornumber,
+		    const motor* y, int motornumber, double time);
+
+
+  AbstractController* controller;
+  AbstractWiring* wiring;
+
+  /// number of sensors of robot
+  int rsensornumber;
+  /// number of motors of robot
+  int rmotornumber;
+  /// number of sensors of comntroller
+  int csensornumber;
+  /// number of motors of comntroller
+  int cmotornumber;
+
+  /// factor that is  muliplied with noise stength
+  double noisefactor;
+
+  motor  *cmotors;
+  sensor *csensors;
+
+  void internInit();
+
+ protected:
+  long int t;
+
+  std::list<PlotOption> plotOptions;
+  std::list<const Inspectable* > inspectables;
+  bool initialised;
+
+  std::list<Callbackable* > callbackables; 
+
+};
+
+#endif 	    /* !WIREDCONTROLLER_H_ */
