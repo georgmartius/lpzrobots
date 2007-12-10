1022ae69a93a899dd1c076eea3fe61a9e29d4433
robot3 <robot3>
2007-12-10 16:39:03 +0000

    O

---
+++ javacontroller/SinusExample/Start.java
@@ -0,0 +1,12 @@
+import controller.ControllerManager;
+import examples.SinController;
+
+public class Start {
+
+	
+	public static void main(String[] args) {
+		ControllerManager.addController(new SinController("localhost",4711,4712));
+
+	}
+
+}
