992e635bf0567329d76b46c9cf0164caef59af88
robot3 <robot3>
2007-12-10 16:51:15 +0000

    *** empty log message ***

--- javacontroller/SinusExample/Start.java
+++ javacontroller/SinusExample/Start.java
@@ -5,7 +5,7 @@ public class Start {
 
 	
 	public static void main(String[] args) {
-		ControllerManager.addController(new SinController("localhost",4711,4712));
+		ControllerManager.addController( new SinController("localhost",4711,4712));
 
 	}
 
