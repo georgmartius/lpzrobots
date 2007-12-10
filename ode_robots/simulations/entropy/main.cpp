40925d14c410c6ce260b3626d056d212423e69b5
der <der>
2007-12-10 14:13:59 +0000

    actual version

--- ode_robots/simulations/entropy/main.cpp
+++ ode_robots/simulations/entropy/main.cpp
@@ -20,7 +20,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.5  2007-12-07 10:51:42  der
+ *   Revision 1.6  2007-12-10 14:13:59  der
+ *   actual version
+ *
+ *   Revision 1.5  2007/12/07 10:51:42  der
  *   tested some things
  *
  *   Revision 1.4  2007/12/07 08:51:18  der
@@ -87,30 +90,13 @@
 #include "odeagent.h"
 #include "playground.h"
 
-#include "passivesphere.h"
 #include "passivebox.h"
-#include "passivecapsule.h"
 
-#include <selforg/invertnchannelcontroller.h>
-#include <selforg/invertmotornstep.h>
-#include <selforg/invertmotorspace.h>
-#include <selforg/sinecontroller.h>
-#include <selforg/noisegenerator.h>
-#include <selforg/one2onewiring.h>
-#include <selforg/derivativewiring.h>
-
-#include "axisorientationsensor.h"
-
-#include "hurlingsnake.h"
-#include "schlangeservo2.h"
-#include "caterpillar.h"
 #include "nimm2.h"
-#include "nimm4.h"
-#include "sphererobot3masses.h"
-#include "sliderwheelie.h"
-#include "shortcircuit.h"
 
-#include <selforg/discretecontrolleradapter.h>
+#include <selforg/invertmotornstep.h>
+#include <selforg/one2onewiring.h>
+
 #include <selforg/oneactivemultipassivecontroller.h>
 #include <selforg/mutualinformationcontroller.h>
 
@@ -124,11 +110,9 @@
 #include <string.h>
 
 
-// fetch all the stuff of lpzrobots into scope
 using namespace lpzrobots;
 
-class ThisSim : public Simulation /*, public Inspectable*/
-{
+class ThisSim : public Simulation {
 public:
 
   StatisticTools* stats;
@@ -139,68 +123,41 @@ public:
   StatisticMeasure* convTest0;
   StatisticMeasure* convTest1;
   TrackableMeasure* trackableEntropySLOW;
-  /*
-  virtual std::list<iparamkey> getInternalParamNames() const  {
-  	std::list<iparamkey> list;
-  	list+=std::string("sumForce");
-  	return list;
-  }
-
-  virtual std::list<iparamval> getInternalParams() const {
-  	std::list<iparamval> list;
-  	//list+=getAvgOf3( oldest,old,myNimm2->getSumForce());
-  	return list;
-  }
-  */
-
 
   ThisSim(double cInit=1.0) : cInit(cInit) {}
 
-  ~ThisSim() {}
+  ~ThisSim() {
+    if (convTest0)
+      delete(convTest0);
+    if (convTest1)
+      delete(convTest1);
+    if (trackableEntropySLOW)
+      delete(trackableEntropySLOW);
+    if (stats)
+      delete(stats);
+    if (mic)
+      delete(mic);
+   }
 
   // starting function (executed once at the beginning of the simulation loop)
   void start(const OdeHandle& odeHandle, const OsgHandle& osgHandle, GlobalData& global)
   {
-
     int numNimm2=1;
-
-    stats = new StatisticTools();
-
     setCameraHomePos(Pos(-19.15, 13.9, 6.9),  Pos(-126.1, -17.6, 0));
-    // initialization
-    // - set noise to 0.1
-    // - register file chess.ppm as a texture called chessTexture (used for the wheels)
 
     global.odeConfig.setParam("noise",0.05);
-    global.odeConfig.setParam("controlinterval",1);
-    global.odeConfig.setParam("realtimefactor",2);
-    //  global.odeConfig.setParam("simstepsize",0.1);
-    //  global.odeConfig.setParam("drawinterval",5);
-    // initialization
+    global.odeConfig.setParam("realtimefactor",0);
 
-    Playground* playground =
-      new Playground(odeHandle, osgHandle,osg::Vec3(18, 0.2, 2.0));
+    Playground* playground = new Playground(odeHandle, osgHandle,osg::Vec3(18, 0.2, 2.0));
     playground->setColor(Color(0.88f,0.4f,0.26f,0.2f));
-    playground->setPosition(osg::Vec3(0,0,0)); // playground positionieren und generieren
+    playground->setPosition(osg::Vec3(0,0,0));
     Substance substance;
     substance.toRubber(40);
     playground->setGroundSubstance(substance);
     global.obstacles.push_back(playground);
 
-    for(int i=0; i<0; i++)
-    {
-      PassiveSphere* s =
-        new PassiveSphere(odeHandle,
-                          osgHandle.changeColor(Color(184 / 255.0, 233 / 255.0, 237 / 255.0)), 0.2);
-      s->setPosition(Pos(i*0.5-2, i*0.5, 1.0));
-      s->setTexture("Images/dusty.rgb");
-      global.obstacles.push_back(s);
-    }
-
     for (int j=0;j<4;j++)
-    {
-      for(int i=0; i<4; i++)
-      {
+      for(int i=0; i<4; i++) {
         PassiveBox* b =
           new PassiveBox(odeHandle,
                          osgHandle, osg::Vec3(1.5+i*0.01,1.5+i*0.01,1.5+i*0.01),0.0);
@@ -209,17 +166,6 @@ public:
         b->setTexture("Images/light_chess.rgb");
         global.obstacles.push_back(b);
       }
-    }
-
-    for(int i=0; i<0; i++)
-    {
-      PassiveCapsule* c =
-        new PassiveCapsule(odeHandle, osgHandle, 0.2f, 0.3f, 0.3f);
-      c->setPosition(Pos(i-1, -i, 1.0));
-      c->setColor(Color(0.2f,0.2f,1.0f,0.5f));
-      c->setTexture("Images/light_chess.rgb");
-      global.obstacles.push_back(c);
-    }
 
     OdeAgent* agent;
     AbstractWiring* wiring;
@@ -233,68 +179,53 @@ public:
     nimm2conf.singleMotor=false;
     nimm2conf.visForce=true;
     nimm2conf.boxMode=true;
+    
     for(int r=0; r < numNimm2; r++)
     {
       myNimm2 = new Nimm2(odeHandle, osgHandle, nimm2conf, "Nimm2_" + std::itos(r));
-      //robot = new ShortCircuit(odeHandle,osgHandle,1,1);
       ((OdeRobot*)myNimm2)->place(Pos ((r-1)*5,5,0));
+      
       InvertMotorNStepConf invertnconf = InvertMotorNStep::getDefaultConf();
       invertnconf.cInit = cInit;
       controller = new InvertMotorNStep(invertnconf);
       controller->setParam( "epsA",0);
       controller->setParam( "epsC",0);
-      //         controller = new InvertMotorSpace(15);
-      //      controller->setParam("s4avg",10);
-      //controller = new SineController();
-      //  controller->setParam( "sinerate",1);
-      //    controller->setParam("factorB",0); // not needed here and it does some harm on the behaviour
-      //wiring = new One2OneWiring(new ColorUniformNoise(0.1));
+      
       wiring = new One2OneWiring(new WhiteNormalNoise());
-      agent = new OdeAgent( std::list<PlotOption>() );
-      // create DiscreteControllerAdapter
-      //	    DiscreteControllerAdapter* discretesizer = new DiscreteControllerAdapter(controller);
-      //	    discretesizer->setIntervalCount(3);
       OneActiveMultiPassiveController* onamupaco = new OneActiveMultiPassiveController(controller,"main");
-       mic = new MutualInformationController(30);
-       mic->setParam("showF",0);
-       mic->setParam("showP",0);
-       onamupaco->addPassiveController(mic,"mi30");
+      mic = new MutualInformationController(30);
+      mic->setParam("showF",0);
+      mic->setParam("showP",0);
+      onamupaco->addPassiveController(mic,"mi30");
 
-       //      agent->addInspectable((Inspectable*)stats);
-      //      agent->addCallbackable((Callbackable*)stats);
-            agent->init(onamupaco, myNimm2		, wiring);
+      stats = new StatisticTools();
+      agent = new OdeAgent( std::list<PlotOption>() );
+      agent->addInspectable((Inspectable*)stats);
+      agent->addCallbackable((Callbackable*)stats);
+      agent->init(onamupaco, myNimm2		, wiring);
       //agent->init(controller, myNimm2		, wiring);
-      agent->setTrackOptions(TrackRobot(true,false, false, false, "entropyc",10));
+      char cdesc[6];
+      sprintf(cdesc, "c=%f_",cInit);
+      agent->setTrackOptions(TrackRobot(true,false, false, false,cdesc,10));
       global.configs.push_back(controller);
       global.agents.push_back(agent);
 
+      
+      convTest1=stats->getMeasure( mic->getMI(1),"MI 1 CONV",CONV,50000,0.01);
+      convTest0=stats->getMeasure( mic->getMI(0),"MI 0 CONV",CONV,50000,0.01);
+      
       //  stats->beginMeasureAt(100);
 
-      //      stats->addMeasure(mic->getMI(0),"MI0",ID,0);
-      // stats->addMeasure(mic->getMI(1),"MI1",ID,0);
-
-//       // this->getWSM()->beginMeasureAt(100);
-//       this->getHUDSM()->addMeasure(mic->getMI(1),"MI 1",ID,1);
-       this->getHUDSM()->addMeasure(mic->getMI(0),"MI 0",ID,1);
-
-      //  convTest1=stats->getMeasure( mic->getMI(1),"MI 1 CONV",CONV,50000,0.002);
-      // getWSM()->addMeasure( mic->getMI(1),"MI 1 CONV",CONV,5,10.0);
-      //convTest0=stats->getMeasure( mic->getMI(0),"MI 0 CONV",CONV,50000,0.002);
+      //this->getHUDSM()->addMeasure(mic->getMI(1),"MI 1",ID,1);
+      //this->getHUDSM()->addMeasure(mic->getMI(0),"MI 0",ID,1);
 
       std::list<Trackable*> trackableList;
       trackableList.push_back(myNimm2);
-      //    TrackableMeasure* trackableEntropy = new TrackableMeasure(trackableList,"E upd Nimm2",ENT,playground->getCornerPointsXY(),X | Y, 50);
-      
-      //this->getHUDSM()->addMeasure(trackableEntropy);
-      
       trackableEntropySLOW= new TrackableMeasure(trackableList,"Eslow Nimm2",ENTSLOW,playground->getCornerPointsXY(),X | Y, 10);
-      
       this->getHUDSM()->addMeasure(trackableEntropySLOW);
+      //TrackableMeasure* trackableEntropy = new TrackableMeasure(trackableList,"E upd Nimm2",ENT,playground->getCornerPointsXY(),X | Y, 50);
       //this->getHUDSM()->addMeasure(trackableEntropy);
-      
-      // convTest1=stats->getMeasure( mic->getMI(0),"MI 0 CONV",CONV,50000,0.001);
-      // getWSM()->addMeasure( mic->getMI(1),"MI 1 CONV",CONV,5,10.0);
-      //convTest0=stats->getMeasure( trackableEntropySLOW->getValueAdress(),"E CONV",CONV,50000,0.001);
+    
     }
 
     showParams(global.configs);
@@ -310,35 +241,18 @@ public:
    */
   void addCallback(GlobalData& globalData, bool draw, bool pause, bool control)
   {
-    if (this->sim_step%100000==0) {
+    if (this->sim_step%100000==0)
+    {
       printf("timeSteps   = %li\n",this->sim_step);
       printf("time in min = %f\n",((float)this->sim_step)/100/60);
-      //   printf("MI sensor 0 = %f\n",mic->getMI(0));
-      //  printf("MI sensor 1 = %f\n",mic->getMI(1));
-      printf("Entropy     = %f\n",trackableEntropySLOW->getValue());
+      printf("(MI0+MI1)/2 = %f\n",(mic->getMI(0)+mic->getMI(1))/2);
+      //printf("Entropy     = %f\n",trackableEntropySLOW->getValue());
     }
-    if (this->sim_step>=600000) {
-        FILE* file;
-      char filename[256];
-      sprintf(filename, "ent_C.log");
-
-      file = fopen(filename,"a");
-
-      //  fprintf(file, "#Logfile for measuring the entropy\n");
-      // fprintf(file,"timeSteps   = %li\n",this->sim_step);
-      // fprintf(file,"time in min = %f\n",((float)this->sim_step)/100/60);
-      //fprintf(file,"MI sensor 0 = %f\n",mic->getMI(0));
-      //fprintf(file,"MI sensor 1 = %f\n",mic->getMI(1));
-      fprintf(file,"%f, %f\n",cInit,trackableEntropySLOW->getValue());
-      fflush(file);
-      if(file) fclose(file);
-      printf("timeSteps   = %li\n",this->sim_step);
-      printf("time in min = %f\n",((float)this->sim_step)/100/60);
-      //printf("MI sensor 0 = %f\n",mic->getMI(0));
-      //printf("MI sensor 1 = %f\n",mic->getMI(1));
-      printf("Entropy     = %f\n",trackableEntropySLOW->getValue());
+    //if (this->sim_step>=600000)
+    if  ((this->convTest0->getValue()==1.0)&&(this->convTest1->getValue()==1.0))
+    {
       simulation_time_reached=true;
-   }    
+    }
   }
 
   // add own key handling stuff here, just insert some case values
@@ -358,20 +272,49 @@ public:
 
 };
 
+void runSim(double cinit, int runs, int argc, char **argv) {
+  double sum = 0.0;
+  double mi=0.0;
+  for (int i=0;i<runs;i++) {
+    std::cout << "run number " << (i+1) << "..." << std::endl;
+    ThisSim* sim;
+    sim = new ThisSim(cinit);
+    sim->run(argc,argv);
+    sum+= sim->trackableEntropySLOW->getValue();
+    mi = (sim->mic->getMI(0) + sim->mic->getMI(1))/2;
+    delete(sim);
+  }
+  
+  double avg = sum / ((double)runs);
+  FILE* file;
+  char filename[256];
+  sprintf(filename, "ent_C.log");
+  file = fopen(filename,"a");
+  fprintf(file,"%f, %f, %f\n",cinit,avg,mi);
+  fflush(file);  
+}
+
 int main (int argc, char **argv)
 {
-
-
+  // check for runs value
+  int runs=1;
+  int index = contains(argv, argc, "-runs");
+  if(index &&  (argc > index))
+      runs = atoi(argv[index]);
   // check for cinit value
-  int index = contains(argv, argc, "-cinit");
-  if(index) {
-    if(argc > index) {
+  index = contains(argv, argc, "-cinit");
+  if (index && (argc > index))
+    {
       ThisSim sim(atof(argv[index]));
       sim.run(argc,argv);
     }
-  } else for (double cinit=0.8;cinit<=1.2;cinit+=0.05)  {
-    ThisSim sim;
-    sim.cInit=cinit;
-    sim.run(argc,argv);
+  else
+  {
+    for (double cinit=0.0;cinit<=0.9;cinit+=0.05)
+      runSim(cinit,runs,argc,argv);
+    for (double cinit=0.95;cinit<=1.15;cinit+=0.025)
+      runSim(cinit,runs,argc,argv);
+    for (double cinit=1.2;cinit<=1.3;cinit+=0.05)
+      runSim(cinit,runs,argc,argv);
   }
 }
