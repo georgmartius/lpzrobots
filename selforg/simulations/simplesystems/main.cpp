dcf9945eb90220d8abdaa521a254ea82b801bca7
martius <martius>
2007-12-11 14:29:14 +0000

    new universalcontroller swing system does not work

--- selforg/simulations/simplesystems/main.cpp
+++ selforg/simulations/simplesystems/main.cpp
@@ -13,6 +13,7 @@
 #include <selforg/invertmotornstep.h>
 #include <selforg/dercontroller.h>
 #include <selforg/one2onewiring.h>
+#include <selforg/universalcontroller.h>
 
 #include "cmdline.h"
 #include "console.h"
@@ -22,9 +23,9 @@ using namespace std;
 
 bool stop=0;
 double noise=.1;
-double realtimefactor=0.1;
+double realtimefactor=1;
 
-typedef enum Mode {NORMAL, EXTRASINE};
+typedef enum Mode {NORMAL, SWING, EXTRASINE};
 
 
 /** This robot emulates different systems based on the mode
@@ -41,6 +42,7 @@ public:
     buffersize=20;
     switch(mode){
     case NORMAL:
+    case SWING:
       motornumber  = dimension;
       sensornumber = dimension;
       break;
@@ -55,8 +57,14 @@ public:
     x_buffer = new double*[buffersize];
     for (unsigned int k = 0; k < buffersize; k++) {
       x_buffer[k] = new double[sensornumber];
+      memset(x_buffer[k],0,sizeof(double)*sensornumber);
     }
+    v = new double[motornumber];
+    memset(x,0,sizeof(double)*motornumber);
+    memset(y,0,sizeof(double)*motornumber);
+    memset(v,0,sizeof(double)*motornumber);
 
+    x[0]=1;
     //0: no interia, 1: only inertia, no control, a good value is 0.9
     addParameterDef("inertia",      &inertia,      0.0); 
     // if not 0 then the last sensor is set to 0 (only noise remains)
@@ -108,14 +116,18 @@ public:
   virtual void setMotors(const motor* motors, int motornumber){
     assert(motornumber >= this->motornumber);
     memcpy(y, motors, sizeof(motor) * this->motornumber);
-    normal();
+    switch(mode){
+    case SWING: swing(); break;
+    case NORMAL: 
+    default: normal(); break;
+    }
     if(mode == EXTRASINE){
       extraSineInput();
     }
     // copy the sensor values into the buffer
     memcpy(x_buffer[t%buffersize],x,sizeof(sensor)*sensornumber); 
     t++;    
-  }
+  } 
 
   /** returns number of sensors */
   virtual int getSensorNumber(){ return sensornumber; }
@@ -140,7 +152,7 @@ public:
     x[sensornumber-1]=sin(t/tau);    
   }
 
-  // system with inertia, correlation of subsequent channels (cyclic),
+  // system with damping, correlation of subsequent channels (cyclic),
   // and delay (the delay is actually done by getSensors()
   void normal(){
     for(int i=0; i<motornumber; i++){
@@ -150,6 +162,23 @@ public:
       x[motornumber-1]= 0;
   }
 
+
+  // system that is swingable, and possible correlation of subsequent channels (cyclic),
+  // and delay (the delay is actually done by getSensors()
+  void swing(){
+    //    double* x_tm1 = x_buffer[(t-1+buffersize)%buffersize];    
+    for(int i=0; i<motornumber; i++){
+      // x_{t+1} = f(x) + x_t - x_{t-1} +  correlation
+      // x_{t+1} = f(x) - (1-1/m)* x_t - x_{t-1}
+      cout <<      x[i] << endl;
+      //      x[i] = tanh(x[i] - (1-1/(inertia+0.5))*x[i] - x_tm1[i]);
+    }    
+    if(disablesensor!=0.0)
+      x[motornumber-1]= 0;
+  }
+
+
+
 private:
   int motornumber;
   int sensornumber;
@@ -157,6 +186,7 @@ private:
 
   Mode mode;
   double* x;
+  double* v; // velocities
   double* y;
   double** x_buffer;
 
@@ -179,7 +209,7 @@ void printRobot(MyRobot* robot){
   for(int i=0; i<len; i++){
     double x = s[i];
     x=clip(x,-1.0,1.0);
-    line[int((x+1.0)/2.0*80.0)]='0'+ i;
+    line[int((x+1.0)/2.0*80.0)%80]='0'+ i;
   }
   
   printf("\033[1G%s",line);
@@ -206,10 +236,12 @@ int main(int argc, char** argv){
     plotoptions.push_back(PlotOption(GuiLogger,Controller,atoi(argv[index])));
   }
   if(contains(argv,argc,"-f")!=0) plotoptions.push_back(PlotOption(File));
+  if(contains(argv,argc,"-n")!=0) plotoptions.push_back(PlotOption(NeuronViz));
   index = contains(argv,argc,"-m");
   if(index >0 && argc>index) {
     modestr = argv[index];
     if(strcasecmp(modestr,"normal")==0) mode = NORMAL;
+    else if(strcasecmp(modestr,"swing")==0) mode = SWING;
     else if(strcasecmp(modestr,"extrasine")==0) mode = EXTRASINE;
     else { fprintf(stderr, "Unknown mode! See help -h\n"); exit(1);}
   }
@@ -217,9 +249,10 @@ int main(int argc, char** argv){
   if(index >0 && argc>index)
     dim=atoi(argv[index]); 
   if(contains(argv,argc,"-h")!=0) {
-    printf("Usage: %s [-g N] [-f] [-m MODE] [d DIM]\n",argv[0]);
+    printf("Usage: %s [-g N] [-f] [-n] [-m MODE] [d DIM]\n",argv[0]);
     printf("\t-g N\tstart guilogger with interval N\n\t-f\twrite logfile\n");
-    printf("\t-m MODE\t system properties: normal (def), extrasine\n");
+    printf("\t-n\tstart neuronviz\n");
+    printf("\t-m MODE\t system properties: normal (def), swing, extrasine\n");
     printf("\t-d DIM\t dimensionality, default 2\n");
     printf("\t-h\tdisplay this help\n");
     exit(0);
@@ -231,22 +264,36 @@ int main(int argc, char** argv){
   initializeConsole();
   
   
-   InvertMotorNStepConf cc = InvertMotorNStep::getDefaultConf();
-   //   cc.useSD=true;
-   cc.someInternalParams=false;
-   AbstractController* controller = new InvertMotorNStep(cc);
+//    InvertMotorNStepConf cc = InvertMotorNStep::getDefaultConf();
+//    //cc.useS=true;
+//    cc.someInternalParams=false;
+//    AbstractController* controller = new InvertMotorNStep(cc);
+  UniversalControllerConf cc = UniversalController::getDefaultConf();
+  vector<Layer> layers;
+  //   layers.push_back(Layer(20,0.5,FeedForwardNN::tanh)); // hidden layer
+  layers.push_back(Layer(0,1,FeedForwardNN::tanhr)); // motor layer
+  // size of output layer is automatically set
+  layers.push_back(Layer(1,0,FeedForwardNN::linear)); 
+  
+  Elman* e = new Elman(1,layers,false, false, false);
+  cc.net = e;
+  cc.init = 1;
+  cc.someInternalParams=false;
+  AbstractController* controller = new UniversalController(cc);
+  controller->setParam("epsDyn",     0);
+
    //  AbstractController* controller = new InvertMotorSpace(10,1.2);
 //  AbstractController* controller = new DerController();
 //  AbstractController* controller = new InvertNChannelController(10,false);
-  controller->setParam("s4delay",   1.0);
+  controller->setParam("s4del",     1.0);
   controller->setParam("s4avg",     1.0);  
   controller->setParam("adaptrate", 0.0);  
   controller->setParam("factorB",   0.01);  
   
   robot         = new MyRobot(string("Robot_") + string(modestr), mode, dim);
   agent         = new Agent(plotoptions);
-  //  AbstractWiring* wiring = new One2OneWiring(new ColorUniformNoise(0.1));  
-  AbstractWiring* wiring = new One2OneWiring(new WhiteUniformNoise());  
+  AbstractWiring* wiring = new One2OneWiring(new ColorUniformNoise(0.2),true);  
+  // AbstractWiring* wiring = new One2OneWiring(new WhiteUniformNoise(),true);  
   agent->init(controller, robot, wiring);
   // if you like, you can keep track of the robot with the following line. 
   //  this assumes that you robot returns its position, speed and orientation. 
