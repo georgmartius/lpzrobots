1d688ca663d173e1085e49f1cfbee1f55762e940
robot3 <robot3>
2008-01-08 14:58:20 +0000

    rg

--- javacontroller/NeuroController/Start.java
+++ javacontroller/NeuroController/Start.java
@@ -5,7 +5,7 @@ public class Start {
 	
 	public static void main(String[] args) {
 		
-		MyController C = new MyController("localhost",4711,4712);
+		NeuroController C = new NeuroController("localhost",4711,4712);
 		ControllerManager.addController( C );
 	}
 
