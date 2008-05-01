78a943c37d14a73ca798f289da45277adf0edeb3
martius <martius>
2008-05-01 17:28:38 +0000

    tools to create binary extra libs

---
+++ dist_utils/createextraDist.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+VERSION=0.4
+OS=`uname -s`;
+ARCH=`uname -m`;
+NAME=lpzrobots-$VERSION-extra-lib-devel_$OS.$ARCH
+BASE=../..
+TEMPLATE=lpzrobots-extra-lib-devel-template
+
+DIR=$BASE/$NAME
+INCDIR=$DIR/include
+LIBDIR=$DIR/lib
+BINDIR=$DIR/bin
+
+if [ -d $DIR ]; then
+    echo -n "The target directory $DIR exits, shall I delete it? [N]: ";
+    read YN;
+    if [ "$YN" = "Y" -o  "$YN" = "y" ]; then
+	rm -rf "$DIR";
+    else
+	echo "Remove it youself!";
+        exit 1;
+    fi
+fi
+cp -r $TEMPLATE $DIR || { echo "Error: copy template"; exit 1;}
+rm -r $DIR/CVS
+mkdir $INCDIR || { echo "Error: mkdir"; exit 1;}
+mkdir $LIBDIR || { echo "Error: mkdir"; exit 1;}
+mkdir $BINDIR || { echo "Error: mkdir"; exit 1;};
+
+echo -n "Where is your ode located? [/usr/local]: "
+read PREFIX;
+[ -z $PREFIX ] && PREFIX='/usr/local';
+[ ! -e $PREFIX/lib/libode.so ] && { echo "Error: $PREFIX/lib/libode.so not found"; exit 1;}
+
+echo "Copy ode stuff";
+cp $PREFIX/lib/libode.* $LIBDIR/;
+[ -e $PREFIX/lib/libdrawstuff.a ] && cp $PREFIX/lib/libdrawstuff.* $LIBDIR/;
+cp -r $PREFIX/include/ode $INCDIR/;
+
+echo -n "Where is your OSG located? [$PREFIX]:"
+read PREFIX_;
+
+[ -n "$PREFIX_" ] && PREFIX=$PREFIX_;
+[ ! -e $PREFIX/lib/libosg.so ] && { echo "Error: $PREFIX/lib/libosg.so not found"; exit 1;}
+
+echo "Copy OSG";
+cp -P $PREFIX/lib/libosg* $LIBDIR/;
+cp -P $PREFIX/lib/libOpenThreads* $LIBDIR/;
+cp -rP $PREFIX/lib/osgPlugins* $LIBDIR/;
+cp -rP $PREFIX/include/osg* $INCDIR/;
+cp -rP $PREFIX/include/OpenThreads $INCDIR/;
+ls $PREFIX/bin/osg* 2>&1 > /dev/null && cp  $PREFIX/bin/osg* $BINDIR/;
+
+echo "Successful";
---
+++ dist_utils/lpzrobots-extra-lib-devel-template/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+################################################################################
+# Install script
+# 
+# Project: lpzrobots extra libs and includes
+# Author: Georg Martius <georg@nld.ds.mpg.de>
+# Copyright: 2008 Georg Martius
+#
+################################################################################
+
+echo  "Welcome to the install script of lpzrobots-extra-lib-devel.";
+echo  "This package contains libaries and headerfiles for ODE and OSG required by lpzrobots";
+echo  "For updates and documentation visit http://robot.informatik.uni-leipzig.de";
+echo  "Press CTRL + C to abort";
+
+echo -e "\n-- Installation base directory DIR --";
+echo -e "\tLibaries go to \$DIR/lib/ ,binary files to \$DIR/bin/ and includes to \$DIR/include/";
+echo -e "\tFor system wide install keep the default (required root permissions)";
+echo -e "\tFor a user installions use /home/yourloginname and follow the instructions below";
+echo -en "\t**Enter base directory [/usr/local]: ";
+read dir;
+if [ -z "$dir" ]; then
+   dir='/usr/local';
+else
+    echo "Make sure that the following environment variables are set: PATH, LD_LIBRARY_PATH"
+    echo " if you use non-standard paths."
+    echo " for bash users add the following to your ~/.bash.bashrc:"
+    echo -e "\texport PATH=\$PATH:$dir/bin"
+    echo -e "\texport LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$dir/lib"    
+    echo -e "\tyou might have to modify the Makefiles by adding to INC and LIBS variables your paths" 
+fi
+
+if ! cp -r include lib bin $dir/; then
+ echo "ERROR: cannot copy the files to $dir/!"; 
+ exit 1;
+fi
+echo "Have Fun!";
+
