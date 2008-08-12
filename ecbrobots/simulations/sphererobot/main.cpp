f98028b5815089ba2e54007666a9434476ab846f
guettler <guettler>
2008-08-12 12:22:50 +0000

    added new custom MySimpleController for controlling motor values due to keyboard

--- ecbrobots/simulations/sphererobot/main.cpp
+++ ecbrobots/simulations/sphererobot/main.cpp
@@ -22,7 +22,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.4  2008-08-12 11:46:16  guettler
+ *   Revision 1.6  2008-08-12 12:22:50  guettler
+ *   added new custom MySimpleController for controlling motor values due to keyboard
+ *
+ *   Revision 1.5  2008/08/12 11:48:30  guettler
  *   plug and play update, added some features for the ECBRobotGUI
  *
  *   Revision 1.3  2008/07/16 14:37:17  robot1
@@ -55,9 +58,88 @@
 // fetch all the stuff of lpzrobots into scope
 using namespace lpzrobots;
 
+class MySimpleController : public AbstractController {
+	public:
+
+		  MySimpleController();
+
+		  /** initialisation of the controller with the given sensor/ motornumber
+			  Must be called before use. The random generator is optional.
+		  */
+		  virtual void init(int sensornumber, int motornumber, RandGen* randGen = 0) {
+			  this->sensorNumber=sensornumber;
+			  this->motorNumber=motornumber;
+			  this->motorvalues = (motor*) malloc(sizeof(motor)*motornumber);
+			  this->sensorvalues = (sensor*) malloc(sizeof(motor)*motornumber);
+			  this->addParameter("sinerate",&sineRate);
+			  this->addParameter("phaseShift",&phaseShift);
+			  sineRate=40;
+			  phaseShift=1;
+		  }
+
+		  /** @return Number of sensors the controller was initialised
+		      with or 0 if not initialised */
+		  virtual int getSensorNumber() const {return sensorNumber;}
+
+
+		  /** @return Number of motors the controller was initialised
+		      with or 0 if not initialised */
+		  virtual int getMotorNumber() const {return motorNumber;}
+
+		  /** performs one step (includes learning).
+		      Calculates motor commands from sensor inputs.
+		      @param sensors sensors inputs scaled to [-1,1]
+		      @param sensornumber length of the sensor array
+		      @param motors motors outputs. MUST have enough space for motor values!
+		      @param motornumber length of the provided motor array
+		  */
+		  virtual void step(const sensor* sensors, int sensornumber, motor* motors, int motornumber) {
+			  stepNoLearning(sensors,sensornumber,motors,motornumber);
+		  }
+
+		  /** performs one step without learning.
+		      @see step
+		  */
+		  virtual void stepNoLearning(const sensor* , int number_sensors, motor* , int number_motors) {
+			  // write internal values into motor array
+			  for (int i=0; i<number_motors; i++){
+				  motors[0]=motorValue[0];
+				  //motors[i]=sin(t/sineRate + i*phaseShift*M_PI/2); // sinecontroller
+			  }
+		  }
+
+		  virtual paramval getParam(const paramkey& key) const {
+
+		  }
+
+		  virtual bool setParam(const paramkey& key, paramval val) {
+
+		  }
+
+		  virtual paramlist getParamList() const  {
+
+		  }
+
+		  virtual bool store(FILE* f) const { return true;  }
+
+		  virtual bool restore(FILE* f) const { return true;  }
+
+
+     int t;
+	 std::string name;
+	 motor* motorValues;
+	 sensor* sensorValues;
+	 int sensorNumber;
+	 int motorNumber;
+	 paramval* sineRate;
+	 paramval* phaseShift;
+};
+
+
 class MyECBManager : public ECBManager {
 
   public:
+	  MySimpleController* myCon;
 
     MyECBManager() {}
 
@@ -81,7 +163,7 @@ class MyECBManager : public ECBManager {
 
 
       // create new controller
-      AbstractController* myCon1 = new InvertMotorSpace ( 10 );
+      myCon = new MySimpleController();
       // create new wiring
       AbstractWiring* myWiring1 = new One2OneWiring ( new WhiteNormalNoise() );
       // create new robot
@@ -94,12 +176,12 @@ class MyECBManager : public ECBManager {
       ecb2.adcTypes[1] = ADC_TILT;
       ecb2.adcTypes[2] = ADC_IR;
       ecb2.adcTypes[3] = ADC_IR;
-      //myRobot1->addECB ( 2,ecbc2 );
+      myRobot1->addECB ( 2,ecbc2 );
 
       // create new agent
       ECBAgent* myAgent1 = new ECBAgent(PlotOption(ECBRobotGUI));
       // init agent with controller, robot and wiring
-      myAgent1->init ( myCon1,myRobot1,myWiring1 );
+      myAgent1->init ( myCon,myRobot1,myWiring1 );
 
       // register agents
       global.agents.push_back ( myAgent1 );
@@ -127,6 +209,12 @@ class MyECBManager : public ECBManager {
         //        case 117:
         //        return true;
         //          break;
+      case 127:
+    	  myCon->motorValues[0]++;
+    	  break;
+      case 115:
+    	  myCon->motorValues[0]++;
+    	  break;
         default:
           std::cout << "key pressed: " << key << std::endl;
         return true;
