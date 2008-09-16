6daf288e2bdcc7c382b72ce1361f32fdecefaefd
martius <martius>
2008-09-16 14:48:05 +0000

    Code indentation

--- ode_robots/agents/odeagent.cpp
+++ ode_robots/agents/odeagent.cpp
@@ -20,7 +20,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.8  2008-04-29 08:44:20  guettler
+ *   Revision 1.9  2008-09-16 14:48:05  martius
+ *   Code indentation
+ *
+ *   Revision 1.8  2008/04/29 08:44:20  guettler
  *   #include <assert> added
  *
  *   Revision 1.7  2008/04/18 09:50:24  guettler
@@ -89,10 +92,10 @@ namespace lpzrobots {
 	init_tracing();
       }
       Pos pos(robot->getPosition());
-     /* if construct used to draw cylinder only when length between actual
-        and last point is larger then a specific value
-     */
-     //if(counter==0 || ((pos - lastpos).length2() > 0.00005)  ) {
+      /* if construct used to draw cylinder only when length between actual
+	 and last point is larger then a specific value
+      */
+      //if(counter==0 || ((pos - lastpos).length2() > 0.00005)  ) {
       double len = (pos - lastpos).length();
       if(segments[counter%trace_length]) delete segments[counter%trace_length];
       OSGPrimitive* s = new OSGCylinder(trace_thickness, len);
@@ -102,52 +105,47 @@ namespace lpzrobots {
       segments[counter%trace_length] = s;
       lastpos = pos;
       counter++;
-     //}
+      //}
     }
   }
 
 
-void OdeAgent::stepOnlyWiredController(double noise, double time) {
-  WiredController::step(rsensors,rsensornumber, rmotors, rmotornumber, noise, time);
-  trackrobot.track(robot, time);
-  if (trackrobot.isDisplayTrace() && t%10==0){
-    if (!tracing_initialized) {
-      init_tracing();
+  void OdeAgent::stepOnlyWiredController(double noise, double time) {
+    WiredController::step(rsensors,rsensornumber, rmotors, rmotornumber, noise, time);
+    trackrobot.track(robot, time);
+    if (trackrobot.isDisplayTrace() && t%10==0){
+      if (!tracing_initialized) {
+	init_tracing();
+      }
+      Pos pos(robot->getPosition());
+      /* if construct used to draw cylinder only when length between actual
+	 and last point is larger then a specific value
+      */
+      //if(counter==0 || ((pos - lastpos).length2() > 0.00005)  ) {
+      double len = (pos - lastpos).length();
+      if(segments[counter%trace_length]) delete segments[counter%trace_length];
+      OSGPrimitive* s = new OSGCylinder(trace_thickness, len);
+      s->init(((OdeRobot*)robot)->osgHandle, OSGPrimitive::Low);
+      s->setMatrix(osg::Matrix::rotate(osg::Vec3(0,0,1), (pos - lastpos)) *
+		   osg::Matrix::translate(lastpos+(pos - lastpos)/2));
+      segments[counter%trace_length] = s;
+      lastpos = pos;
+      counter++;
+      //}
     }
-    Pos pos(robot->getPosition());
-     /* if construct used to draw cylinder only when length between actual
-        and last point is larger then a specific value
-     */
-     //if(counter==0 || ((pos - lastpos).length2() > 0.00005)  ) {
-    double len = (pos - lastpos).length();
-    if(segments[counter%trace_length]) delete segments[counter%trace_length];
-    OSGPrimitive* s = new OSGCylinder(trace_thickness, len);
-    s->init(((OdeRobot*)robot)->osgHandle, OSGPrimitive::Low);
-    s->setMatrix(osg::Matrix::rotate(osg::Vec3(0,0,1), (pos - lastpos)) *
-                 osg::Matrix::translate(lastpos+(pos - lastpos)/2));
-    segments[counter%trace_length] = s;
-    lastpos = pos;
-    counter++;
-     //}
   }
-}
 
-    /**
-     * Special function for the class Simulation to seperate the step
-     * of the WiredController (plus TrackRobot) and the setting and getting
-     * of the motor- and sensorvalues.
-     */
-void OdeAgent::setMotorsGetSensors() {
-  robot->setMotors(rmotors, rmotornumber);
+  void OdeAgent::setMotorsGetSensors() {
+    robot->setMotors(rmotors, rmotornumber);
 
-  assert(robot && rsensors && rmotors);
+    assert(robot && rsensors && rmotors);
 
-  int len =  robot->getSensors(rsensors, rsensornumber);
-  if(len != rsensornumber){
-    fprintf(stderr, "%s:%i: Got not enough sensors, expected %i, got %i!\n", __FILE__, __LINE__,
-            rsensornumber, len);
+    int len =  robot->getSensors(rsensors, rsensornumber);
+    if(len != rsensornumber){
+      fprintf(stderr, "%s:%i: Got not enough sensors, expected %i, got %i!\n", __FILE__, __LINE__,
+	      rsensornumber, len);
+    }
   }
-}
 
 }
 
