71c3219beea64750d54f6da6f11539604220abaf
martius <martius>
2008-09-16 14:55:20 +0000

    different params and cmdline options

--- ode_robots/simulations/wheelie/main.cpp
+++ ode_robots/simulations/wheelie/main.cpp
@@ -21,7 +21,10 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.14  2008-05-01 22:03:56  martius
+ *   Revision 1.15  2008-09-16 14:55:20  martius
+ *   different params and cmdline options
+ *
+ *   Revision 1.14  2008/05/01 22:03:56  martius
  *   build system expanded to allow system wide installation
  *   that implies  <ode_robots/> for headers in simulations
  *
@@ -89,15 +92,24 @@
 
 // fetch all the stuff of lpzrobots into scope
 using namespace lpzrobots;
-
+double velScale = 4;
+double power    = 4;
+double powerRatio = 0.5;
+double eps       = 0.01;
 
 class ThisSim : public Simulation {
 public:
+  AbstractController *controller;
+  OdeRobot* robot;
+  int useReinforcement;
+  double totalReinforcement;
 
   // starting function (executed once at the beginning of the simulation loop)
   void start(const OdeHandle& odeHandle, const OsgHandle& osgHandle, GlobalData& global) 
   {
 
+    useReinforcement      = 1;
+    totalReinforcement    = 0;
     bool useWheelie       = false;
     bool useSliderWheelie = true;
 
@@ -107,9 +119,9 @@ public:
     // - register file chess.ppm as a texture called chessTexture (used for the wheels)
 
     global.odeConfig.setParam("noise",0.05);
-    global.odeConfig.setParam("controlinterval",1);
-    global.odeConfig.setParam("gravity",-2); // normally at -9.81
-    global.odeConfig.setParam("realtimefactor",1); 
+    global.odeConfig.setParam("controlinterval",2);
+    global.odeConfig.setParam("gravity",-3); // normally at -9.81
+    //    global.odeConfig.setParam("realtimefactor",1);
     // initialization
     
 //     Playground* playground = new Playground(odeHandle, osgHandle, osg::Vec3(30, 0.2, 1));
@@ -125,12 +137,10 @@ public:
 //       global.obstacles.push_back(s);    
 //     }
         
-    OdeAgent *agent, *slideragent;
-    AbstractWiring *wiring, *sliderwiring;
-    AbstractController *controller, *slidercontroller;
+    OdeAgent *agent;
+    AbstractWiring *wiring;
 
-    if(useWheelie){
-      Wheelie *myWheelie;
+    if(useWheelie){      
       WheelieConf myWheelieConf = DefaultWheelie::getDefaultConf();
       /******* w H E E L I E *********/
       myWheelieConf.jointLimit=M_PI/3;
@@ -140,58 +150,92 @@ public:
       //    myWheelieConf.segmLength = 0.2;
       //    myWheelieConf.segmDia = 0.1;
       myWheelieConf.motorPower = 1;
-      myWheelie = new Wheelie(odeHandle, osgHandle, myWheelieConf, "Wheelie1");
-      ((OdeRobot*) myWheelie)->place(Pos(-5,-6,0.2)); 
+      robot = new Wheelie(odeHandle, osgHandle, myWheelieConf, "Wheelie1");
+      ((OdeRobot*) robot)->place(Pos(-5,-6,0.2)); 
       InvertMotorNStepConf invertnconf = InvertMotorNStep::getDefaultConf();
       controller = new InvertMotorNStep(invertnconf);    
       wiring = new One2OneWiring(new ColorUniformNoise(0.1));
       agent = new OdeAgent(plotoptions);
-      agent->init(controller, myWheelie, wiring);
+      agent->init(controller, robot, wiring);
       global.agents.push_back(agent);
       global.configs.push_back(controller);
-      global.configs.push_back(myWheelie);   
-      myWheelie->setParam("gamma", 0.0);
+      global.configs.push_back(robot);   
+      robot->setParam("gamma", 0.0);
     }
 
     if(useSliderWheelie){
-      SliderWheelie *mySliderWheelie;
       SliderWheelieConf mySliderWheelieConf = SliderWheelie::getDefaultConf();
       /******* S L I D E R - w H E E L I E *********/
       mySliderWheelieConf.segmNumber=12;
-      mySliderWheelieConf.jointLimit=M_PI/2;
-      mySliderWheelieConf.motorPower=0.5;
-      mySliderWheelieConf.frictionGround=0.8;
+      mySliderWheelieConf.jointLimitIn=M_PI/3;
+      mySliderWheelieConf.motorPower=power; // 2 
+      mySliderWheelieConf.powerRatio=powerRatio; // 2; 
+      mySliderWheelieConf.frictionGround=0.5;
       mySliderWheelieConf.sliderLength=0.5;
       mySliderWheelieConf.segmLength=1.4;
-      mySliderWheelie = new SliderWheelie(odeHandle, osgHandle, mySliderWheelieConf, "sliderWheelie1");
-      ((OdeRobot*) mySliderWheelie)->place(Pos(-5,-3,0.0)); 
+      robot = new SliderWheelie(odeHandle, osgHandle, mySliderWheelieConf, "sliderWheelie1");
+      ((OdeRobot*) robot)->place(Pos(-5,-3,3.0)); 
       InvertMotorNStepConf sliderinvertnconf = InvertMotorNStep::getDefaultConf();
-      sliderinvertnconf.cInit=0.5;
-      sliderinvertnconf.useSD=true;
-      slidercontroller = new InvertMotorNStep(sliderinvertnconf);    
-      //slidercontroller = new SineController();
-      slidercontroller->setParam("noiseY",0);
-      //    slidercontroller->setParam("epsC",0);
-      slidercontroller->setParam("adaptrate",0.005);
-      slidercontroller->setParam("rootE",3); 
-      slidercontroller->setParam("steps",1);
-      slidercontroller->setParam("factorB",0);
+      //      sliderinvertnconf.cInit=0.1;
+      sliderinvertnconf.cInit=1;
+      // sliderinvertnconf.useSD=true;
+      controller = new InvertMotorNStep(sliderinvertnconf);    
+      //controller = new SineController();
+      controller->setParam("noiseY",0);
+      controller->setParam("epsC",eps); // 0.01
+      controller->setParam("epsA",eps);
+      controller->setParam("adaptrate",0.00);
+      controller->setParam("logaE",3); 
+      controller->setParam("steps",1);
+      controller->setParam("factorB",0);
 
       DerivativeWiringConf c = DerivativeWiring::getDefaultConf();
       c.useId = false;
       c.useFirstD = true;
-      //sliderwiring = new DerivativeWiring ( c , new ColorUniformNoise(0.1) );
-      sliderwiring = new One2OneWiring(new ColorUniformNoise(0.1));
-      slideragent = new OdeAgent(plotoptions);
-      slideragent->init(slidercontroller, mySliderWheelie, sliderwiring);
-      global.agents.push_back(slideragent);
-      global.configs.push_back(slidercontroller);
-      global.configs.push_back(mySliderWheelie);   
+      //wiring = new DerivativeWiring ( c , new ColorUniformNoise(0.1) );
+      wiring = new One2OneWiring(new ColorUniformNoise(0.1));
+      agent = new OdeAgent(plotoptions);
+      agent->init(controller, robot, wiring);
+      global.agents.push_back(agent);
+      global.configs.push_back(controller);
+      global.configs.push_back(robot);   
     }
 
     showParams(global.configs);
 
   }
+
+  virtual void addCallback(GlobalData& globalData, bool draw, bool pause, bool control) {
+    static Position lastPos = robot->getPosition();
+    if(useReinforcement==1 && control){ // speed reinforcement
+      Position pos = robot->getPosition();
+      Position speed = (pos - lastPos) * 
+	(globalData.odeConfig.controlInterval / globalData.odeConfig.simStepSize);
+      lastPos=pos;
+      double vel = sqrt(speed.x*speed.x + speed.y*speed.y);
+      //	matrix::Matrix m(3,1, speed.toArray());
+      //c->setReinforcement(tanh(sqrt(m.map(sqr).elementSum())/4 - 1)); 
+      double reinf = tanh(vel/velScale - 1);
+      totalReinforcement += reinf;	
+      InvertMotorNStep* c = dynamic_cast<InvertMotorNStep*>(controller);
+      if(c){
+	c->setReinforcement(reinf);
+      }
+    }
+  }
+
+  void end(GlobalData& globalData) {
+    FILE* f;
+    f = fopen("result","w");
+    if(!f) return;
+    //    fprintf(f,"#C power powerRatio eps velScale seed reinf_ps\n"); 
+    fprintf(f,"%f %f %f %f %li %f\n",
+	    power, powerRatio, eps, velScale, 
+	    globalData.odeConfig.randomSeed, 
+	    totalReinforcement/globalData.time); 
+    fclose(f);    
+  }
+
   // add own key handling stuff here, just insert some case values
   virtual bool command(const OdeHandle&, const OsgHandle&, GlobalData& globalData, int key, bool down)
   {
@@ -208,9 +252,20 @@ public:
   
 };
 
+
 int main (int argc, char **argv)
 { 
   ThisSim sim;
+  int index = ThisSim::contains(argv,argc,"--vals");
+  if(index >0 && argc>index+3) {
+    power      = atof(argv[index++]);
+    powerRatio = atof(argv[index++]);
+    eps        = atof(argv[index++]);
+    velScale   = atof(argv[index++]);
+    printf("Params: P %f, PF %f, eps %f, VS %f\n", 
+	   power, powerRatio, eps, velScale);
+  }
+  
   // run simulation
   return sim.run(argc, argv) ? 0 : 1;
 }
