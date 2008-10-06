3535986d2f9bf26e7c1507390b22ec795e6636b5
martius <martius>
2008-10-06 14:24:07 +0000

    newer automake version

--- opende/autogen.sh
+++ opende/autogen.sh
@@ -6,26 +6,26 @@
 
 ## The reason this uses sed instead of "grep --only-matches"
 ## is because MinGW's grep is an old version and does not contain that flag.
-#automake_version=`automake --version | grep --regexp='[+0-9].[+0-9].[+0-9]' | sed -n 's/[* ()A-Za-z]//g;p'`
-#automake_mayor=${automake_version%.*.*}
-#automake_minor=${automake_version%.*}
-#automake_minor=${automake_minor##*.}
-#automake_revision=${automake_version##*.}
-#echo "AutoMake Version: $automake_mayor.$automake_minor.$automake_revision"
-#
-#if [ $automake_mayor -eq 1 ]; then
-#    if [ $automake_minor -lt 8 ]; then
-#	echo "Automake must be 1.8.2 or higher, please upgrade"
-#	exit
-#    else
-#	if [ $automake_minor -eq 8 ] && [ $automake_revision -lt 2 ]; then
-#	    echo "Automake must be 1.8.2 or higher, please upgrade"
-#	    exit
-#	fi
-#    fi
-#fi
+automake_version=`automake --version | grep --regexp='[+0-9].[+0-9].[+0-9]' | sed -n 's/[* ()A-Za-z]//g;p'`
+automake_mayor=${automake_version%.*.*}
+automake_minor=${automake_version%.*}
+automake_minor=${automake_minor##*.}
+automake_revision=${automake_version##*.}
+echo "AutoMake Version: $automake_mayor.$automake_minor.$automake_revision"
 
-echo "Please make sure that you use automake 1.8.2 or later"
+if [ $automake_mayor -eq 1 ]; then
+    if [ $automake_minor -lt 8 ]; then
+	echo "Automake must be 1.8.2 or higher, please upgrade"
+	exit
+    else
+	if [ $automake_minor -eq 8 ] && [ $automake_revision -lt 2 ]; then
+	    echo "Automake must be 1.8.2 or higher, please upgrade"
+	    exit
+fi
+    fi
+fi
+
+#echo "Please make sure that you use automake 1.8.2 or later"
 echo "Warnings about underquoted definitions are harmless"
  
 echo "Running aclocal"
