71e5bc8cb4c8111444c1c8f3b8624b0298bfbd33
robot12 <robot12>
2009-04-30 11:51:25 +0000

    some implements... new classes

---
+++ ga_tools/GenAlg/ValueMutationStrategie.h
@@ -0,0 +1,44 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2009 by Robot Group Leipzig                        *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *    guettler@informatik.uni-leipzig.de                                   *
+ *    jhoffmann@informatik.uni-leipzig.de                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
+ *                                                                         *
+ *   Informative Beschreibung der Klasse                                   *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1  2009-04-30 11:51:25  robot12
+ *   some implements... new classes
+ *
+ *
+ *
+ ***************************************************************************/
+
+#ifndef VALUEMUTATIONSTRATEGIE_H_
+#define VALUEMUTATIONSTRATEGIE_H_
+
+class ValueMutationStrategie {
+public:
+	ValueMutationStrategie();
+	virtual ~ValueMutationStrategie();
+};
+
+#endif /* VALUEMUTATIONSTRATEGIE_H_ */
