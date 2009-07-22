52b1ff4c2e82406bf109df51f0abdf126ca1c095
robot12 <robot12>
2009-07-22 13:21:14 +0000

    bugfix in randomgenerator. so it should be work on a 64bit system. see comment in line 61 for the union around the buffer.

--- selforg/utils/randomgenerator.h
+++ selforg/utils/randomgenerator.h
@@ -23,7 +23,11 @@
  *   Random generator with internal state used for multitheading envs.     *
  *                                                                         *
  *   $Log$
- *   Revision 1.4  2009-07-21 08:47:33  robot12
+ *   Revision 1.5  2009-07-22 13:21:14  robot12
+ *   bugfix in randomgenerator. so it should be work on a 64bit system.
+ *   see comment in line 61 for the union around the buffer.
+ *
+ *   Revision 1.4  2009/07/21 08:47:33  robot12
  *   add some comments
  *
  *   Revision 1.3  2009/07/15 08:33:58  guettler
@@ -51,16 +55,35 @@ typedef struct _RandGen {
     init(::rand());
   }
   void init(long int seedval){
-    srand48_r(seedval, buffer);
+    srand48_r(seedval, &buffer);
   }
   double rand(){
     double r;
-    drand48_r(buffer,&r);
+    drand48_r(&buffer,&r);
     return r;
   }
   // allocate more memory than needed, because there is a bug
-  // with 64bit compilers: dran48_r overwrites too much data!
-  struct drand48_data buffer[2];
+  // with 64bit compilers: drand48_r overwrites too much data!
+  //
+  // see drand48_data structure
+  //struct drand48_data
+  //  {
+  //    unsigned short int __x[3];	/* Current state.  */
+  //    unsigned short int __old_x[3]; /* Old state.  */
+  //    unsigned short int __c;	/* Additive const. in congruential formula.  */
+  //    unsigned short int __init;	/* Flag for initializing.  */
+  //    unsigned long long int __a;	/* Factor in congruential formula.  */
+  //  };
+  //
+  // the function drand48_r overrides in __x. I think he write in the 8 Byte great array 16 Bytes.
+  // And so this destroy the call stack. With a union and an char array of 24 Bytes i force him,
+  // to write all elements in the structure aligned. The override writes so in the __old:x array
+  // and the call stack is save. Isn't the best way, but everything which isn't predictable is in
+  // a random generator perfect. :o)
+  union {
+	  struct drand48_data buffer;
+	  char dummy[23];
+  };
 } RandGen;
 
 #endif
