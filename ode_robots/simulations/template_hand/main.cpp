0bf1745584acb0b6ce3350d7ca04d80099c50258
fhesse <fhesse>
2009-09-10 12:12:38 +0000

    cleaned up

--- ode_robots/simulations/template_hand/main.cpp
+++ ode_robots/simulations/template_hand/main.cpp
@@ -20,59 +20,21 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  *                                                                         *
  *   $Log$
- *   Revision 1.1  2009-09-10 10:25:01  fhesse
+ *   Revision 1.2  2009-09-10 12:12:38  fhesse
+ *   cleaned up
+ *
+ *   Revision 1.1  2009/09/10 10:25:01  fhesse
  *   initial and preleminary version
  *
- *   Revision 1.20  2008/11/14 11:23:05  martius
- *   added centered Servos! This is useful for highly nonequal min max values
- *   skeleton has now also a joint in the back
- *
- *   Revision 1.19  2008/05/01 22:03:56  martius
- *   build system expanded to allow system wide installation
- *   that implies  <ode_robots/> for headers in simulations
- *
- *   Revision 1.18  2007/01/26 12:07:08  martius
- *   orientationsensor added
- *
- *   Revision 1.17  2006/08/04 15:07:47  martius
- *   documentation
- *
- *   Revision 1.16  2006/07/14 12:23:54  martius
- *   selforg becomes HEAD
- *
- *   Revision 1.15.4.5  2006/05/15 13:11:30  robot3
- *   -handling of starting guilogger moved to simulation.cpp
- *    (is in internal simulation routine now)
- *   -CTRL-F now toggles logging to the file (controller stuff) on/off
- *   -CTRL-G now restarts the GuiLogger
- *
- *   Revision 1.15.4.4  2006/02/20 10:50:20  martius
- *   pause, random, windowsize, Ctrl-keys
- *
- *   Revision 1.15.4.3  2006/01/12 15:17:39  martius
- *   *** empty log message ***
- *
- *   Revision 1.15.4.2  2006/01/10 20:33:50  martius
- *   moved to osg
- *
- *   Revision 1.15.4.1  2005/11/15 12:30:17  martius
- *   new selforg structure and OdeAgent, OdeRobot ...
- *
- *   Revision 1.15  2005/11/09 14:54:46  fhesse
- *   nchannelcontroller used
- *
- *   Revision 1.14  2005/11/09 13:41:25  martius
- *   GPL'ised
- *
- ***************************************************************************/
+ ****************************************************************************/
 
 // include simulation environment stuff
 #include <ode_robots/simulation.h>
 
 // include agent (class for holding a robot, a controller and a wiring)
 #include <ode_robots/odeagent.h>
-#include <ode_robots/octaplayground.h> // arena
 #include <ode_robots/passivesphere.h>  // passive balls
+#include <ode_robots/passivecapsule.h> // passive capsules
 
 // controller
 #include <selforg/invertmotorspace.h>
@@ -84,25 +46,31 @@
 // robots
 #include <ode_robots/hand.h>
 
-//#include <ode_robots/sphererobot3masses.h>
-//#include <ode_robots/axisorientationsensor.h>
-//
-//#include <ode_robots/axisorientationsensor.h>
-
 // fetch all the stuff of lpzrobots into scope
 using namespace lpzrobots;
+
+// choose between sine and self-org-controller
+#define SINEController
 
 
 class ThisSim : public Simulation {
 public:
   AbstractController* controller;
   OdeRobot* hand;
-//  Sphererobot3Masses* sphere1;
+
+  // fixes the hand in the sky
+  Joint* fixator;
+
+  // passive object for playing
+  PassiveCapsule* passive_capsule;
+
 
   // starting function (executed once at the beginning of the simulation loop)
   void start(const OdeHandle& odeHandle, const OsgHandle& osgHandle, GlobalData& global) 
   {
-    setCameraHomePos(Pos(5.2728, 7.2112, 3.31768), Pos(140.539, -13.1456, 0));
+    setCameraHomePos (Pos(12.1211, 5.91774, 7.22559),  Pos(110.806, -12.6131, 0));
+
+
     // initialization
     // - set global noise to 0.1
     global.odeConfig.setParam("noise",0.1);
@@ -113,11 +81,17 @@ public:
     //   optional parameters radius and mass,where the latter is not used here) )
     // - set Pose(Position) of sphere 
     // - add sphere to list of obstacles
-    for(int i=0; i<8; i++){
-      PassiveSphere* s = new PassiveSphere(odeHandle, osgHandle.changeColor(Color(0.0,1.0,0.0)), 0.5);
-      s->setPosition(osg::Vec3(5,0,i*3)); 
+    for(int i=0; i<1; i++){
+      PassiveSphere* s = new PassiveSphere(odeHandle, osgHandle.changeColor(Color(0.0,1.0,0.0)), 1/*0.5*/);
+      s->setPosition(osg::Vec3(0,0,4+i*2)); 
       global.obstacles.push_back(s);    
     }
+
+    // simulated robotic hand
+    // - get default configuration for robot
+    // - adapt configuration
+    // - create pointer to spherical robot (with odeHandle, osgHandle and configuration)
+    // - place robot 
 
     HandConf conf = Hand::getDefaultConf();
     conf.velocity = 0.02;
@@ -131,47 +105,41 @@ public:
     conf.irs_at_fingerbottom =false;
     conf.servo_motor_Power = 1.2;
     conf.fix_palm_joint=true;
-    conf.one_finger_as_one_motor=true;
+    conf.one_finger_as_one_motor=false;//true;
     conf.draw_joints=false;//true;
-    conf.showFingernails=false;
+    conf.showFingernails=false; // not supported yet
     conf.fingerJointBendAngle=M_PI*2/5;
     conf.initWithOpenHand=true;
     hand = new Hand(odeHandle, osgHandle,conf,"Hand");
     hand->setColor(Color(1.0,0.5,1.0));
-    hand->place(Pos(0,0,6));
+    hand->place(Pos(0,0,3));
     global.configs.push_back ( hand );
 
-    // Spherical Robot with axis (gyro) sensors:
-    // - get default configuration for robot
-    // - create pointer to spherical robot (with odeHandle, osgHandle and configuration)
-    // - place robot (unfortunatelly there is a type cast necessary, which is not quite understandable)
-//    Sphererobot3MassesConf conf = Sphererobot3Masses::getDefaultConf();
-//    conf.addSensor(new AxisOrientationSensor(AxisOrientationSensor::ZProjection));
-//    // regular behaviour
-//    conf.motorsensor=false;
-//    conf.diameter=1.0;
-//    conf.pendularrange= 0.2;
-//     conf.diameter=1.0;
-//     conf.pendularrange= 0.35;
-//    sphere1 = new Sphererobot3Masses ( odeHandle, osgHandle.changeColor(Color(1.0,0.0,0)),
-//				       conf, "Sphere1", 0.2);
-//    ((OdeRobot*)sphere1)->place ( Pos( 0 , 0 , 0.1 ));
-//    global.configs.push_back ( sphere1 );
+    // fix hand (in actual position) to simulation environment
+    Primitive* trunk = hand->getMainPrimitive();
+    fixator = new FixedJoint(trunk, global.environment);
+    fixator->init(odeHandle, osgHandle);
 
+
+#ifdef SINEController
+    //SineController (produces just sine waves)
+    // create pointer to controller
+    // set some parameters
+    controller = new SineController();  
+    controller->setParam("amplitude", 1);  
+    controller->setParam("period", 500);  
+    controller->setParam("phaseshift", 0.5);
+
+#else 
     // Selforg - Controller
     // create pointer to controller
     // set some parameters
+    controller = new InvertMotorSpace(10);
+    controller->setParam("epsA",0.3); // model learning rate
+    controller->setParam("epsC",0.3); // controller learning rate
+    controller->setParam("rootE",3);    // model and contoller learn with square rooted error
+#endif
     // push controller in global list of configurables
-//    controller = new InvertMotorSpace(10);
-//    controller->setParam("epsA",0.3); // model learning rate
-//    controller->setParam("epsC",0.3); // controller learning rate
-//    controller->setParam("rootE",3);    // model and contoller learn with square rooted error
-//    global.configs.push_back ( controller );
-
-    //SineController (produces just sine waves)
-    controller = new SineController();  
-    controller->setParam("sinerate", 40);  
-    controller->setParam("phaseshift", 0.0);
     global.configs.push_back ( controller );
     
     // create pointer to one2onewiring which uses colored-noise 
@@ -182,8 +150,6 @@ public:
     // push agent in globel list of agents    
     OdeAgent* agent = new OdeAgent ( plotoptions );
     agent->init ( controller , hand , wiring );
-    // the following line will enable a position tracking of the robot, which is written into a file
-    //agent->setTrackOptions(TrackRobot(true, true, true, false, "Sphere_zaxis", 20));
     global.agents.push_back ( agent );
       
     // display all parameters of all configurable objects on the console
@@ -198,16 +164,17 @@ public:
 		       int key, bool down) { 
     if (down) { // only when key is pressed, not when released
       switch ( (char) key ) {
-//      case 'X' : dBodyAddForce ( sphere1->getMainPrimitive()->getBody() , 30 ,0 , 0 ); break;
-//      case 'x' : dBodyAddForce ( sphere1->getMainPrimitive()->getBody() , -30 , 0 , 0 ); break;
-//      case 'T' : dBodyAddTorque ( sphere1->getMainPrimitive()->getBody() , 0 , 0 , 3 ); break;
-//      case 't' : dBodyAddTorque ( sphere1->getMainPrimitive()->getBody() , 0 , 0 , -3 ); break;
-////       case 'S' : controller->setParam("sineRate", controller->getParam("sineRate")*1.2);
-//// 	printf("sineRate : %g\n", controller->getParam("sineRate"));
-////       break;
-////       case 's' : controller->setParam("sineRate", controller->getParam("sineRate")/1.2);
-//// 	printf("sineRate : %g\n", controller->getParam("sineRate"));
-//// 	break;
+	case 'x': 
+	  if(fixator) delete fixator;
+	  fixator=0;	 
+	  break;
+        case 'c' :{
+	  passive_capsule =  new PassiveCapsule(odeHandle, osgHandle, 1,1,5);
+      	  passive_capsule->setColor(Color(1.0f,0.2f,0.2f,1.0f));
+      	  passive_capsule->setTexture("Images/furry_toy.jpg");
+      	  passive_capsule->setPosition(Pos(0,0,5)); 
+      	  globalData.obstacles.push_back(passive_capsule); }
+	  break;
       default:
 	return false;	
       }
@@ -216,12 +183,8 @@ public:
   }  
 
   virtual void bindingDescription(osg::ApplicationUsage & au) const {
-//    au.addKeyboardMouseBinding("Simulation: X","Push robot to right (positive x)");
-//    au.addKeyboardMouseBinding("Simulation: x","Push robot to left (negative x)");
-//    au.addKeyboardMouseBinding("Simulation: T","Spin robot counter-clockwise");
-//    au.addKeyboardMouseBinding("Simulation: t","Spin robot clockwise");
-//    //    au.addKeyboardMouseBinding("Controller: S","Increase sine frequency");
-//    //    au.addKeyboardMouseBinding("Controller: s","Decrease sine frequency");
+    au.addKeyboardMouseBinding("Simulation: x","Delete fixation joint (hand will fall down)");
+    au.addKeyboardMouseBinding("Simulation: c","Create Capsule in hand");
   }
 
 };
