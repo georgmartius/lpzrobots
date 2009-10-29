f70bef3b6af526c322af0568dee3192ee5130fdf
martius <martius>
2009-10-29 15:23:43 +0000

    implementation of random gen for non-gnu systems

---
+++ selforg/utils/randomgenerator.cpp
@@ -0,0 +1,109 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *    guettler@informatik.uni-leipzig.de                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************
+ *                                                                         *
+ *                                                                         *
+ *  $Log$
+ *  Revision 1.1  2009-10-29 15:23:43  martius
+ *  implementation of random gen for non-gnu systems
+ *
+ *                                                                         *
+ **************************************************************************/
+#include "randomgenerator.h"
+
+#ifndef _GNU_SOURCE
+
+int __drand48_iterate (unsigned short int xsubi[3], struct drand48_data* buffer){
+  uint64_t X;
+  uint64_t result;
+
+  /* Initialize buffer, if not yet done.  */
+  if (__builtin_expect (!buffer->__init, 0))
+    {
+      buffer->__a = 0x5deece66dull;
+      buffer->__c = 0xb;
+      buffer->__init = 1;
+    }
+
+  /* Do the real work.  We choose a data type which contains at least
+     48 bits.  Because we compute the modulus it does not care how
+     many bits really are computed.  */
+
+  X = (uint64_t) xsubi[2] << 32 | (uint32_t) xsubi[1] << 16 | xsubi[0];
+
+  result = X * buffer->__a + buffer->__c;
+
+  xsubi[0] = result & 0xffff;
+  xsubi[1] = (result >> 16) & 0xffff;
+  xsubi[2] = (result >> 32) & 0xffff;
+
+  return 0;
+}
+
+int __erand48_r (unsigned short int xsubi[3], struct drand48_data *buffer,
+                  double *result) {
+  union ieee754_double temp;
+
+  /* Compute next state.  */
+  if (__drand48_iterate (xsubi, buffer) < 0)
+    return -1;
+
+  /* Construct a positive double with the 48 random bits distributed over
+     its fractional part so the resulting FP number is [0.0,1.0).  */
+
+  temp.ieee.negative = 0;
+  temp.ieee.exponent = IEEE754_DOUBLE_BIAS;
+  temp.ieee.mantissa0 = (xsubi[2] << 4) | (xsubi[1] >> 12);
+  temp.ieee.mantissa1 = ((xsubi[1] & 0xfff) << 20) | (xsubi[0] << 4);
+
+  /* Please note the lower 4 bits of mantissa1 are always 0.  */
+  *result = temp.d - 1.0;
+
+  return 0;
+}
+
+
+int srand48_r (long int seedval, struct drand48_data *buffer);
+{
+  /* The standards say we only have 32 bits.  */
+  if (sizeof (long int) > 4)
+    seedval &= 0xffffffffl;
+
+  buffer->__x[2] = seedval >> 16;
+  buffer->__x[1] = seedval & 0xffffl;
+  buffer->__x[0] = 0x330e;
+
+  buffer->__a = 0x5deece66dull;
+  buffer->__c = 0xb;
+  buffer->__init = 1;
+
+  return 0;
+}
+
+
+int drand48_r ( struct drand48_data *buffer, double *result)
+{
+  return __erand48_r (buffer->__x, buffer, result);
+}
+
+
+#endif
