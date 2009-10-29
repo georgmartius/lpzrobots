e64723b3eacfbdcc6771324c03676c695d89ad47
martius <martius>
2009-10-29 15:24:20 +0000

    removed implementation, just declaration

--- selforg/utils/mac_drand48r.h
+++ selforg/utils/mac_drand48r.h
@@ -23,7 +23,10 @@
  *   This is a copy of the stdlib version                                  *
  *                                                                         *
  *   $Log$
- *   Revision 1.6  2009-10-29 15:20:03  fhesse
+ *   Revision 1.7  2009-10-29 15:24:20  martius
+ *   removed implementation, just declaration
+ *
+ *   Revision 1.6  2009/10/29 15:20:03  fhesse
  *   *** empty log message ***
  *
  *   Revision 1.5  2009/10/29 14:51:14  fhesse
@@ -90,78 +93,14 @@ struct drand48_data {
 };
 
 
-int __drand48_iterate (unsigned short int xsubi[3], struct drand48_data* buffer){
-  uint64_t X;
-  uint64_t result;
-
-  /* Initialize buffer, if not yet done.  */
-  if (__builtin_expect (!buffer->__init, 0))
-    {
-      buffer->__a = 0x5deece66dull;
-      buffer->__c = 0xb;
-      buffer->__init = 1;
-    }
-
-  /* Do the real work.  We choose a data type which contains at least
-     48 bits.  Because we compute the modulus it does not care how
-     many bits really are computed.  */
-
-  X = (uint64_t) xsubi[2] << 32 | (uint32_t) xsubi[1] << 16 | xsubi[0];
-
-  result = X * buffer->__a + buffer->__c;
-
-  xsubi[0] = result & 0xffff;
-  xsubi[1] = (result >> 16) & 0xffff;
-  xsubi[2] = (result >> 32) & 0xffff;
-
-  return 0;
-}
+int __drand48_iterate (unsigned short int xsubi[3], struct drand48_data* buffer);
 
 int __erand48_r (unsigned short int xsubi[3], struct drand48_data *buffer,
-                  double *result) {
-  union ieee754_double temp;
+                 double *result);
 
-  /* Compute next state.  */
-  if (__drand48_iterate (xsubi, buffer) < 0)
-    return -1;
+int srand48_r (long int seedval, struct drand48_data *buffer);
 
-  /* Construct a positive double with the 48 random bits distributed over
-     its fractional part so the resulting FP number is [0.0,1.0).  */
-
-  temp.ieee.negative = 0;
-  temp.ieee.exponent = IEEE754_DOUBLE_BIAS;
-  temp.ieee.mantissa0 = (xsubi[2] << 4) | (xsubi[1] >> 12);
-  temp.ieee.mantissa1 = ((xsubi[1] & 0xfff) << 20) | (xsubi[0] << 4);
-
-  /* Please note the lower 4 bits of mantissa1 are always 0.  */
-  *result = temp.d - 1.0;
-
-  return 0;
-}
-
-
-int srand48_r (long int seedval, struct drand48_data *buffer)
-{
-  /* The standards say we only have 32 bits.  */
-  if (sizeof (long int) > 4)
-    seedval &= 0xffffffffl;
-
-  buffer->__x[2] = seedval >> 16;
-  buffer->__x[1] = seedval & 0xffffl;
-  buffer->__x[0] = 0x330e;
-
-  buffer->__a = 0x5deece66dull;
-  buffer->__c = 0xb;
-  buffer->__init = 1;
-
-  return 0;
-}
-
-
-int drand48_r ( struct drand48_data *buffer, double *result)
-{
-  return __erand48_r (buffer->__x, buffer, result);
-}
+int drand48_r ( struct drand48_data *buffer, double *result);
 
 
 #endif
