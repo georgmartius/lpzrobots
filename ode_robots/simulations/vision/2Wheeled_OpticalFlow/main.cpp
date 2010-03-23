665cc06f1fd06dc6c22514ec376d08b7c651b818
martius <martius>
2010-03-23 18:42:26 +0000

    new simulation for vision

---
+++ ode_robots/simulations/vision/2Wheeled_OpticalFlow/main.cpp
@@ -0,0 +1,165 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *    frankguettler@gmx.de                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1  2010-03-23 18:42:26  martius
+ *   new simulation for vision
+ *
+ *
+ ***************************************************************************/
+#include <stdio.h>
+
+// include noisegenerator (used for adding noise to sensorvalues)
+#include <selforg/noisegenerator.h>
+#include <selforg/invertmotorspace.h>
+#include <selforg/semox.h>
+#include <selforg/sinecontroller.h>
+#include <selforg/one2onewiring.h>
+#include <selforg/selectiveone2onewiring.h>
+
+// include simulation environment stuff
+#include <ode_robots/simulation.h>
+
+// include agent (class for holding a robot, a controller and a wiring)
+#include <ode_robots/odeagent.h>
+
+
+// used arena
+#include <ode_robots/playground.h>
+// used passive spheres
+#include <ode_robots/passivesphere.h>
+#include <ode_robots/passivebox.h>
+
+#include <ode_robots/camera.h>
+#include <ode_robots/imageprocessors.h>
+#include <ode_robots/camerasensors.h>
+#include <ode_robots/opticalflow.h>
+
+#include <ode_robots/twowheeled.h>
+
+// fetch all the stuff of lpzrobots into scope
+using namespace lpzrobots;
+using namespace std;
+
+class ThisSim : public Simulation {
+public:
+
+
+  AbstractObstacle* playground;
+
+  
+  // starting function (executed once at the beginning of the simulation loop)
+  void start(const OdeHandle& odeHandle, const OsgHandle& osgHandle, GlobalData& global)
+  {
+    int numSeeingRobots=1;
+    int numBlindRobots=1;
+
+    setCameraHomePos(Pos(-1.64766, 4.48823, 1.71381),  Pos(-158.908, -10.5863, 0));
+
+    global.odeConfig.setParam("controlinterval",4);
+
+    // use Playground as boundary:
+    playground = new Playground(odeHandle, osgHandle,
+				osg::Vec3(10, .2, 1));
+    playground->setPosition(osg::Vec3(0,0,0.1));
+    global.obstacles.push_back(playground);
+
+    // add passive spheres as obstacles
+    for (int i=0; i< 1/*2*/; i+=1){
+      PassiveSphere* s1 = new PassiveSphere(odeHandle, osgHandle.changeColor(Color(1,1,0)), 0.3);
+      // s1->setPosition(osg::Vec3(-4.5+i*4.5,0,0));
+      s1->setPosition(osg::Vec3(0,0,1+i*5));
+      s1->setTexture("Images/dusty.rgb");
+      global.obstacles.push_back(s1);
+    }
+
+    for(int i=0; i<numSeeingRobots; i++){
+      // the twowheeled robot is derived from Nimm2 and has a camera onboard
+      TwoWheeledConf twc = TwoWheeled::getDefaultConf();
+      twc.n2cfg.force=3;
+      twc.n2cfg.speed=8;
+
+
+      twc.camcfg.width  = 256;
+      twc.camcfg.height = 128;
+      twc.camcfg.fov    = 120;      
+      // get rid of the image processing
+      twc.camcfg.removeProcessors();      
+
+      OpticalFlowConf ofc = OpticalFlow::getDefaultConf();
+      // ofc.points = OpticalFlow::getDefaultPoints(3);
+      ofc.dims   = Sensor::X;
+      ofc.verbose   = 2;
+      //ofc.points  = getDefaultPoints(3);      
+      twc.camSensor     = new OpticalFlow(ofc);
+
+      OdeRobot* vehicle = new TwoWheeled(odeHandle, osgHandle, twc, 
+                                         "CamRobot_" + itos(i));
+      vehicle->setColor(Color(1,.7,0));
+      vehicle->place(osg::Matrix::rotate(M_PI, 0,0,1)
+                     *osg::Matrix::translate(3,-4+2*i,0.3));
+      
+      SeMoXConf cc = SeMoX::getDefaultConf();
+      cc.modelExt = true;
+      AbstractController *controller = new SeMoX(cc);
+      
+      // AbstractController *controller = new Braitenberg(Braitenberg::Aggressive, 2, 3);
+      AbstractWiring* wiring = new SelectiveOne2OneWiring(new ColorUniformNoise(0.1),
+                                                          new select_from_to(2,3));
+      OdeAgent* agent = new OdeAgent(plotoptions, 0); // no noise
+      agent->init(controller, vehicle, wiring);
+      global.configs.push_back(controller);
+      global.agents.push_back(agent);
+    }
+
+
+    for(int i=0; i<numBlindRobots; i++){
+      // this robot has no camera
+      OdeRobot* vehicle = new Nimm2(odeHandle, osgHandle, Nimm2::getDefaultConf(), 
+                                    "BlindRobot_" + itos(i));
+      vehicle->setColor(Color(1,1,0));
+      vehicle->place(Pos(-3,-4+2*i,0.3));
+      AbstractController *controller = new InvertMotorSpace(10);
+      One2OneWiring* wiring = new One2OneWiring(new ColorUniformNoise(0.1));
+      OdeAgent* agent = new OdeAgent();
+      agent->init(controller, vehicle, wiring);
+      global.agents.push_back(agent);
+    }
+
+    showParams(global.configs);
+  }
+
+  virtual void addCallback(GlobalData& globalData, bool draw, bool pause, bool control) {
+  }
+  
+  virtual void end(GlobalData& globalData){
+  }
+};
+
+
+int main (int argc, char **argv)
+{
+  ThisSim sim;
+  return sim.run(argc, argv) ? 0 : 1;
+
+}
+
