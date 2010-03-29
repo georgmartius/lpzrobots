918d8784b9529e18b16987e77ef501c0a5c25029
martius <martius>
2010-03-29 16:26:45 +0000

    new simulation for Ball playing

---
+++ ode_robots/simulations/vision/BallPlaying/main.cpp
@@ -0,0 +1,291 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *    frankguettler@gmx.de                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1  2010-03-29 16:26:45  martius
+ *   new simulation for Ball playing
+ *
+ *   Revision 1.3  2010/03/29 07:17:36  martius
+ *   nimm4 body inverted
+ *
+ *   Revision 1.2  2010/03/26 14:17:15  martius
+ *   fourwheeled in 2wheeled mode addded
+ *
+ *   Revision 1.1  2010/03/25 16:38:07  martius
+ *   vision experiments
+ *
+ *   Revision 1.2  2010/03/24 16:51:38  martius
+ *   QuickMP uses now the number of processors in the system
+ *   optical flow improved
+ *   video recording works with offscreen rendering
+ *   Make system: Optimization -O1 is switched on by default (add a debug version without optimization)
+ *
+ *   Revision 1.1  2010/03/23 18:42:26  martius
+ *   new simulation for vision
+ *
+ *
+ ***************************************************************************/
+#include <stdio.h>
+
+// include noisegenerator (used for adding noise to sensorvalues)
+#include <selforg/noisegenerator.h>
+#include <selforg/invertmotorspace.h>
+#include <selforg/crossmotorcoupling.h>
+#include <selforg/sinecontroller.h>
+#include <selforg/one2onewiring.h>
+#include <selforg/selectiveone2onewiring.h>
+
+#include "semoxhebmod.h"
+
+// include simulation environment stuff
+#include <ode_robots/simulation.h>
+
+// include agent (class for holding a robot, a controller and a wiring)
+#include <ode_robots/odeagent.h>
+
+
+// used arena
+#include <ode_robots/playground.h>
+#include <ode_robots/octaplayground.h> // arena
+
+// used passive spheres
+#include <ode_robots/passivesphere.h>
+#include <ode_robots/passivebox.h>
+
+#include <ode_robots/camera.h>
+#include <ode_robots/imageprocessors.h>
+#include <ode_robots/camerasensors.h>
+
+#include <ode_robots/twowheeled.h>
+#include <ode_robots/addsensors2robotadapter.h>
+#include <ode_robots/fourwheeled.h>
+
+
+#include <osg/Light>
+#include <osg/LightSource>
+
+// fetch all the stuff of lpzrobots into scope
+using namespace lpzrobots;
+using namespace std;
+
+class ThisSim : public Simulation {
+public:
+
+  AbstractController* controller;
+  // starting function (executed once at the beginning of the simulation loop)
+  void start(const OdeHandle& odeHandle, const OsgHandle& osgHandle, GlobalData& global)
+  {
+    int numSeeing4wheeled = 1;
+    int numBlindRobots    = 0;
+
+    int numBalls          = 5;
+
+    bool useSquareGround  = false;
+    bool useCorridor      = true;
+    double radius         = 10;
+
+    if(useCorridor)
+      setCameraHomePos(Pos(-7.38466, 10.1764, 3.17434),  Pos(-96.9417, -12.6582, 0));
+    else
+      setCameraHomePos(Pos(-1.64766, 4.48823, 1.71381),  Pos(-158.908, -10.5863, 0));
+
+    global.odeConfig.setParam("controlinterval",4);
+
+    addParameterDef("attraction", &attraction, 0.000);
+    addParameterDef("friction", &friction, 1);
+
+    addParameterDef("sizefactor", &sizefactor, 0);
+    addParameterDef("posfactor", &posfactor, 1);
+
+    global.configs.push_back(this);
+
+    if(useSquareGround){
+      // use Playground as boundary:
+      Playground* playground = new Playground(odeHandle, osgHandle,
+                                              osg::Vec3(10, .2, 1));
+      playground->setPosition(osg::Vec3(0,0,0.1));
+      playground->setGroundSubstance(DebugSubstance(0,0.00,40,0.0));
+      //      playground->setGroundSubstance(Substance(0.8,0,40,0.5));
+      global.obstacles.push_back(playground);
+    }else if(useCorridor){       // use circular Corridor 
+      // outer ground
+      OdeHandle wallHandle = odeHandle;
+      wallHandle.substance.toMetal(0.1);
+      OctaPlayground* outer = new OctaPlayground(wallHandle, osgHandle.changeAlpha(0.2), 
+                                                 osg::Vec3(radius+2, 0.2, 1), 12);
+      outer->setTexture("");
+      outer->setPosition(osg::Vec3(0,0,0.1));
+      outer->setGroundSubstance(Substance(0.4,0.005,40,0.5));
+      global.obstacles.push_back(outer);
+      // inner walls (without ground
+      OctaPlayground* inner = new OctaPlayground(wallHandle, osgHandle.changeColor(0.1,0.4,0.1), 
+                                                 osg::Vec3(radius-2, 0.2, 1), 12, false);
+      inner->setTexture("");
+      inner->setPose(osg::Matrix::rotate(M_PI/12,0,0,1) * osg::Matrix::translate(0,0,0.1));
+      global.obstacles.push_back(inner);
+    }
+
+    // add passive spheres as obstacles
+    for (int i=0; i< numBalls; i++){
+      PassiveSphere* s1 = new PassiveSphere(odeHandle, osgHandle.changeColor(Color(1,1,0)), 0.3);
+      // s1->setPosition(osg::Vec3(-4.5+i*4.5,0,0));      
+      if(useCorridor) s1->setPosition(osg::Vec3(sin(i/3.0)*radius,cos(i/3.0)*radius,1));
+      else  s1->setPosition(osg::Vec3(i%5,-2+i/5,1));
+      s1->setTexture("Images/dusty.rgb");
+      global.obstacles.push_back(s1);
+    }
+
+
+    /// FOURWHEELED
+    for(int i=0; i<numSeeing4wheeled; i++){
+      CameraConf camcfg = Camera::getDefaultConf();
+      camcfg.width  = 256;
+      camcfg.height = 128;
+      camcfg.fov    = 120;      
+      camcfg.camSize = 0.08;
+      camcfg.processors.push_back(new HSVImgProc(false,1));
+      // filter only Yellow color
+      camcfg.processors.push_back(new ColorFilterImgProc(true, .5, 
+                                  HSVImgProc::Red+20, HSVImgProc::Green-20,100));
+      Camera* cam = new Camera(camcfg);
+      CameraSensor* camSensor = new MotionCameraSensor(2, MotionCameraSensor::Position | 
+                                                       MotionCameraSensor::Size | 
+                                                       MotionCameraSensor::SizeChange);
+      camSensor->setInitData(cam, odeHandle, osgHandle, osg::Matrix::rotate(-M_PI/2,0,0,1)
+			     * osg::Matrix::translate(0.2,0, 0.40) );
+      std::list<Sensor*> sensors;
+      sensors.push_back(camSensor);      
+      FourWheeledConf fwc = FourWheeled::getDefaultConf();
+      fwc.twoWheelMode = true;
+      fwc.useBumper    = false;
+      OdeRobot* robot = new FourWheeled(odeHandle, osgHandle, 
+                                        fwc, 
+                                        "4WCamRobot_" + itos(i));
+      OdeRobot* vehicle = new AddSensors2RobotAdapter(odeHandle, osgHandle, robot, sensors);
+      vehicle->setColor(Color(1,.7,0));
+      if(useCorridor) 
+        vehicle->place(osg::Vec3(sin(i/2.0+.5)*radius,cos(i/2.0+.5)*radius,0.3));
+      else
+        vehicle->place(osg::Matrix::rotate(M_PI, 0,0,1) 
+                       * osg::Matrix::translate(3,-4+2*i,0.3));
+
+      
+      SeMoXHebModConf cc = SeMoXHebMod::getDefaultConf();
+      cc.modelExt = true;
+      SeMoXHebMod *semox = new SeMoXHebMod(cc);
+      controller = semox;
+//       CrossMotorCoupling* controller = new CrossMotorCoupling(semox, semox, 0.0);
+//       std::list<int> perm;
+//       perm += 1;
+//       perm += 0;
+//       controller->setCMC(CrossMotorCoupling::getPermutationCMC(perm));        
+//       controller->setParam("gamma_teach",0.005);
+      controller->setParam("gamma_teach",0.005);
+      //  controller->setParam("rootE",3);
+      
+      AbstractWiring* wiring = new One2OneWiring(new WhiteUniformNoise());
+      OdeAgent* agent = new OdeAgent( i==0 ? plotoptions : std::list<PlotOption>(),0.1);
+      agent->init(controller, vehicle, wiring);
+      global.configs.push_back(controller);
+      global.agents.push_back(agent);
+    }
+
+
+    for(int i=0; i<numBlindRobots; i++){
+      // this robot has no camera
+      OdeRobot* vehicle = new Nimm2(odeHandle, osgHandle, Nimm2::getDefaultConf(), 
+                                    "BlindRobot_" + itos(i));
+      vehicle->setColor(Color(1,1,0));
+      vehicle->place(Pos(-3,-4+2*i,0.3));
+      AbstractController *controller = new InvertMotorSpace(10);
+      One2OneWiring* wiring = new One2OneWiring(new ColorUniformNoise(0.1));
+      OdeAgent* agent = new OdeAgent();
+      agent->init(controller, vehicle, wiring);
+      global.agents.push_back(agent);
+    }
+
+    showParams(global.configs);
+  }
+
+  virtual void addCallback(GlobalData& globalData, bool draw, bool pause, bool control) {
+    if(control){
+      SeMoXHebMod* semox = dynamic_cast<SeMoXHebMod*>(controller);
+      if(semox){
+        matrix::Matrix desired = semox->getLastSensorValues();
+        // size: \dot size = -(size - 1) // set point is 1
+        desired.val(5,0) += - (desired.val(4,0)-1)* sizefactor;
+        // position: \dot pos = -(pos) // set point is 0
+        desired.val(2,0) += - (desired.val(3,0)) * posfactor;
+        semox->setSensorTeaching(desired);
+      }
+    }
+    // ball friction and move all balls to robot
+    Pos rpos = (*globalData.agents.begin())->getRobot()->getPosition();
+    FOREACH(ObstacleList, globalData.obstacles, o){
+      PassiveSphere* s = dynamic_cast<PassiveSphere*>(*o);
+      if(s){
+        Pos spos = s->getMainPrimitive()->getPosition();
+        s->getMainPrimitive()->applyForce((rpos-spos)*attraction);
+        Pos svel = s->getMainPrimitive()->getVel();
+        s->getMainPrimitive()->applyForce(-svel*friction);        
+
+      }
+    }
+  }
+  
+  virtual void end(GlobalData& globalData){
+  }
+
+  osg::LightSource* makeLights(osg::StateSet* stateset)
+  {
+    // create a directional light (infinite distance place at 45 degrees)
+    osg::Light* myLight = new osg::Light;
+    myLight->setLightNum(1);
+    myLight->setPosition(osg::Vec4(1.0,1.0,1.0,0.0f));
+    myLight->setDirection(osg::Vec3(-1.0, -1.0, -1.0));
+    myLight->setAmbient(osg::Vec4(.9f,.9f,.9f,.9f));
+    myLight->setDiffuse(osg::Vec4(.7f,.7f,.7f,.7f));
+    myLight->setConstantAttenuation(1.0f);
+
+    osg::LightSource* lightS = new osg::LightSource;
+    lightS->setLight(myLight);
+    lightS->setLocalStateSetModes(osg::StateAttribute::ON);
+
+    lightS->setStateSetModes(*stateset,osg::StateAttribute::ON);
+
+    return lightS;
+  }
+
+  paramval attraction;
+  paramval friction;
+  paramval sizefactor;
+  paramval posfactor;
+};
+
+
+int main (int argc, char **argv)
+{
+  ThisSim sim;
+  return sim.run(argc, argv) ? 0 : 1;
+
+}
+
---
+++ ode_robots/simulations/vision/BallPlaying/semoxhebmod.cpp
@@ -0,0 +1,436 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *                                                                         *
+ *   ANY COMMERCIAL USE FORBIDDEN!                                         *
+ *   LICENSE:                                                              *
+ *   This work is licensed under the Creative Commons                      *
+ *   Attribution-NonCommercial-ShareAlike 2.5 License. To view a copy of   *
+ *   this license, visit http://creativecommons.org/licenses/by-nc-sa/2.5/ *
+ *   or send a letter to Creative Commons, 543 Howard Street, 5th Floor,   *
+ *   San Francisco, California, 94105, USA.                                *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1  2010-03-29 16:26:45  martius
+ *   new simulation for Ball playing
+ *
+ *   Revision 1.4  2010/01/26 09:42:57  martius
+ *   restore bug fixed
+ *   ThisSim is a friend class (this is a hack)
+ *
+ *   Revision 1.3  2009/10/14 10:00:39  martius
+ *   added description of inspectables
+ *
+ *   Revision 1.2  2009/08/07 14:44:35  martius
+ *   changed inspectables order and time of last motor/sensor values
+ *
+ *   Revision 1.1  2009/08/05 23:08:21  martius
+ *   new reduced motor space controller implementing extensions
+ *    described in georgs dissertation.
+ *
+ *
+ ***************************************************************************/
+
+#include "semoxhebmod.h"
+#include <selforg/regularisation.h>
+using namespace matrix;
+using namespace std;
+
+SeMoXHebMod::SeMoXHebMod( const SeMoXHebModConf& conf) 
+  : HomeokinBase(conf.buffersize, "SeMoXHebMod", "$Id$"), 
+    conf(conf) {
+  
+
+  addParameterDef("epsM",&epsM,0.01);
+  addParameterDef("dampModel",&dampModel,0);
+  addParameterDef("dampController",&dampController,0);
+  addParameterDef("modelNoise",&modelNoise,0);  
+  addParameterDef("gamma_cont",&gamma_cont,0);
+  addParameterDef("gamma_teach",&gamma_teach,0);
+  addParameterDef("discountS",&discountS,0.001);
+  _modelExt_copy = conf.modelExt;
+  addParameter("extendedModel",&_modelExt_copy);
+
+  addInspectableMatrix("A", &A, false, "model matrix");
+  addInspectableMatrix("S", &S, conf.someInternalParams, "extended Model matrix");
+  addInspectableMatrix("M", &M, false, "hebbian model");
+  addInspectableMatrix("C", &C, conf.someInternalParams, "controller matrix");
+  addInspectableMatrix("R", &R, conf.someInternalParams, "linear Response matrix");
+  addInspectableMatrix("yteach", &y_teaching, false, "motor teaching signal");
+  
+  addInspectableMatrix("H", &H, false, "controller bias");
+  addInspectableMatrix("B", &B, false, "model bias");
+  if(!conf.someInternalParams){
+    addInspectableMatrix("v", &v, false, "postdiction error");  
+  }
+  addInspectableValue("xsi_norm", &xsi_norm, "1-norm of prediction error");
+  addInspectableValue("xsi_norm_avg", &xsi_norm_avg, "averaged 1-norm of prediction error");
+
+  managementInterval=10;
+  intern_useTeaching=false;
+  BNoiseGen = 0;
+};
+
+
+SeMoXHebMod::~SeMoXHebMod(){
+  if(x_buffer && y_buffer){
+    delete[] x_buffer;
+    delete[] y_buffer;
+  }
+  if(BNoiseGen) delete BNoiseGen;
+}
+
+
+void SeMoXHebMod::init(int sensornumber, int motornumber,
+			      RandGen* randGen){
+
+  number_motors  = motornumber;
+  number_sensors = sensornumber - conf.numContext;  
+  
+  A.set(number_sensors, number_motors);
+  A = (A^0) * conf.aInit; // set A to identity matrix;
+  B.set(number_sensors, 1);
+  if (conf.modelExt) {
+    S.set(number_sensors, number_sensors  + conf.numContext );
+    S = (S^0) * conf.sInit;   
+  }
+
+  M.set(number_sensors, number_motors);  
+
+  if(conf.initialC.getM() != number_motors || conf.initialC.getN() != number_sensors ){
+    if(!conf.initialC.isNulltimesNull()) 
+      cerr << "dimension of initialC are not correct, default is used! \n";
+    C.set(number_motors,  number_sensors);    
+    // initialise the C matrix with identity + noise scaled to cInit value
+    C = ((C^0) + C.mapP(randGen, random_minusone_to_one) * conf.cNonDiag) * conf.cInit;   
+  }else{
+    C=conf.initialC; // use given matrix
+  }
+
+  H.set(number_motors,  1);
+
+  R.set(number_motors, number_motors);
+  SmallID.set(number_motors, number_motors);
+  SmallID.toId();
+  SmallID *= 0.001;
+
+  xsi.set(number_sensors,1);
+  xsi_norm=0;
+  xsi_norm_avg=0.2;
+  pain=0;
+
+  BNoiseGen = new WhiteUniformNoise();
+  BNoiseGen->init(number_sensors, randGen);
+
+  v.set(number_sensors,1);
+
+  x_buffer = new Matrix[buffersize];
+  x_c_buffer = new Matrix[buffersize];
+  y_buffer = new Matrix[buffersize];
+  for (unsigned int k = 0; k < buffersize; k++) {
+    x_buffer[k].set(number_sensors,1);
+    x_c_buffer[k].set(number_sensors + conf.numContext,1);
+    y_buffer[k].set(number_motors,1);
+  }
+  y_teaching.set(number_motors, 1); 
+
+  t_rand = rand()%managementInterval;
+  initialised = true;
+}
+
+
+/// performs one step (includes learning). Calculates motor commands from sensor inputs.
+void SeMoXHebMod::step(const sensor* x_, int number_sensors, 
+			    motor* y_, int number_motors){
+  fillBuffersAndControl(x_, number_sensors, y_, number_motors);
+  if(t>buffersize){
+    int delay = max(int(s4delay)-1,0);   
+    calcXsi(delay);            // calculate the error (use delayed y values)        
+    // learn controller with effective input/output
+    learnController();
+    
+    // learn Model with actual sensors and with effective motors;
+    learnModel(delay);
+  }
+  // update step counter
+  t++;
+};
+
+/// performs one step without learning. Calulates motor commands from sensor inputs.
+void SeMoXHebMod::stepNoLearning(const sensor* x, int number_sensors, 
+				            motor*  y, int number_motors ){
+  fillBuffersAndControl(x, number_sensors, y, number_motors);
+  // update step counter
+  t++;
+};
+
+
+void SeMoXHebMod::fillBuffersAndControl(const sensor* x_, int number_sensors, 
+					      motor* y_, int number_motors){
+  assert((unsigned)number_sensors == (this->number_sensors + (unsigned)conf.numContext) 
+	 && (unsigned)number_motors == this->number_motors);
+  
+  Matrix x(this->number_sensors,1,x_);
+  Matrix x_c(this->number_sensors + conf.numContext,1,x_);
+  
+  // put new input vector in ring buffer x_buffer
+  putInBuffer(x_buffer, x);
+  putInBuffer(x_c_buffer, x_c);
+  
+  // averaging over the last s4avg values of x_buffer
+  const Matrix& x_smooth = calculateSmoothValues(x_buffer, t < s4avg ? 1 : int(max(1.0,s4avg)));
+  
+  // calculate controller values based on smoothed input values
+  const Matrix& y = calculateControllerValues(x_smooth);
+
+  // from time to time call management function. For example damping is done here.
+  if((t+t_rand)%managementInterval==0) management();
+
+  // put new output vector in ring buffer y_buffer
+  putInBuffer(y_buffer, y);
+
+  // convert y to motor* 
+  y.convertToBuffer(y_, number_motors);
+}
+
+/// calculates xsi for the current time step using the delayed y values
+//  @param delay 0 for no delay and n>0 for n timesteps delay in the time loop 
+void SeMoXHebMod::calcXsi(int delay){ 
+  const Matrix& x     = x_buffer[t% buffersize];
+  const Matrix& y     = y_buffer[(t - 1 - delay) % buffersize];
+  xsi = x -  model(x_buffer, 1 , y);        
+  xsi_norm = matrixNorm1(xsi);
+  xsi_norm_avg = xsi_norm_avg*0.999 + xsi_norm*0.001; // calc longterm average
+
+  pain= xsi_norm > 5*xsi_norm_avg; // pain if large raise in error (arbitrary choice)
+}
+
+
+/// calculates the predicted sensor values
+Matrix SeMoXHebMod::model(const Matrix* x_buffer, int delay, const matrix::Matrix& y){
+  if(conf.modelExt){
+    const Matrix& x_c_tm1 = x_c_buffer[(t - 1) % buffersize];
+    return A * y + S * x_c_tm1 + B;        
+  } else {
+    return A * y + B; 
+  }
+}
+
+/// learn controller paramters C and H
+void SeMoXHebMod::learnController(){ 
+
+
+  // prepare update matrices
+  Matrix C_update(C.getM(), C.getN());
+  Matrix H_update(H.getM(), H.getN());
+  Matrix C_updateTeaching;
+  Matrix H_updateTeaching;
+  bool teaching = intern_useTeaching || (gamma_cont!=0);
+  if(teaching){
+    C_updateTeaching.set(C.getM(), C.getN());
+    H_updateTeaching.set(H.getM(), H.getN());
+  }
+
+  
+  // eta = A^-1 xsi (first shift in motor-space at current time) 
+  //  we use pseudoinverse eta = U^-1 A^T xsi, with U=A^T A + \lambda I
+  //  it is additionally clipped to -1 to 1 via g (arbitrary choice)
+  const Matrix& eta = (A.pseudoInverse(0.001) * xsi).map(g); 
+  
+  const Matrix& x          = x_buffer[(t-1)%buffersize];
+  const Matrix& z          = C * x + H;
+  const Matrix g_prime     = Matrix::map2(g_s, z, eta);
+  const Matrix g_prime_inv = g_prime.map(one_over);
+  const Matrix g_2p_div_p  = Matrix::map2(g_ss_div_s, z, eta);
+  
+  const Matrix zeta = eta.multrowwise(g_prime_inv); // G'(Z)^-1 * (eta+v)
+  R                 = C * A;
+  const Matrix chi  = ((R.multMT()+SmallID)^-1) * zeta;
+  v                 = (R^T) * chi;
+  // squash v to -3,3 (arbitrary choice)
+  double size = 3;
+  v = v.mapP(&size,squash);
+  const Matrix rho  = g_2p_div_p.multrowwise(chi.multrowwise(zeta)) * -1;
+  
+  C_update += ( chi*(v^T)*(A^T) - rho*(x^T) ) * epsC;
+  H_update += rho * -epsC;
+
+  // scale of the additional terms (natural gradient with metric LL^T)
+  if(teaching){
+    // scale of the additional terms
+    const Matrix& LLT_I = ((R & g_prime).multMT()+SmallID)^-1;
+
+    if(gamma_cont!=0) {  // learning to keep motorcommands smooth
+      // the teaching signal is the previous motor command
+      const Matrix& y = y_buffer[(t)% buffersize];
+      const Matrix& y_tm1 = y_buffer[(t-1)% buffersize];
+      const Matrix& delta = (y_tm1 - y) & (g_prime);
+      C_updateTeaching += ( LLT_I * delta *(x^T) ) * (gamma_cont * epsC);
+      H_updateTeaching += LLT_I * delta * (gamma_cont * epsC);
+    }
+    if(intern_useTeaching && gamma_teach!=0){    
+      const Matrix& y = y_buffer[(t-1)% buffersize];
+      const Matrix& xsi = y_teaching - y;
+      const Matrix& delta = xsi.multrowwise(g_prime);
+      C_updateTeaching += (LLT_I * delta*(x^T) ) * (gamma_teach * epsC);
+      H_updateTeaching += (LLT_I * delta) * (gamma_teach * epsC);
+      intern_useTeaching=false; // after we applied teaching signal it is switched off until new signal is given
+    }
+  }
+   
+  double error_factor = calcErrorFactor(v, (logaE & 1) !=0, (rootE & 1) !=0); 
+  C_update *= error_factor;
+  H_update *= error_factor;
+  if(teaching){
+    C_update+=C_updateTeaching;
+    H_update+=H_updateTeaching;
+  }
+
+  C += C_update.mapP(&squashSize, squash);
+  double H_squashSize = squashSize*10;
+  H += H_update.mapP(&H_squashSize, squash);
+};
+
+
+// normal delta rule (xsi is assumed to be already up to date via calcXsi())
+void SeMoXHebMod::learnModel(int delay){
+  const Matrix& y_tm1 = y_buffer[(t - 1 - delay) % buffersize];
+
+  if(!pain) {
+    double error_factor = calcErrorFactor(xsi, (logaE & 2) != 0, (rootE & 2) != 0);
+    Matrix A_update;  
+    Matrix S_update;
+    Matrix B_update;
+    
+    // new hierarchical learning
+    if(conf.modelExt){                 
+      const Matrix& x = x_buffer[t % buffersize];
+      const Matrix& x_c_tm1 = x_c_buffer[(t - 1) % buffersize];
+      // first learn A on error with discounted S
+      const Matrix& zeta = x -  (A*y_tm1 + B + (S*x_c_tm1)*(1-discountS));	
+      A_update=(( zeta*(y_tm1^T) ) * (epsA * error_factor)); 
+      S_update=(( xsi*(x_c_tm1^T) ) * (epsA * error_factor));// as usual with full error
+    }else{
+      A_update=(( xsi*(y_tm1^T) ) * (epsA * error_factor)); 
+    }
+
+    B_update=( xsi * (epsA * factorB * error_factor));
+    if(modelNoise>0){
+      B_update  = noiseMatrix(B.getM(),B.getN(), *BNoiseGen, -modelNoise, modelNoise); // noise for bias
+    }
+
+    // new hebbian world modell (for distal teaching)
+    { 
+      const Matrix& x = x_buffer[t % buffersize];
+      M += x * (y_tm1^T) * epsM;
+      M.toMapP(1.0, clip);
+    }
+      
+    A += A_update.mapP(&squashSize, squash);
+    B += B_update.mapP(&squashSize, squash);
+    if(conf.modelExt) S += S_update.mapP(&squashSize, squash);
+  }
+};
+
+/// calculate controller outputs 
+/// @param x_smooth smoothed sensors Matrix(number_channels,1) 
+Matrix SeMoXHebMod::calculateControllerValues(const Matrix& x_smooth){
+  return (C*x_smooth+H).map(g); 
+};
+
+
+void SeMoXHebMod::management(){
+  if(dampModel){    
+    A *= 1 - dampModel * managementInterval;
+    B *= 1 - dampModel * managementInterval;    
+    S *= 1 - dampModel * managementInterval;    
+  }
+  if(dampController){
+    Matrix oldC = C;
+    C *= 1 - dampController * managementInterval;
+    for(int i=0; i< min(C.getM(),C.getN()); i++){
+      C.val(i,i) = oldC.val(i,i);
+    }
+  }
+}
+
+
+bool SeMoXHebMod::store(FILE* f) const {  
+  // save matrix values
+  C.store(f);
+  H.store(f);
+  A.store(f);
+  B.store(f);
+  M.store(f);
+  if(conf.modelExt) S.store(f);
+  Configurable::print(f,0);
+  return true;
+}
+
+bool SeMoXHebMod::restore(FILE* f){
+  // save matrix values
+  C.restore(f);
+  H.restore(f);
+  A.restore(f);
+  B.restore(f);
+  M.restore(f);
+  if(conf.modelExt) S.restore(f);
+  Configurable::parse(f);
+  t=0; // set time to zero to ensure proper filling of buffers
+  return true;
+}
+
+list<Inspectable::ILayer> SeMoXHebMod::getStructuralLayers() const {
+  list<Inspectable::ILayer> l;
+  l+=ILayer("x","", number_sensors, 0, "Sensors");
+  l+=ILayer("y","H", number_motors, 1, "Motors");
+  l+=ILayer("xP","B", number_sensors, 2, "Prediction");
+  return l;
+}
+
+list<Inspectable::IConnection> SeMoXHebMod::getStructuralConnections() const {
+  list<Inspectable::IConnection> l;
+  l+=IConnection("C", "x", "y");
+  l+=IConnection("A", "y", "xP");
+  if(conf.modelExt) l+=IConnection("S", "x", "xP");
+  return l;
+}
+
+
+///////// TEACHABLE ////////////////
+
+void SeMoXHebMod::setMotorTeaching(const matrix::Matrix& teaching){
+  assert(teaching.getM() == number_motors && teaching.getN() == 1);  
+  // Note: through the clipping the otherwise effectless 
+  //  teaching with old motor value has now an effect, 
+  //  namely to drive out of the saturation region. 
+  y_teaching= teaching.mapP(0.95,clip);
+  intern_useTeaching=true;
+}
+
+void SeMoXHebMod::setSensorTeaching(const matrix::Matrix& teaching){
+  assert(teaching.getM() == number_sensors && teaching.getN() == 1);  
+//   // new version  
+//   const Matrix& error = teaching - getLastSensorValues();
+//   const Matrix& eta   = (M.pseudoInverse(0.001)*error).mapP(0.2,clip); // small errors only
+//   y_teaching = (getLastMotorValues() + eta).mapP(0.95, clip); 
+  const Matrix& error = teaching - getLastSensorValues();
+  const Matrix& eta   = (A.pseudoInverse(0.001)*error);
+  y_teaching = (getLastMotorValues() + eta).mapP(0.85, clip); 
+  //  y_teaching = (A.pseudoInverse(0.001) * (teaching-B)).mapP(0.95, clip); 
+  intern_useTeaching=true;  
+}
+
+matrix::Matrix SeMoXHebMod::getLastMotorValues(){
+  return y_buffer[(t-1+buffersize)%buffersize];
+}
+
+matrix::Matrix SeMoXHebMod::getLastSensorValues(){
+  return x_buffer[(t-1+buffersize)%buffersize];
+}
