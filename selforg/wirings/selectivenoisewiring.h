5bd9b588c0d67a2e988d00c2b871d7d7a88097c1
martius <martius>
2010-04-01 14:54:10 +0000

    wiring to add noise only to some sensor values

---
+++ selforg/wirings/selectivenoisewiring.h
@@ -0,0 +1,73 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Robot Group Leipzig                             *
+ *    martius@informatik.uni-leipzig.de                                    *
+ *    fhesse@informatik.uni-leipzig.de                                     *
+ *    der@informatik.uni-leipzig.de                                        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ *   $Log$
+ *   Revision 1.1  2010-04-01 14:54:10  martius
+ *   wiring to add noise only to some sensor values
+ *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef __SELECTIVENOISEWIRING_H
+#define __SELECTIVENOISEWIRING_H
+
+#include <selforg/one2onewiring.h>
+#include <vector>
+
+/** 
+ * Implements a one to one wiring 
+ *  and allows to select the noise strength per sensor channel
+ */
+class SelectiveNoiseWiring : public One2OneWiring {
+public:
+  /** constructor
+      @param noise NoiseGenerator that is used for adding noise to motor values  
+      @param noiseStrengths for each channel the noise strength 
+       (no noise if channel is out of array bounds)
+      @param plotMode see AbstractWiring 
+  */
+  SelectiveNoiseWiring(NoiseGenerator* noise, std::vector<double> noiseStrengths, 
+                       int plotMode=Controller)
+    : One2OneWiring(noise, plotMode), noiseStrengths(noiseStrengths) {    
+  }
+  virtual ~SelectiveNoiseWiring(){}
+  
+  virtual bool wireSensorsIntern(const sensor* rsensors, int rsensornumber, 
+                                 sensor* csensors, int csensornumber, double noise){
+    assert(rsensornumber == this->rsensornumber);
+    assert(csensornumber == this->csensornumber);
+    // the noisevals are set in abstractwiring
+    for(int i=0; i< rsensornumber; i++){
+      csensors[i] = rsensors[i] + getNoiseStrength(i)*noisevals[i];
+    }
+    return true;  
+  }
+  
+protected:
+  double getNoiseStrength(unsigned int i){
+    if(i>=noiseStrengths.size()) return 0;
+    else return noiseStrengths[i];
+  }
+
+  std::vector<double> noiseStrengths;
+
+};
+
+#endif
