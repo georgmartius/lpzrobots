01fa5f18d6a671d0b62a7406886764adc9414094
robot14 <robot14>
2010-06-30 11:33:08 +0000

    fixed parser

--- matrixviz/src/InputFilter/MatrixPipeFilter.cpp
+++ matrixviz/src/InputFilter/MatrixPipeFilter.cpp
@@ -27,7 +27,10 @@
  *                                                                         *
  *                                                                         *
  *  $Log$
- *  Revision 1.6  2010-05-11 16:53:03  robot14
+ *  Revision 1.7  2010-06-30 11:33:08  robot14
+ *  fixed parser
+ *
+ *  Revision 1.6  2010/05/11 16:53:03  robot14
  *  *** empty log message ***
  *
  *  Revision 1.5  2010/03/30 13:18:06  robot14
@@ -72,62 +75,63 @@ MatrixPipeFilter::~MatrixPipeFilter() {
 
 AbstractPlotChannel* MatrixPipeFilter::createChannel(std::string name)
 {
-    //if (name.find("A[0,1]")==0) return (new MotorSpeedPlotChannel("motorCspeedX"));
-	//test
 	if (debug) cout << name << endl;
+  bool hasBrace = false, hasComma = false;
+  int bracePos, commaPos;
+  for(unsigned int i = 0; i < name.size(); i++){
+    if(name.at(i) == '['){
+      hasBrace = true;
+      bracePos = i;
+    }else
+    if(name.at(i) == ','){
+      hasComma = true;
+      commaPos = i;
+    }
+  }
 	/*
 	 * Looking for new vector
 	 */
-	if ((name.at(3) == ']') || (name.at(4) == ']')) {
-    // empty vector or new vector
-    if (vectors.size() == 0 || name.at(0) != vectors.back()->getChannelName().at(0)) {
-      VectorPlotChannel* vePloChannel = new VectorPlotChannel(name.substr(0, 1));
+	if (hasBrace && !hasComma) {
+    // empty vector or new vector (different name)
+    if (vectors.size() == 0 || name.substr(0, bracePos) != vectors.back()->getChannelName().substr(0, bracePos)) {
+      VectorPlotChannel* vePloChannel = new VectorPlotChannel(name.substr(0, bracePos));
       if (debug) cout << "push_back new vpc" << endl;
       vectors.push_back(vePloChannel);
     }
-    //A[x,0] : new row channel (active + add) new elementplotchannel to active rowchannel
-    //    if (name.substr(4,1) == "0"){
-    //      VectorPlotChannel* rowChannel = new VectorPlotChannel("0");
-    //      vectors.back()->addRow(rowChannel);
-    //    }
     if (debug) cout << "new VEPC" << endl;
     VectorElementPlotChannel* elementChannel = new VectorElementPlotChannel(name);
     if (debug) cout << "vectors back" << endl;
     vectors.back()->addElement(elementChannel);
 
-    // eigentlichen Channel hinzufügen
-    // überlegen: evtl. Dimension der Matrix in der Hierarchie berücksichtigen (mxn)
-    // addRow(GroupChannel*) verwenden usw.
     if (debug) cout << "return VEPC" << endl;
     return elementChannel;
-  } else
-	/*
-   * Looking for new matrix _A_[x,y] //(and not A[x_]_)
-   */
-  if (name.at(0) == toupper(name.at(0)) /*&& !((name.at(3) == ']') || (name.at(4) == ']'))*/ /*ERSTER BUCHSTABE IN name großgeschrieben*/) {
-    // empty vector or new matrix
-    if (debug) cout << "test new matrix" << endl;
-    if (matrices.size() == 0 || name.at(0) != matrices.back()->getChannelName().at(0)) {
-      MatrixPlotChannel* maPloChannel = new MatrixPlotChannel(name.substr(0, 1));
-      matrices.push_back(maPloChannel);
-    }
-    //A[x,0] : new row channel (active + add) new elementplotchannel to active rowchannel
-    if (name.substr(4, 1) == "0") {
-      MatrixPlotChannel* rowChannel = new MatrixPlotChannel("0");
-      matrices.back()->addRow(rowChannel);
-    }
-    MatrixElementPlotChannel* elementChannel = new MatrixElementPlotChannel(name);
+  } else {
+    /*
+     * Looking for new matrix _A_[x,y]
+     */
+    if (hasBrace && hasComma && name.at(0) == toupper(name.at(0))) {
+      // empty vector or new matrix
+      if (debug)
+        cout << "test new matrix" << endl;
+      if (matrices.size() == 0 || name.substr(0, bracePos) != matrices.back()->getChannelName().substr(0, bracePos)) {
+        MatrixPlotChannel* maPloChannel = new MatrixPlotChannel(name.substr(0, bracePos));
+        matrices.push_back(maPloChannel);
+      }
+      //A[x,0] : new row channel (active + add) new elementplotchannel to active rowchannel
+      if (name.substr(commaPos + 1, 1) == "0") {
+        if (debug ) cout << "new MPC" << endl;
+        MatrixPlotChannel* rowChannel = new MatrixPlotChannel("0");
+        matrices.back()->addRow(rowChannel);
+      }
+      MatrixElementPlotChannel* elementChannel = new MatrixElementPlotChannel(name);
 
-    matrices.back()->getLastRow()->addPlotChannel(elementChannel);
+      matrices.back()->getLastRow()->addPlotChannel(elementChannel);
 
-    // eigentlichen Channel hinzufügen
-    // überlegen: evtl. Dimension der Matrix in der Hierarchie berücksichtigen (mxn)
-    // addRow(GroupChannel*) verwenden usw.
-
-    return elementChannel;
-  } else
-    // default
-    return new DefaultPlotChannel(name);
+      return elementChannel;
+    } else
+      // default
+      return new DefaultPlotChannel(name);
+  }
 }
 
 std::vector<MatrixPlotChannel*> MatrixPipeFilter::getMatrixChannels(){
