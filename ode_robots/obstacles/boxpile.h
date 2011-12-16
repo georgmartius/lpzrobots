1b11645b4c78ff0aeaa8fb3f57b20919d1c5afa6
Georg Martius <georg.martius@web.de>
2011-12-16 13:32:20 +0100

    boxpile added

---
+++ ode_robots/obstacles/boxpile.h
@@ -0,0 +1,109 @@
+/***************************************************************************
+ *   Copyright (C) 2005-2011 LpzRobots development team                    *
+ *    Georg Martius  <georg dot martius at web dot de>                     *
+ *    Frank Guettler <guettler at informatik dot uni-leipzig dot de        *
+ *    Frank Hesse    <frank at nld dot ds dot mpg dot de>                  *
+ *    Ralf Der       <ralfder at mis dot mpg dot de>                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef __BOXPILE_H
+#define __BOXPILE_H
+
+#include <stdio.h>
+#include <cmath>
+
+#include "primitive.h"
+#include "joint.h"
+#include "abstractobstacle.h"
+
+namespace lpzrobots {
+
+/**
+ *  Boxpile
+ */
+class Boxpile : public AbstractObstacle {
+  Pos dimension;
+  int num;
+  int seed;
+  Pos boxsizemean;
+  Pos boxsizevar;
+  RandGen randGen;
+
+public:
+  /**
+   * Constructor
+   * a random boxpile is fixed to the world
+   * @param dimension size of pile
+   * @param num number of boxes
+   * @param seed seed for random generator (it is fixed to generate deterministic piles)
+   * @param boxsizemean mean size of boxes
+   * @param boxsizevar  variance of boxes sizes
+   */
+  Boxpile(const OdeHandle& odeHandle, const OsgHandle& osgHandle, 
+          const osg::Vec3& dimension = osg::Vec3(5.0, 5.0, 0.5), 
+          int num = 10, int seed = 0,
+          const osg::Vec3& boxsizemean = osg::Vec3(1.0, 1.0, 0.1), 
+          const osg::Vec3& boxsizevar = osg::Vec3(0.5, 0.5, 0.05) ) 
+    : AbstractObstacle::AbstractObstacle(odeHandle, osgHandle), dimension(dimension),
+      num(num), boxsizemean(boxsizemean), boxsizevar(boxsizevar) 
+  {
+    setTexture("Images/wood_sw.jpg");
+    randGen.init(seed);
+
+    obstacle_exists=false;    
+  };
+
+  
+  virtual void setPose(const osg::Matrix& pose){
+    this->pose = pose;
+    if (!obstacle_exists) {
+      create();
+    }
+  };
+
+  virtual Primitive* getMainPrimitive() const { 
+    if(!obst.empty()) return obst[0]; 
+    else return 0;
+  }
+  
+protected:
+  virtual void create(){
+    OdeHandle oh(odeHandle);
+    oh.createNewSimpleSpace(odeHandle.space);
+    for(int i=0; i< num; i++){
+      Box* b;
+      Pos rand(randGen.rand()-0.5,randGen.rand()-0.5,randGen.rand()-0.5);
+      Pos s = boxsizemean + ((rand*2) & boxsizevar); // & component wise mult
+      b = new Box(s);
+      b->setTextures(getTextures(i));
+      b->init(oh, 0, osgHandle, Primitive::Geom | Primitive::Draw);
+
+      Pos pos = dim&Pos(randGen.rand()-0.5,randGen.rand()-0.5,randGen.rand())
+        + Pos(0,0,s.z()/2);
+      b->setPose(TRANSM(pos) * pose);
+      obst.push_back(b);
+    }    
+    obstacle_exists=true;
+  };
+
+};
+
+}
+
+#endif
+
