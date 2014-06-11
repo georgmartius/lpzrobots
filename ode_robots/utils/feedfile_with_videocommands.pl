16c50797228df956dd06855031de0e2fb08cf11c
Georg Martius <georg.martius@web.de>
2014-06-11 14:58:06 +0200

    script added to record videos with matrixviz from logfiles

---
+++ ode_robots/utils/feedfile_with_videocommands.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+use strict;
+
+
+if(defined $ARGV[0] && defined $ARGV[1]){
+    my $name  = shift;
+    my $delay = shift;
+    my $every = 1;
+    my $start = 0;
+    if($ARGV[0]){
+       $every = shift;
+    }
+    if($ARGV[0]){
+       $start = shift;
+    }
+    my $cnt=0;
+    my $framenr=0;
+    while (<>) {
+        my $cmt=0;
+        if(/^#V / ){ # skip original video tags
+            next;
+        }elsif(/^#/ ){
+          $cnt-=1;
+          $cmt=1;
+        }else{
+          select(undef,undef,undef,$delay/1000) if($delay);
+        }
+        $cnt+=1;
+        if($cmt || ($cnt % $every == 0 && $cnt >= $start)){
+            print $_;
+            $framenr=$framenr+1;
+            print "#V " . $framenr . " " . $name . "\n";
+        }
+    }
+
+}else{
+    print "Usage: feedfile_with_videocommands.pl videoname delay [every] [start] < logfile\n";
+    print "\t outputs the logfile with \"#V CNT videoname\" lines every given line\n";
+    print "\t for video recording with matrixviz etc. if no video was recorded in the simulation.\n";
+    print "\t otherwise use feedfile.pl directly\n";
+    print "\tdelay in milliseconds\n\tevery xth dataset to use for the video\n\tstart dataset number to start with\n";
+}
