3b17bd6befd46c6471dcd00db6676d874b9ea0ba
Georg Martius <georg.martius@web.de>
2014-06-12 10:20:37 +0200

    use h264 codec if available

--- ode_robots/utils/encodevideo.sh
+++ ode_robots/utils/encodevideo.sh
@@ -21,48 +21,15 @@
 #   Free Software Foundation, Inc.,                                       *
 #   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
 #**************************************************************************
-#                                                                         *
-#  DESCRIPTION                                                            *
-#                                                                         *
-#   $Log$
-#   Revision 1.18  2010-01-27 10:34:44  martius
-#   fixed framerate for hq flash
-#
-#   Revision 1.17  2010/01/27 10:23:05  martius
-#   increased framerate
-#
-#   Revision 1.16  2010/01/26 11:19:36  martius
-#   added flash videos and removed wmv and small versions
-#
-#   Revision 1.15  2010/01/12 15:18:24  martius
-#   added flash video
-#
-#   Revision 1.14  2010/01/04 15:30:47  der
-#   added high quality video im MS Mpeg4 format
-#
-#   Revision 1.13  2009/03/13 09:19:53  martius
-#   changed texture handling in osgprimitive
-#   new OsgBoxTex that supports custom texture repeats and so on
-#   Box uses osgBoxTex now. We also need osgSphereTex and so on.
-#   setTexture has to be called before init() of the primitive
-#
-#   Revision 1.12  2009/02/03 18:12:09  martius
-#   added wmv to encoding and provided new script to convert existing movies
-#
-#   Revision 1.11  2008/02/22 06:51:46  der
-#   added small xvid variant
-#
-#                                                                         *
-#**************************************************************************
 
-if test -z "$1"; then 
+if test -z "$1"; then
     echo -e "USAGE: $0: BaseName [Target]\n\tExample: $0 frame_00 SuperVideo";
     exit 1;
 fi
 
 NAME=$1;
 TARGET=$1;
-if test -n "$2"; then 
+if test -n "$2"; then
     TARGET=$2;
 fi
 
@@ -78,8 +45,21 @@ mencoder mf://$NAME*.jpg -mf fps=25:type=jpg -ovc lavc -lavcopts vcodec=mjpeg -o
 #echo -e "*********************** to wmv **************************";
 #mencoder mf://$NAME*.jpg -mf fps=25:type=jpg -ovc lavc -lavcopts vcodec=wmv2:vbitrate=600 -oac copy -o "${TARGET}.wmv.avi"
 
-echo -e "*********************** to mpeg4 xvid 4 **************************";
-transcode -i "$TARGET.mjpeg" -o "$TARGET.avi" -y xvid4,null -w 800
+#echo -e "*********************** to mpeg4 xvid 4 **************************";
+#transcode -i "$TARGET.mjpeg" -o "$TARGET.avi" -y xvid4,null -w 800
+if which x264; then
+    echo -e "******************** to high quality mp4 and flv (h264) ***************";
+    x264 --crf 15 -o "${TARGET}_hq.mp4" "$TARGET.mjpeg";
+    x264 --crf 15 -o "${TARGET}_hq.flv" "$TARGET.mjpeg";
+else
+    echo -e "******************** to high quality avi (msmpeg4) ***************";
+    ffmpeg -i "$TARGET.mjpeg" -vcodec msmpeg4 -sameq  "${TARGET}_hq.avi"
+    echo -e "******************** to flash video (high quality) ***************";
+    ffmpeg -i "$TARGET.mjpeg" -b 2500k  "${TARGET}_hq.flv"
+fi
+
+echo -e "******************** to flash video (web version)  ***************";
+ffmpeg -i "$TARGET.mjpeg" -b 800k  "${TARGET}.flv"
 
 #echo -e "******************** to mpeg4 xvid 4 small variant ***************";
 #transcode -i "$TARGET.mjpeg" -o "$TARGET"_small.avi -y xvid4,null -w 100 -r 2
@@ -87,13 +67,4 @@ transcode -i "$TARGET.mjpeg" -o "$TARGET.avi" -y xvid4,null -w 800
 #echo -e "******************** to wmv small variant ***************";
 #transcode -i "$TARGET.mjpeg" -o "$TARGET"_small.wmv.avi -y ffmpeg,null -F wmv2 -w 100 -r 2
 
-echo -e "******************** to high quality avi (msmpeg4) ***************";
-ffmpeg -i "$TARGET.mjpeg" -vcodec msmpeg4 -sameq  "${TARGET}_hq.avi"
-
-echo -e "******************** to flash video (high quality) ***************";
-ffmpeg -i "$TARGET.mjpeg" -b 2500k  "${TARGET}_hq.flv"
-
-echo -e "******************** to flash video (web version)  ***************";
-ffmpeg -i "$TARGET.mjpeg" -b 800k  "${TARGET}.flv"
-
-
+rm -f "$TARGET.mjpeg";
