04c66b9193aa11209b48e36633df5919a4371c41
Georg Martius <martius@mis.mpg.de>
2017-04-04 19:12:37 +0200

    removed Delaysensor from humanoid-main because it has its own file now

--- ode_robots/simulations/humanoid_DEP/main.cpp
+++ ode_robots/simulations/humanoid_DEP/main.cpp
@@ -83,6 +83,7 @@ Trainer 2 Robots Standing
 #include <ode_robots/joint.h>
 #include <ode_robots/axisorientationsensor.h>
 #include <ode_robots/speedsensor.h>
+#include <ode_robots/delaysensor.h>
 #include <ode_robots/relativepositionsensor.h>
 
 #include <ode_robots/operators.h>
@@ -148,74 +149,6 @@ protected:
   double* othersensors;
   double factor;
 };
-
-/// provides sensor that delays sensor values from other sensors
-class DelaySensor : public virtual Sensor, public Configurable {
-public:
-  DelaySensor(std::shared_ptr<Sensor> sensor)
-  : childSensor(sensor), time(0) {
-    addParameterDef("delay",&delay, 5, 0, 50, "delay in steps for sensor values");
-    Configurable* c = dynamic_cast<Configurable*>(childSensor.get());
-    if(c) {
-      addConfigurable(c);
-      setName("Delay-of-" + c->getName());
-    }else setName("DelaySensor");
-
-  }
-
-  virtual ~DelaySensor(){
-    for(int k=0; k<buffer.getBufferSize(); k++){
-      if(buffer[k]) delete[] buffer[k];
-    }
-  }
-
-  virtual void init(Primitive* own, Joint* joint = 0) override {
-    assert(childSensor.get());
-    number = childSensor->getSensorNumber();
-    buffer.init(buffersize,0);
-    for(int k=0; k<buffersize; k++){
-      buffer[k]=new double[number];
-      memset(buffer[k],0,sizeof(double)*number);
-    }
-    time = 0;
-  };
-
-  virtual bool sense(const GlobalData& globaldata) override {
-    assert(childSensor);
-    childSensor->sense(globaldata);
-    time++;
-    return true;
-  };
-
-  virtual int getSensorNumber() const override { return number;};
-
-  virtual int get(sensor* sensors, int length) const override {
-    assert(length>=number);
-    childSensor->get(buffer[time],number);
-    memcpy(sensors, buffer[time-delay],sizeof(double)*number);
-    return number;
-  }
-  virtual std::list<sensor> getList() const override {
-    return getListOfArray();
-  };
-
-  virtual std::list<SensorMotorInfo> getSensorInfos() const {
-    std::list<SensorMotorInfo> l = childSensor->getSensorInfos();
-    for (auto& i : l){
-      i.name = i.name + "-delayed";
-    }
-    return l;
-  };
-
-protected:
-  const int buffersize=250;
-  int number;
-  std::shared_ptr<Sensor> childSensor;
-  RingBuffer<double*> buffer;
-  int delay;
-  long time;
-};
-
 
 enum SimType { Normal, TwoNormal, Rescue, Fight, FightFixed, Reck, Bungee, TwoBungee, Copy, Fly, Table, Trainer, TwoTrainer, Stool, TwoStools };
 string typeToString(SimType t){
